67baa40eba5fea5e96960737a19d447a3b3f1d87
Aleksandr Dezhin <me@dezhin.net>
2013-01-03 10:33:33 +0400

    Конфиг без комментариев

--- nextgisweb/script.py
+++ nextgisweb/script.py
@@ -40,20 +40,31 @@ def main(argv=sys.argv):
 
 
 def config(argv=sys.argv):
+    argparser = ArgumentParser()
+    argparser.add_argument(
+        '--no-comments', dest='no_comments', action='store_true',
+        help=u"Не включать описание настроек в комментарии"
+    )
+
+    args = argparser.parse_args(argv[1:])
+
+    # немного чиним utf-8
+    sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
+
     from .component import Component, load_all
     load_all()
 
     for comp in Component.registry:
         if hasattr(comp, 'settings_info'):
             print u'[%s]' % comp.identity
-            print u''
+            if not args.no_comments:
+                print u''
             for s in comp.settings_info:
-                if 'desc' in s:
-                    print '# %s ' % s['desc']
+                if not args.no_comments and 'desc' in s:
+                    print u'# %s ' % s['desc']
                 if 'default' in s:
                     print '%s = %s' % (s['key'], s['default'])
                 else:
                     print '# %s = ' % s['key']
 
             print ''
-
