4d2774bad8e96dd9f289be4c9febd6518c4ba964
Aleksandr Dezhin <me@dezhin.net>
2013-01-05 04:57:00 +0400

    Хитрые статические урлы, предотвращающие ошибки кеширования

--- nextgisweb/pyramidcomp.py
+++ nextgisweb/pyramidcomp.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+import sys
+from hashlib import md5
+from StringIO import StringIO
+
 from .component import Component
 
 from pyramid.config import Configurator
@@ -31,9 +35,36 @@ class PyramidComponent(Component):
         authz_policy = ACLAuthorizationPolicy()
         config.set_authorization_policy(authz_policy)
 
-        config.add_static_view('static', 'static', cache_max_age=3600)
         config.add_route('home', '/')
-        config.add_route('amd_package', '/amd_package/*subpath')
+
+        # Чтобы не приходилось вручную чистить кеш статики, сделаем
+        # так, чтобы у них всегда были разные URL. В качестве ключа
+        # используем хеш md5 от всех установленных в окружении пакетов,
+        # который можно вытянуть через pip freeze. Так же pip freeze
+        # вместе с версиями возвращает текущий коммит, для пакетов из
+        # VCS, что тоже полезно.
+
+        # Наверное это можно как-то получше сделать, но делаем так:
+        # перенаправляем sys.stdout в StringIO, запускаем pip freeze и
+        # затем возвращаем sys.stdout на место.
+
+        stdout = sys.stdout
+        static_key = ''
+
+        try:
+            import pip
+            buf = StringIO()
+            sys.stdout = buf
+            pip.main(['freeze', ])
+            h = md5()
+            h.update(buf.getvalue())
+            static_key = '/' + h.hexdigest()
+
+        finally:
+            sys.stdout = stdout
+
+        config.add_static_view('static%s/asset' % static_key, 'static', cache_max_age=3600)
+        config.add_route('amd_package', 'static%s/amd/*subpath' % static_key)
 
         # TODO: не лезть в приватные переменные _env
         for comp in self._env._components.itervalues():
