99afcba239fd52f73bec034dcdafaf525991e08d
Aleksandr Dezhin <me@dezhin.net>
2013-01-08 21:50:09 +0400

    Немного комментариев относительно адаптеров веб-карты

--- nextgisweb/webmap/adapter.py
+++ nextgisweb/webmap/adapter.py
@@ -3,17 +3,29 @@ from ..registry import registry_maker
 
 
 class WebMapAdapter(object):
+    """ Адаптер веб-карты отвечает за то, каким образом стиль слоя будет
+    отображаться на веб-карте.
+
+    Состоит из двух частей. Первая работает на сервере и реализуется в виде
+    python-класса, вторая работает не клиенте и реализуется AMD модуля. """
 
     registry = registry_maker()
 
 
 @WebMapAdapter.registry.register
 class TMSAdapter(object):
+    """ Адаптер, реализующий отображение стиля слоя через TMS-подобный
+    сервис, однако сам TMS-подобный сервис реализуется другим компонентов. """
+
     identity = 'tms'
     js_module = 'webmap/TMSAdapter'
 
 
 @WebMapAdapter.registry.register
 class ImageAdapter(object):
+    """ Адаптер, реализующий отображение стиля слоя через сервис подобный
+    WMS-запросу GetImage, однако сам WMS-подобный сервис реализуется другим
+    компонентом. """
+
     identity = 'image'
     js_module = 'webmap/ImageAdapter'
