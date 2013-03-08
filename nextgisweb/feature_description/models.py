5b2f2cf85df09b51df4d620088a7389a000ddaf4
Aleksandr Dezhin <me@dezhin.net>
2013-03-08 21:13:04 +0400

    Каскадное удаление описаний и фотографий объектов

--- nextgisweb/feature_description/models.py
+++ nextgisweb/feature_description/models.py
@@ -5,12 +5,18 @@ import sqlalchemy.orm as orm
 
 def initialize(comp):
     Base = comp.env.core.Base
+    Layer = comp.env.layer.Layer
 
     class FeatureDescription(Base):
         __tablename__ = 'feature_description'
 
-        layer_id = sa.Column(sa.Integer, sa.ForeignKey('layer.id'), primary_key=True)
+        layer_id = sa.Column(sa.ForeignKey(Layer.id), primary_key=True)
         feature_id = sa.Column(sa.Integer, primary_key=True)
         value = sa.Column(sa.Unicode)
+
+        layer = orm.relationship(
+            Layer,
+            backref=orm.backref('__feature_description', cascade='all')
+        )
 
     comp.FeatureDescription = FeatureDescription
--- nextgisweb/feature_photo/models.py
+++ nextgisweb/feature_photo/models.py
@@ -2,17 +2,25 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
 
+
 def initialize(comp):
     Base = comp.env.core.Base
+    Layer = comp.env.layer.Layer
+    FileObj = comp.env.file_storage.FileObj
 
     class FeaturePhoto(Base):
         __tablename__ = 'feature_photo'
 
         id = sa.Column(sa.Integer, primary_key=True)
-        layer_id = sa.Column(sa.Integer, sa.ForeignKey('layer.id'), nullable=False)
+        layer_id = sa.Column(sa.ForeignKey(Layer.id), nullable=False)
         feature_id = sa.Column(sa.Integer, nullable=False)
-        fileobj_id = sa.Column(sa.Integer, sa.ForeignKey('fileobj.id'), nullable=False)
+        fileobj_id = sa.Column(sa.ForeignKey(FileObj.id), nullable=False)
 
-        fileobj = orm.relationship('FileObj')
+        fileobj = orm.relationship(FileObj)
+
+        layer = orm.relationship(
+            Layer,
+            backref=orm.backref('__feature_photo', cascade='all')
+        )
 
     comp.FeaturePhoto = FeaturePhoto
