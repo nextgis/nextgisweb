bd7b1268fa9e3d72a324d2fe7b98cc14b27e86dc
Aleksandr Dezhin <me@dezhin.net>
2013-10-21 00:43:17 +0400

    Кнопки добавления/удаления ссылок

--- nextgisweb/amd_packages/feature_description/Widget.js
+++ nextgisweb/amd_packages/feature_description/Widget.js
@@ -1,13 +1,15 @@
 define([
     "dojo/_base/declare",
     "ngw/modelWidget/Widget",
-    "dijit/Editor"
+    "dijit/Editor",
+    "dijit/_editor/plugins/LinkDialog"
 ], function (
     declare,
     Widget,
     Editor
 ) {
     return declare([Widget, Editor], {
-        title: "Описание"
+        title: "Описание",
+        extraPlugins: ['|', 'createLink', 'unlink']
     });
 });
