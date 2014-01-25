33a77cc9d6d92282dcc0ae9681f7b6d03084060b
Aleksandr Dezhin <me@dezhin.net>
2014-01-25 10:28:36 +0400

    Новая схема работы с моделями в marker_library

--- nextgisweb/marker_library/models.py
+++ nextgisweb/marker_library/models.py
@@ -4,47 +4,56 @@ from shutil import copyfileobj
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
 
+from ..models import declarative_base
+from ..file_storage import FileObj
+
+Base = declarative_base()
+
+
+class MarkerCollection(Base):
+    __tablename__ = 'marker_collection'
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    keyname = sa.Column(sa.Unicode, unique=True, nullable=False)
+    display_name = sa.Column(sa.Unicode, nullable=False)
+
+
+class MarkerCategory(Base):
+    __tablename__ = 'marker_category'
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    keyname = sa.Column(sa.Unicode, unique=True, nullable=False)
+    display_name = sa.Column(sa.Unicode, nullable=False)
+
+
+class Marker(Base):
+    __tablename__ = 'marker'
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    collection_id = sa.Column(sa.ForeignKey(MarkerCollection.id), nullable=False)
+    category_id = sa.Column(sa.ForeignKey(MarkerCategory.id), nullable=False)
+    keyname = sa.Column(sa.Unicode, unique=True, nullable=False)
+    display_name = sa.Column(sa.Unicode, nullable=False)
+    fileobj_id = sa.Column(sa.ForeignKey(FileObj.id), nullable=False)
+
+    collection = orm.relationship(MarkerCollection)
+    category = orm.relationship(MarkerCategory)
+    fileobj = orm.relationship(FileObj)
+
+    def load_file(self, fp):
+        fileobj = self._file_storage.fileobj('marker_library')
+        filename = self._file_storage.filename(fileobj, makedirs=True)
+
+        with open(filename, 'wb') as fd:
+            copyfileobj(fp, fd)
+
+        self.fileobj = fileobj
+
 
 def initialize(comp):
-    Base = comp.env.core.Base
-    file_storage = comp.env.file_storage
-    FileObj = comp.env.file_storage.FileObj
+    Marker._file_storage = comp.env.file_storage
 
-    class MarkerCollection(Base):
-        __tablename__ = 'marker_collection'
-
-        id = sa.Column(sa.Integer, primary_key=True)
-        keyname = sa.Column(sa.Unicode, unique=True, nullable=False)
-        display_name = sa.Column(sa.Unicode, nullable=False)
-
-    class MarkerCategory(Base):
-        __tablename__ = 'marker_category'
-        id = sa.Column(sa.Integer, primary_key=True)
-        keyname = sa.Column(sa.Unicode, unique=True, nullable=False)
-        display_name = sa.Column(sa.Unicode, nullable=False)
-
-    class Marker(Base):
-        __tablename__ = 'marker'
-
-        id = sa.Column(sa.Integer, primary_key=True)
-        collection_id = sa.Column(sa.ForeignKey(MarkerCollection.id), nullable=False)
-        category_id = sa.Column(sa.ForeignKey(MarkerCategory.id), nullable=False)
-        keyname = sa.Column(sa.Unicode, unique=True, nullable=False)
-        display_name = sa.Column(sa.Unicode, nullable=False)
-        fileobj_id = sa.Column(sa.ForeignKey(FileObj.id), nullable=False)
-
-        collection = orm.relationship(MarkerCollection)
-        category = orm.relationship(MarkerCategory)
-        fileobj = orm.relationship(FileObj)
-
-        def load_file(self, fp):
-            fileobj = file_storage.fileobj(comp.identity)
-            filename = file_storage.filename(fileobj, makedirs=True)
-
-            with open(filename, 'wb') as fd:
-                copyfileobj(fp, fd)
-
-            self.fileobj = fileobj
+    comp.metadata = Base.metadata
 
     comp.MarkerCollection = MarkerCollection
     comp.MarkerCategory = MarkerCategory
