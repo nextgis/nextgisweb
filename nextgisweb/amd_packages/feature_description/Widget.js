1d9efaab7bcfc1a6a06c5b64a016a566cabf48f2
Aleksandr Dezhin <me@dezhin.net>
2014-01-30 04:23:09 +0400

    Описание объекта так же сворачиваем

--- nextgisweb/amd_packages/feature_description/Widget.js
+++ nextgisweb/amd_packages/feature_description/Widget.js
@@ -10,6 +10,17 @@ define([
 ) {
     return declare([Widget, Editor], {
         title: "Описание",
-        extraPlugins: ['|', 'createLink', 'unlink']
+        extraPlugins: ['|', 'createLink', 'unlink'],
+
+        hasData: function () {
+            return this.get('value') != '';
+        },
+
+        _getValueAttr: function () {
+            var value = this.value;
+            value = value.replace("<br _moz_editor_bogus_node=\"TRUE\" />", "");
+            if (value === '<br />') { value = ''; }
+            return value;
+        }       
     });
 });
