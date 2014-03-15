e73e9f79d68f22d570f61ff90bf71ae4e5ef4a06
Aleksandr Dezhin <me@dezhin.net>
2014-03-15 20:30:38 +0400

    Убераем python-virtualenv из зависимостей

--- puppet/nextgisweb/manifests/venv.pp
+++ puppet/nextgisweb/manifests/venv.pp
@@ -1,31 +1,30 @@
-define nextgisweb::venv (
-    $sources = []
-) {
-
-    file { $name: ensure => directory }
+define nextgisweb::venv ($sources, $user = "root") {
 
     package { "python-virtualenv": ensure => installed }
 
-    python::virtualenv { $name:
-        distribute => false,
-        require => [
-            File[$name],
-            Package['python-virtualenv']
-        ]
+    exec { 'virtualenv ${name}':
+        command => "su ${user} -c \"/usr/bin/virtualenv --no-site-packages ${name}\"",
+        path => "/bin:/usr/bin",
+        cwd => "/tmp",
+        creates => $name,
+        require => Package['python-virtualenv'],
     }
 
     nextgisweb::venv::source { $sources:
-        venvname => $name,
+        venv => $name,
+        user => $user,
+        require => Exec['virtualenv ${name}'],
     }
 
 }
 
-define nextgisweb::venv::source($venvname = undef) {
+define nextgisweb::venv::source($venv = undef, $user = undef) {
 
-    exec { "nextgisweb::venv::source ${venvname} ${name}":
-        command => "${venvname}/bin/pip install -e ${name}",
-        require => Python::Virtualenv[$venvname],
+    exec { "nextgisweb::venv::source ${venv} ${name}":
+        command => "su ${user} -c \"export; ${venv}/bin/pip install -e ${name}\"",
+        path => "/bin:/usr/bin",
         timeout => 1200,
+        loglevel => debug
     }
 
 }
