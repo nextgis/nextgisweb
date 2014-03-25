7ed091ca43ee98c2401bdbfcec85695fbbe91544
Aleksandr Dezhin <me@dezhin.net>
2014-03-26 02:15:35 +0400

    Пареметр wrapDateLine включен по-умолчанию для пустого слоя

--- nextgisweb/amd_packages/ngw/openlayers/layer/Layer.js
+++ nextgisweb/amd_packages/ngw/openlayers/layer/Layer.js
@@ -1,16 +1,22 @@
 define([
     "dojo/_base/declare",
+    "dojo/_base/lang",
     "./_Base"
 ], function (
     declare,
+    lang,
     _Base
 ) {
     return declare([_Base], {
         olClassName: "OpenLayers.Layer",
 
         constructor: function (name, options) {
+            options = lang.clone(options);
+
+            if (options.wrapDateLine === undefined) { options.wrapDateLine = true; }
+
             this.olArgs = [name, options];
             this.inherited(arguments);
-        } 
-    })
+        }
+    });
 });
