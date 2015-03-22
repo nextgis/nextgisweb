25dc18241b5ce00699730ab361aa6b745875001c
drnextgis <rykovd@gmail.com>
2015-03-22 13:09:01 +0600

    Поправил отображение дат

--- nextgisweb/feature_layer/amd/FieldsDisplayWidget.js
+++ nextgisweb/feature_layer/amd/FieldsDisplayWidget.js
@@ -1,21 +1,42 @@
 define([
     "dojo/_base/declare",
     "dojo/_base/array",
+    "dojo/date/locale",
     "put-selector/put",
     "./DisplayWidget"
 ], function (
     declare,
     array,
+    locale,
     put,
     DisplayWidget
 ) {
     return declare([DisplayWidget], {
         title: "Атрибуты",
-        
+
         renderValue: function (value) {
             var tbody = put(this.domNode, "table.pure-table.pure-table-horizontal tbody");
             for (var k in value) {
-                put(tbody, "tr th.keyname $ < td.value $", k, value[k]);
+                var val = value[k];
+                var field = this.fields[k];
+
+                if (field.datatype == 'DATE') {
+                    val = locale.format(new Date(val.year, val.month, val.day), {
+                        selector: "date",
+                        formatLength: "medium"
+                    });
+                } else if (field.datatype == 'TIME') {
+                    val = locale.format(new Date(0, 0, 0, val.hour, val.minute, val.second), {
+                        selector: "time",
+                        formatLength: "medium"
+                    });
+                } else if (field.datatype == 'DATETIME') {
+                    val = locale.format(new Date(val.year, val.month, val.day, val.hour, val.minute, val.second), {
+                        formatLength: "medium"
+                    });
+                }
+
+                put(tbody, "tr th.keyname $ < td.value $", k, val);
             }
         }
     });
--- nextgisweb/feature_layer/template/show.mako
+++ nextgisweb/feature_layer/template/show.mako
@@ -19,6 +19,7 @@
                 resourceId: ${ obj.id | json.dumps, n },
                 featureId: ${ feature_id | json.dumps, n },
                 extmid: extmid,
+                fields: ${ fields | json.dumps, n },
                 style: "width: 100%; height: 100%; padding: 1px;"});
 
             widget.placeAt(dom.byId("widget"));
