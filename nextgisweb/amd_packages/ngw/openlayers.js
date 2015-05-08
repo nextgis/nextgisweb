5e755d0c3434fc4df0db6a57e2cdba110ff2e474
drnextgis <rykovd@gmail.com>
2015-05-08 14:41:21 +0600

    Do not manage tiles from singleTile layers

--- nextgisweb/amd_packages/ngw/openlayers.js
+++ nextgisweb/amd_packages/ngw/openlayers.js
@@ -1,5 +1,25 @@
 define([
     ngwConfig.assetUrl + "openlayers/OpenLayers.js"
 ], function () {
+
+    // This fix for issue https://github.com/openlayers/openlayers/issues/1302
+    // have to be removed after switching to OpenLayers newer than 2.13.1
+    OpenLayers.TileManager.prototype.addTile = function(evt) {
+        if (evt.tile instanceof OpenLayers.Tile.Image) {
+          if (!evt.tile.layer.singleTile) {
+            evt.tile.events.on({
+                beforedraw: this.queueTileDraw,
+                beforeload: this.manageTileCache,
+                loadend: this.addToCache,
+                unload: this.unloadTile,
+                scope: this
+            });
+          }
+        } else {
+            // Layer has the wrong tile type, so don't handle it any longer
+            this.removeLayer({layer: evt.tile.layer});
+        }
+    };
+
     return OpenLayers;
 })
