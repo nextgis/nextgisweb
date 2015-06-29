3a274762cecaf81f4fca75d96421243303792cef
drnextgis <rykovd@gmail.com>
2015-06-30 00:09:47 +0600

    Поддержка Basic HTTP аутентификации для компонента wmsclient

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from subprocess import check_output, CalledProcessError
 from setuptools import setup, find_packages
 
 try:
-    gdalver = '==' + check_output(['gdal-config', '--version']).strip()
+    gdalver = '>=' + check_output(['gdal-config', '--version']).strip()
 
 except CalledProcessError:
     gdalver = ''
@@ -29,6 +29,7 @@ requires = [
     'lxml',
     'passlib',
     'owslib',
+    'requests',
 ]
 
 entry_points = {
