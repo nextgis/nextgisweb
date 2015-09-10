f05c6022dd97feb7c2b40759ec413df7807ad670
Aleksandr Dezhin <me@dezhin.net>
2015-09-10 14:11:55 +0300

    Перевод ImageAdapter на новый API отрисовки

--- nextgisweb/render/api.py
+++ nextgisweb/render/api.py
@@ -46,5 +46,5 @@ def image(request):
 
 
 def setup_pyramid(comp, config):
-    config.add_route('render.image', '/api/component/render/image') \
+    config.add_route('render.image', '/api/component/render/image', client=()) \
         .add_view(image)
