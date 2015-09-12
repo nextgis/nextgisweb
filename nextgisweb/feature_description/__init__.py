647adfada253a170d4a04f481a738556cd580409
Aleksandr Dezhin <me@dezhin.net>
2015-09-13 02:39:07 +0300

    Удален неиспользуемый код

--- nextgisweb/feature_description/__init__.py
+++ nextgisweb/feature_description/__init__.py
@@ -16,31 +16,3 @@ class FeatureDescriptionComponent(Component):
     @require('feature_layer')
     def initialize(self):
         from . import extension # NOQA
-
-        # FeatureExtension = self.env.feature_layer.FeatureExtension
-
-        # @FeatureExtension.registry.register
-        # class FeatureDescriptionExtension(FeatureExtension):
-        #     identity = 'feature_description'
-        #     display_widget = 'feature_description/DisplayWidget'
-
-        #     comp = self
-
-        #     def feature_data(self, feature):
-        #         obj = FeatureDescription.filter_by(
-        #             layer_id=self.layer.id,
-        #             feature_id=feature.id,
-        #         ).first()
-
-        #         if obj:
-        #             return obj.value
-
-        #     @property
-        #     def feature_widget(self):
-        #         class _Widget(self.comp.FeatureDescriptionEditWidget):
-        #             layer = self.layer
-        #         return _Widget
-
-    # def setup_pyramid(self, config):
-    #     from . import view
-    #     view.setup_pyramid(self, config)
