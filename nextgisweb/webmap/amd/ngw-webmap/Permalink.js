567b3a7e27ecf84b1d4f789ab48b799c78402d63
Ivan Kavaliou <jownkiv@gmail.com>
2016-07-26 19:56:47 +0300

    refactoring: were added new functions

--- nextgisweb/webmap/amd/ngw-webmap/Permalink.js
+++ nextgisweb/webmap/amd/ngw-webmap/Permalink.js
@@ -20,55 +20,72 @@ define([
                 visbleItems: this.display.getVisibleItems(),
                 map: this.display._mapDeferred
             }).then(
-                lang.hitch(this, function (results) {
-                    var visibleStyles = array.map(
-                        results.visbleItems,
-                        lang.hitch(this, function (i) {
-                            return this.display.itemStore.dumpItem(i).styleId;
-                        })
-                    );
-
-                    var center = ol.proj.toLonLat(this.display.map.olMap.getView().getCenter());
-                    var queryStr = ioQuery.objectToQuery({
-                        base: this.display._baseLayer.name,
-                        lon: center[0].toFixed(4),
-                        lat: center[1].toFixed(4),
-                        angle: this.display.map.olMap.getView().getRotation(),
-                        zoom: this.display.map.olMap.getView().getZoom(),
-                        styles: visibleStyles.join(",")
-                    });
-
-                    var permalink = window.location.origin
-                        + window.location.pathname
-                        + "?" + queryStr;
-
-                    var permalinkDialog = new Dialog({
-                        title: i18n.gettext("Permalink"),
-                        draggable: false,
-                        autofocus: false
-                    });
-
-                    var permalinkContent = new TextBox({
-                        readOnly: false,
-                        selectOnClick: true,
-                        value: decodeURIComponent(permalink),
-                        style: {
-                            width: "300px"
-                        }
-                    });
-
-                    domConstruct.place(
-                        permalinkContent.domNode,
-                        permalinkDialog.containerNode,
-                        "first"
-                    );
-                    permalinkContent.startup();
-                    permalinkDialog.show();
+                lang.hitch(this, function (gettingVisibleItemsResults) {
+                    this._showPermalink(gettingVisibleItemsResults.visbleItems);
                 }),
                 function (error) {
                     console.log(error);
                 }
             );
+        },
+
+        _showPermalink: function (visbleItems) {
+            var permalinkText = this._getPermalink(visbleItems);
+            this._showPermalinkDialog(permalinkText);
+        },
+
+        _getPermalink: function (visbleItems) {
+            var visibleStyles, center, queryStr;
+
+            visibleStyles = array.map(
+                visbleItems,
+                lang.hitch(this, function (i) {
+                    return this.display.itemStore.dumpItem(i).styleId;
+                })
+            );
+
+            center = ol.proj.toLonLat(this.display.map.olMap.getView().getCenter());
+
+            queryStr = ioQuery.objectToQuery({
+                base: this.display._baseLayer.name,
+                lon: center[0].toFixed(4),
+                lat: center[1].toFixed(4),
+                angle: this.display.map.olMap.getView().getRotation(),
+                zoom: this.display.map.olMap.getView().getZoom(),
+                styles: visibleStyles.join(",")
+            });
+
+            return window.location.origin
+                + window.location.pathname
+                + "?" + queryStr;
+        },
+
+        _showPermalinkDialog: function (permalinkText) {
+            var permalinkDialog, permalinkContent;
+
+            permalinkDialog = new Dialog({
+                title: i18n.gettext("Permalink"),
+                draggable: false,
+                autofocus: false
+            });
+
+            permalinkContent = new TextBox({
+                readOnly: false,
+                selectOnClick: true,
+                value: decodeURIComponent(permalinkText),
+                style: {
+                    width: "300px"
+                }
+            });
+
+            domConstruct.place(
+                permalinkContent.domNode,
+                permalinkDialog.containerNode,
+                "first"
+            );
+
+            permalinkContent.startup();
+            permalinkDialog.show();
         }
     });
 });
