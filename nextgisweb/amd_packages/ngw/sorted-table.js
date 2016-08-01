2f2f61dacd69628c562be83396836cf8e584d1af
drnextgis <rykovd@gmail.com>
2016-08-02 02:20:11 +0700

    Return promise from sorted-table

--- nextgisweb/amd_packages/ngw/sorted-table.js
+++ nextgisweb/amd_packages/ngw/sorted-table.js
@@ -1,32 +1,44 @@
 define([
     "dojo/_base/declare",
+    "dojo/Deferred",
     "dojo/dom-attr",
+    "dojo/_base/array",
     "dojo/query",
     "dojo/has",
     "dojo/_base/sniff",
     ngwConfig.assetUrl + "tablesort/tablesort.min.js"
 ], function (
     declare,
+    Deferred,
     domAttr,
+    array,
     query,
     has
 ) {
     var Tablesort = window.Tablesort,
-        requirements = [];
+        mids = [];
 
     if (has("ie")) {
-        requirements.push(ngwConfig.assetUrl + "polyfills/classList.js");
+        mids.push(ngwConfig.assetUrl + "polyfills/classList.js");
     }
 
     return function(node) {
-        var dataattr = "data-sort-method";
+        var deferred = new Deferred(),
+            dataattr = "data-sort-method";
+
         query("[" + dataattr + "]", node).forEach(function (n) {
-            var method = domAttr.get(n, dataattr);
-            requirements.push(ngwConfig.assetUrl + "tablesort/sorts/tablesort." + method + ".js");
+            var method = domAttr.get(n, dataattr),
+                mid = ngwConfig.assetUrl + "tablesort/sorts/tablesort." + method + ".js";
+
+            if (array.indexOf(mids, mid) == -1) {
+                mids.push(mid);
+            }
         });
 
-        require(requirements, function() {
-            new Tablesort(node);
+        require(mids, function() {
+            deferred.resolve(new Tablesort(node));
         });
+
+        return deferred.promise;
     }
 });
