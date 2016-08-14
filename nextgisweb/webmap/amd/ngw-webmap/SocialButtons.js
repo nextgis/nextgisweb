516a65ce34f33b57152fa9e62a4fa9192cfa57fb
Ivan Kavaliou <jownkiv@gmail.com>
2016-08-15 00:20:21 +0300

    removed unused dependencies

--- nextgisweb/webmap/amd/ngw-webmap/SocialButtons.js
+++ nextgisweb/webmap/amd/ngw-webmap/SocialButtons.js
@@ -4,68 +4,31 @@ define([
     "dojo/_base/array",
     "dojo/_base/lang",
     "dojo/dom-style",
-    "dijit/layout/ContentPane",
+    "dojo/on",
     "dijit/_TemplatedMixin",
     "dijit/_WidgetsInTemplateMixin",
-    "dojo/data/ItemFileWriteStore",
-    "dijit/tree/TreeStoreModel",
-    "dijit/Tree",
-    "dijit/tree/dndSource",
-    "dijit/registry",
-    "ngw-resource/serialize",
-    "ngw-resource/ResourceStore",
-    "ngw-resource/ResourcePicker",
+    "dijit/layout/ContentPane",
     "ngw-pyramid/i18n!webmap",
     "ngw-pyramid/hbs-i18n",
-    // resource
     "dojo/text!./template/SocialButtons.hbs",
     "ngw/settings!webmap",
-    // template
-    "dijit/layout/TabContainer",
-    "dojox/layout/TableContainer",
-    "dijit/layout/BorderContainer",
-    "dijit/layout/StackContainer",
-    "dijit/layout/ContentPane",
-    "dijit/Dialog",
-    "dijit/Toolbar",
-    "ngw-pyramid/form/DisplayNameTextBox",
-    "ngw-pyramid/form/ScaleTextBox",
-    "dijit/form/TextBox",
-    "dijit/form/CheckBox",
-    "dijit/form/NumberTextBox",
-    "dijit/form/Select",
-    "ngw-resource/Tree",
     "xstyle/css!./template/resources/SocialButtons/css/fontello.css",
     "xstyle/css!./template/resources/SocialButtons/SocialButtons.css"
-], function (
-    declare,
-    array,
-    lang,
-    domStyle,
-    ContentPane,
-    _TemplatedMixin,
-    _WidgetsInTemplateMixin,
-    ItemFileWriteStore,
-    TreeStoreModel,
-    Tree,
-    dndSource,
-    registry,
-    serialize,
-    ResourceStore,
-    ResourcePicker,
-    i18n,
-    hbsI18n,
-    template,
-    settings
-) {
-    return declare([ContentPane, serialize.Mixin, _TemplatedMixin, _WidgetsInTemplateMixin], {
+], function (declare, array, lang, domStyle, on,
+             _TemplatedMixin, _WidgetsInTemplateMixin, ContentPane,
+             i18n, hbsI18n, template, settings) {
+    return declare([ContentPane, _TemplatedMixin, _WidgetsInTemplateMixin], {
         title: i18n.gettext("Layers"),
         templateString: hbsI18n(template, i18n),
         url: window.location.href,
 
-        constructor: function (){},
+        constructor: function () {
 
-        postCreate: function (){},
+        },
+
+        postCreate: function () {
+
+        },
 
         startup: function () {
             this.inherited(arguments);
