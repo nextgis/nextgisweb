21194c02c8abce08eb2495c99f2c7ae2846666d3
drnextgis <rykovd@gmail.com>
2016-09-21 18:38:42 +0700

    Backup vector_layer sequences

--- nextgisweb/vector_layer/__init__.py
+++ nextgisweb/vector_layer/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from ..component import Component, require
-from ..core import BackupBase, TableBackup
+from ..core import BackupBase, TableBackup, SequenceBackup
 
 from .model import Base, TableInfo, VectorLayer
 
@@ -29,9 +29,24 @@ class VectorLayerComponent(Component):
         from . import view
 
     def backup(self):
+        conn = self.env.core.DBSession.connection()
+
         for i in super(VectorLayerComponent, self).backup():
             yield i
 
         for l in VectorLayer.query():
             yield VectorLayerBackup(self, l.id)
             yield TableBackup(self, 'vector_layer.' + l._tablename)
+
+            for col in l.__table__.columns:
+                if col.primary_key:
+                    test_seq_name = l._tablename + "_" + col.name + "_seq"
+                    res = conn.execute(
+                        """SELECT relname FROM pg_class
+                        WHERE relkind = 'S' AND relname = %s""",
+                        test_seq_name)
+
+                    row = res.fetchone()
+                    if row:
+                        yield SequenceBackup(self, 'vector_layer.' +
+                                             test_seq_name)
