ffad2a5bd2195a04211fa31c5c5882a850db1452
drnextgis <rykovd@gmail.com>
2016-10-17 12:16:19 +0700

    Vector layer AMD module

---
+++ nextgisweb/amd_packages/ngw/openlayers/layer/Vector.js
@@ -0,0 +1,16 @@
+define([
+    "dojo/_base/declare",
+    "./_Base"
+], function (
+    declare,
+    _Base
+) {
+    return declare([_Base], {
+        olLayerClassName: "layer.Vector",
+        olSourceClassName: "source.Vector",
+
+        constructor: function(name, loptions, soptions) {
+            this.inherited(arguments);
+        }
+    })
+});
