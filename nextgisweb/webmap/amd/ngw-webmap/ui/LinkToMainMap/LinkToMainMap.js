680ceb837e24ada390ee09c441ba0b6aefcaeb38
Ivan Kavaliou <jownkiv@gmail.com>
2017-03-27 10:55:25 +0300

    unused dependencies were removed

--- nextgisweb/webmap/amd/ngw-webmap/ui/LinkToMainMap/LinkToMainMap.js
+++ nextgisweb/webmap/amd/ngw-webmap/ui/LinkToMainMap/LinkToMainMap.js
@@ -1,33 +1,21 @@
 define([
     "dojo/_base/declare",
-    "dojo/_base/array",
-    "dojo/_base/lang",
-    "dojo/dom-style",
-    "dojo/on",
-    "dojo/query",
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
-    "dijit/layout/ContentPane",
     "ngw-pyramid/i18n!webmap",
     "ngw-pyramid/hbs-i18n",
     "dojo/text!./LinkToMainMap.hbs",
-    "../NgwShareButtons/NgwShareButtons",
-    "xstyle/css!./LinkToMainMap.css"
-], function (declare, array, lang, domStyle, on, query,
-             _WidgetBase, _TemplatedMixin, ContentPane,
-             i18n, hbsI18n, template, NgwShareButtons) {
+    "xstyle/css!./LinkToMainMap.css",
+    "xstyle/css!../NgwShareButtons/NgwShareButtons.css"
+], function (declare, _WidgetBase, _TemplatedMixin,
+             i18n, hbsI18n, template) {
     return declare([_WidgetBase, _TemplatedMixin], {
         templateString: hbsI18n(template, i18n),
         tinyDisplayUrl: null,
 
         constructor: function (url) {
-            this.inherited(arguments);
             this.tinyDisplayUrl = url;
             this.title = i18n.gettext('Open full map');
-        },
-
-        startup: function () {
-            this.inherited(arguments);
         }
     });
 });
