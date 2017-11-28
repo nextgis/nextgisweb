1914c98afb71e37e529b210bc8fc0d319f4d693f
Ivan Kavaliou <jownkiv@gmail.com>
2017-11-28 22:22:16 +0300

    #605 optional context parameter was added

--- nextgisweb/pyramid/amd/ngw-pyramid/hbs-i18n.js
+++ nextgisweb/pyramid/amd/ngw-pyramid/hbs-i18n.js
@@ -3,14 +3,15 @@ define([
 ], function (
     handlebars
 ) {
-    var translate = function (template, jed) {
+    var translate = function (template, jed, context) {
+        var _context = context || {};
         var env = handlebars.create();
         env.registerHelper("gettext", function (arg) {
             return jed.gettext(arg);
         });
 
         var tobj = env.compile(template);
-        return tobj({});
+        return tobj(_context);
     };
 
     return translate;
