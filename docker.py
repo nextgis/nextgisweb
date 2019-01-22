34b58fca4089e146d11112b44d96a927e1284908
Aleksandr Dezhin <me@dezhin.net>
2019-01-23 00:10:52 +0300

    Add docker.py for new docker environment

---
+++ docker.py
@@ -0,0 +1,19 @@
+import sys
+
+from ngwdocker import PackageBase
+
+class Package(PackageBase):
+
+    def debpackages(self):
+        return (
+            'libgdal-dev',
+            'libgeos-dev',
+            'gdal-bin',
+            'g++',
+            'libxml2-dev',
+            'libxslt1-dev',
+            'zlib1g-dev',
+            'libjpeg-turbo8-dev',
+            'nodejs',
+            'postgresql-client',
+        )
