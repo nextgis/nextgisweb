02adce9e23a560fe68b70038acf9a660382ad115
Ivan Kavaliou <jownkiv@gmail.com>
2019-02-09 08:35:43 +0300

    #660 added addItem method for calling from other modules of NGW

--- nextgisweb/pyramid/amd/ngw-pyramid/navigation-menu/NavigationMenu.js
+++ nextgisweb/pyramid/amd/ngw-pyramid/navigation-menu/NavigationMenu.js
@@ -1,68 +1,98 @@
 define([
-    "dojo/Stateful",
     'dojo/_base/declare',
+    'dojo/_base/lang',
+    'dojo/_base/array',
+    'dojo/Stateful',
     'ngw-pyramid/i18n!webmap',
     'ngw-pyramid/hbs-i18n',
-    "dojo/_base/array",
-    "dojo/query",
-    "dojo/dom-construct",
-    "dijit/_TemplatedMixin",
-    "dijit/layout/ContentPane",
-    "dojo/text!./NavigationMenu.hbs",
-    "dijit/layout/BorderContainer",
-    "xstyle/css!./NavigationMenu.css"
+    'dojo/query',
+    'dojo/dom-construct',
+    'dijit/_TemplatedMixin',
+    'dijit/layout/ContentPane',
+    'dojo/text!./NavigationMenu.hbs',
+    'dijit/layout/BorderContainer',
+    'xstyle/css!./NavigationMenu.css'
 ], function (
-    Stateful,
     declare,
+    lang,
+    array,
+    Stateful,
     i18n,
     hbsI18n,
-    array,
     query,
     domConstruct,
     _TemplatedMixin,
     ContentPane,
     template
 ) {
-    return declare([Stateful, ContentPane,_TemplatedMixin],{
+    return declare([Stateful, ContentPane, _TemplatedMixin], {
         templateString: hbsI18n(template, i18n),
-        items: [], // array of menu item. {name: "name", icon: "icon", value: "value"}
+        items: [],
         value: undefined, //selected item
+
+        /**
+         * Constructor of NavigationMenu.
+         * @param {Object} options - NavigationMenu options.
+         * @param {Object[]} options.items - Array of menu items, [{name: "", title: "", icon: "", value: ""}].
+         */
         constructor: function (options) {
-            declare.safeMixin(this,options);
+            declare.safeMixin(this, options);
         },
-        postCreate: function(){
-            this._render();
+
+        postCreate: function () {
+            this._renderAll();
         },
-        _render: function(){
+
+        _renderAll: function () {
             array.forEach(this.items, function (item) {
-                 var widget = this,
-                     itemEl,
-                     itemActiveClass = (this.value === item.value) ? "active" : "";
-
-                 itemEl = domConstruct.create("div", {
-                     innerHTML: '<span class="navigation-menu__icon material-icons">' + item.icon + '</span>',
-                     className: "navigation-menu__item " + itemActiveClass,
-                     title: item.title,
-                     onclick: function(e){
-                         if (widget.value === item.value)
-                             widget.reset();
-                         else
-                            widget.activateItem(item.value, this);
-                     }
-                 }, this.domNode);
-
-                 itemEl.setAttribute("data-item-value", item.value);
-
+                this._renderItem(item);
             }, this);
         },
-        activateItem: function(value, node){
+
+        /**
+         * Add new menu item to the menu.
+         * @param {Object} item - Menu item {name: "", title: "", icon: "", value: ""}.
+         */
+        addItem: function (item) {
+            if (!relItemName) {
+
+            }
+            this._addItem(item);
+            this._renderItem(item);
+        },
+
+        _addItem: function (item) {
+            this.items.push(item);
+        },
+
+        _renderItem: function (item) {
+            var widget = this,
+                itemActiveClass = (this.value === item.value) ? 'active' : '';
+
+            item.el = domConstruct.create('div', {
+                innerHTML: '<span class="navigation-menu__icon material-icons">' + item.icon + '</span>',
+                className: 'navigation-menu__item ' + itemActiveClass,
+                title: item.title,
+                onclick: function (e) {
+                    if (widget.value === item.value)
+                        widget.reset();
+                    else
+                        widget.activateItem(item.value, this);
+                }
+            }, this.domNode);
+
+            item.el.setAttribute('data-item-value', item.value);
+        },
+
+        activateItem: function (value, node) {
             if (this.value) this.reset();
-            this.set("value", value);
+            this.set('value', value);
             query(node).addClass('active');
         },
-        reset: function(){
-            this.set("value", undefined);
-            query(".active", this.domNode).removeClass("active");
+
+        reset: function () {
+            this.set('value', undefined);
+            query('.active', this.domNode).removeClass('active');
         }
     });
 });
