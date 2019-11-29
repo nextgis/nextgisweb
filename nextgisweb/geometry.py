652f386214490249f6b96439ed0700247ab7db15
edpop <ed.popov.ru@yandex.ru>
2019-11-29 17:47:03 +1000

    Add API for geom transform

--- nextgisweb/geometry.py
+++ nextgisweb/geometry.py
@@ -5,6 +5,7 @@ from inspect import isclass
 import shapely.geometry
 from shapely.geometry import base
 from shapely import wkt, wkb
+from osgeo import ogr, osr
 
 for t in dir(shapely.geometry):
     original = getattr(shapely.geometry, t)
@@ -40,8 +41,23 @@ def geom_from_wkt(data, srid=None):
     return g
 
 
+def geom_to_wkt(g):
+    return wkt.dumps(g)
+
+
 def geom_from_wkb(data, srid=None):
     g = wkb.loads(data)
     g.__class__ = globals()[g.__class__.__name__]
     g._srid = srid
     return g
+
+
+def geom_wkt_transform(data, srs_from, srs_to):
+    geom = ogr.CreateGeometryFromWkt(data)
+    source = osr.SpatialReference()
+    target = osr.SpatialReference()
+    source.ImportFromProj4(srs_from.proj4.encode("utf-8"))
+    target.ImportFromProj4(srs_to.proj4.encode("utf-8"))
+    transformation = osr.CoordinateTransformation(source, target)
+    geom.Transform(transformation)
+    return geom.ExportToWkt()
--- nextgisweb/spatial_ref_sys/api.py
+++ nextgisweb/spatial_ref_sys/api.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, absolute_import, print_function, unicode_literals
 
+from pyramid.response import Response
+
 from .models import SRS
 from .util import convert_projstr_to_wkt, _
+from ..geometry import geom_wkt_transform
 from nextgisweb.core.exception import ValidationError
 
 
@@ -34,6 +37,15 @@ def srs_convert(request):
     return dict(wkt=wkt)
 
 
+def geom_transform(request):
+    srs_from = SRS.filter_by(id=int(request.json_body["srs_id_from"])).one()
+    srs_to = SRS.filter_by(id=int(request.json_body["srs_id_to"])).one()
+    geom_wkt = request.json_body["geom"]
+    geom_wkt_transformed = geom_wkt_transform(geom_wkt, srs_from, srs_to)
+
+    return Response(geom_wkt_transformed)
+
+
 def setup_pyramid(comp, config):
     config.add_route(
         "spatial_ref_sys.collection", "/api/component/spatial_ref_sys/",
@@ -45,3 +57,7 @@ def setup_pyramid(comp, config):
 
     config.add_route("spatial_ref_sys.convert", "/api/component/spatial_ref_sys/convert/") \
         .add_view(srs_convert, request_method="POST", renderer="json")
+
+    config.add_route(
+        "spatial_ref_sys.geom_transform", "/api/component/spatial_ref_sys/geom_transform/") \
+        .add_view(geom_transform, request_method="POST")
