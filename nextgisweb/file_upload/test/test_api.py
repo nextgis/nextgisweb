7f572970b3675b3d3eb99a710c49410c98dbfb7b
Aleksandr Dezhin <me@dezhin.net>
2019-12-24 10:18:37 +0300

    Add file_upload POST test

---
+++ nextgisweb/file_upload/test/test_api.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, absolute_import, print_function, unicode_literals
+
+import webtest
+
+TEST_FILENAME = 'file.ext'
+TEST_CONTENT = bytes('content')
+
+
+def test_upload_post(env, webapp):
+    resp = webapp.post('/api/component/file_upload/upload', dict(
+        file=webtest.Upload(TEST_FILENAME, TEST_CONTENT)
+    ))
+
+    datafn, metafn = env.file_upload.get_filename(resp.json['upload_meta'][0]['id'])
+    with open(datafn, 'rb') as fp:
+        assert fp.read() == TEST_CONTENT
