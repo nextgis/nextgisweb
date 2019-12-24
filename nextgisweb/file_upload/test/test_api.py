1e2ab4cec43d054cfb9fcb522dca375c31323620
Aleksandr Dezhin <me@dezhin.net>
2019-12-24 21:21:48 +0300

    Add file upload PUT test

--- nextgisweb/file_upload/test/test_api.py
+++ nextgisweb/file_upload/test/test_api.py
@@ -9,9 +9,15 @@ TEST_CONTENT = bytes('content')
 
 def test_upload_post(env, webapp):
     resp = webapp.post('/api/component/file_upload/upload', dict(
-        file=webtest.Upload(TEST_FILENAME, TEST_CONTENT)
-    ))
+        file=webtest.Upload(TEST_FILENAME, TEST_CONTENT)))
 
     datafn, metafn = env.file_upload.get_filename(resp.json['upload_meta'][0]['id'])
+    with open(datafn, 'rb') as fp:
+        assert fp.read() == TEST_CONTENT
+
+
+def test_upload_put(env, webapp):
+    resp = webapp.put('/api/component/file_upload/upload', TEST_CONTENT)
+    datafn, metafn = env.file_upload.get_filename(resp.json['id'])
     with open(datafn, 'rb') as fp:
         assert fp.read() == TEST_CONTENT
