ea39e82ea976ddef6f353d0e288d475eb20ab99c
Aleksandr Dezhin <me@dezhin.net>
2020-01-01 19:46:01 +0300

    Future imports in auth component

--- nextgisweb/auth/command.py
+++ nextgisweb/auth/command.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals, print_function, absolute_import
+from __future__ import division, absolute_import, print_function, unicode_literals
+
 import transaction
 
 from ..command import Command
