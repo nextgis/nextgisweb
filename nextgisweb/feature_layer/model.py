86ca5ff4e221fd4cc2bf7b7cf5c9c103a65ab4a1
edpop <ed.popov.ru@yandex.ru>
2020-01-24 12:17:22 +1000

    fix feature_layer serialize in py3

--- nextgisweb/feature_layer/model.py
+++ nextgisweb/feature_layer/model.py
@@ -127,14 +127,13 @@ class LayerFieldsMixin(object):
 class _fields_attr(SP):
 
     def getter(self, srlzr):
-        return map(
-            lambda f: OrderedDict((
+        return [OrderedDict((
                 ('id', f.id), ('keyname', f.keyname),
                 ('datatype', f.datatype), ('typemod', None),
                 ('display_name', f.display_name),
                 ('label_field', f == srlzr.obj.feature_label_field),
-                ('grid_visibility', f.grid_visibility))),
-            srlzr.obj.fields)
+                ('grid_visibility', f.grid_visibility)))
+            for f in srlzr.obj.fields]
 
     def setter(self, srlzr, value):
         obj = srlzr.obj
