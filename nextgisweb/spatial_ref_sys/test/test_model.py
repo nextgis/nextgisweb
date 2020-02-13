212d57b948fd518b352b480d686652ac7676c97e
Aleksandr Dezhin <me@dezhin.net>
2020-02-14 01:13:49 +0300

    Fix tile extent for EPSG:4326 and add some tests

--- nextgisweb/spatial_ref_sys/test/test_model.py
+++ nextgisweb/spatial_ref_sys/test/test_model.py
@@ -6,7 +6,10 @@ import pytest
 from nextgisweb import db
 from nextgisweb.models import DBSession
 from nextgisweb.core.exception import ValidationError
-from nextgisweb.spatial_ref_sys.models import SRS, SRID_LOCAL, WKT_ESPG_4326, WKT_ESPG_3857
+from nextgisweb.spatial_ref_sys.models import (
+    SRS, SRID_LOCAL,
+    WKT_ESPG_4326, WKT_ESPG_3857,
+    BOUNDS_EPSG_3857)
 
 
 def test_postgis_sync(txn):
@@ -54,3 +57,18 @@ def test_wkt_valid():
 def test_wkt_invalid():
     with pytest.raises(ValidationError):
         SRS(wkt='INVALID')
+
+
+@pytest.mark.parametrize('srs_id, tile, expected', (
+    # West and east hemispheres
+    (4326, (0, 0, 0), (-180, -90, 0, 90)),
+    (4326, (0, 1, 0), (0, -90, 180, 90)),
+    # Root tile in EPSG:3857
+    (3857, (0, 0, 0), BOUNDS_EPSG_3857),
+    # Example from mercantile docs rounded to 2 decimal places
+    (3857, (10, 486, 332), (-1017529.72, 7005300.77, -978393.96, 7044436.53)),
+))
+def test_tile_extent(txn, srs_id, tile, expected):
+    srs = SRS.filter_by(id=srs_id).one()
+    extent = tuple([round(c, 2) for c in srs.tile_extent(tile)])
+    assert extent == expected
