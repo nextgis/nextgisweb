b5a5adf2d617bc807953a88879e22ebcba079399
edpop <ed.popov.ru@yandex.ru>
2020-04-09 23:23:49 +1000

    connect nextgis geoservices

--- nextgisweb/tmsclient/api.py
+++ nextgisweb/tmsclient/api.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, absolute_import, print_function, unicode_literals
 
+import requests
+
 from ..resource import ConnectionScope, resource_factory
 
 from .model import Connection, NEXTGIS_GEOSERVICES
@@ -11,12 +13,25 @@ def inspect_connection(request):
 
     connection = request.context
 
-    result = []
+    layers = []
 
     if connection.capmode == NEXTGIS_GEOSERVICES:
-        pass
+        result = requests.get(
+            request.env.tmsclient.options['nextgis_geoservices.layers'],
+            headers=request.env.tmsclient.headers
+        )
 
-    return result
+        for layer in result.json():
+            layers.append(dict(
+                layer=layer['layer'],
+                description=layer['description'],
+                tilesize=layer['tile_size'],
+                minzoom=layer['min_zoom'],
+                maxzoom=layer['max_zoom'],
+                bounds=layer['bounds']
+            ))
+
+    return layers
 
 
 def setup_pyramid(comp, config):
