5c8cdd106a5c9ea1d0433702717b22b3431c7569
drnextgis <rykovd@gmail.com>
2020-06-03 23:39:59 +0200

    Do not let users pick SRS

--- nextgisweb/vector_layer/amd/ngw-vector-layer/Widget.js
+++ nextgisweb/vector_layer/amd/ngw-vector-layer/Widget.js
@@ -7,11 +7,11 @@ define([
     "ngw-pyramid/i18n!vector_layer",
     "ngw-pyramid/hbs-i18n",
     "ngw-resource/serialize",
+    "ngw-spatial-ref-sys/SRSSelect",
     // resource
     "dojo/text!./template/Widget.hbs",
     // template
     "dojox/layout/TableContainer",
-    "ngw-spatial-ref-sys/SRSSelect",
     "ngw-file-upload/Uploader",
     "dijit/form/ComboBox"
 ], function (
@@ -23,12 +23,17 @@ define([
     i18n,
     hbsI18n,
     serialize,
+    SRSSelect,
     template
 ) {
     return declare([ContentPane, serialize.Mixin, _TemplatedMixin, _WidgetsInTemplateMixin], {
         templateString: hbsI18n(template, i18n),
         title: i18n.gettext("Vector layer"),
         prefix: "vector_layer",
+
+        constructor: function() {
+            this.wSrs = SRSSelect({allSrs: null});
+        },
 
         serializeInMixin: function (data) {
             var prefix = this.prefix,
