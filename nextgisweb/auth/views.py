0f080a045304eabace02bb9a10c8ce0aacf0081a
edpop <ed.popov.ru@yandex.ru>
2020-06-25 18:57:04 +1000

    Check admin staying of last user

--- nextgisweb/auth/views.py
+++ nextgisweb/auth/views.py
@@ -247,18 +247,20 @@ def setup_pyramid(comp, config):
                         message=self.request.localizer.translate(
                             _("Login is not unique."))))
 
-            if self.operation == 'edit' and (
-                self.data.get('disabled', False) or 'member_of' in self.data
-            ):
-                admins = Group.filter_by(keyname='administrators').one()
-                if not any([
-                    user for user in admins.members
-                    if not user.disabled and user.principal_id != self.obj.principal_id
-                ]):
-                    result = False
-                    self.error.append(dict(
-                        message=self.request.localizer.translate(
-                            _("You can't disable current administrator. At least one enabled administrator is required."))))  # NOQA
+            if self.operation == 'edit':
+                disabled = self.data.get('disabled', False)
+                if disabled or 'member_of' in self.data:
+                    admins = Group.filter_by(keyname='administrators').one()
+                    if not disabled and admins.id in self.data['member_of']:
+                        pass
+                    elif not any([
+                        user for user in admins.members
+                        if not user.disabled and user.principal_id != self.obj.principal_id
+                    ]):
+                        result = False
+                        self.error.append(dict(
+                            message=self.request.localizer.translate(
+                                _("You can't disable current administrator. At least one enabled administrator is required."))))  # NOQA
 
             return result
 
