263458a831736f1513314b70370037de329522b6
Aleksandr Dezhin <me@dezhin.net>
2020-07-04 23:50:38 +0300

    Add 404 error when serving static amd files

--- nextgisweb/views/__init__.py
+++ nextgisweb/views/__init__.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
+import sys
+import errno
+import os.path
 from pkg_resources import resource_filename
+from six import reraise
 
 from pyramid.response import FileResponse
+from pyramid.httpexceptions import HTTPNotFound
 
 from ..package import amd_packages
+from ..compat import lru_cache
 
 from .model_controller import ModelController, DeleteWidget
 
@@ -23,10 +29,27 @@ def permalinker(model, route_name, keys=('id', )):
 
 
 def amd_package(request):
-    amd_package_name = request.matchdict['subpath'][0]
-    amd_package_path = '/'.join(request.matchdict['subpath'][1:])
+    subpath = request.matchdict['subpath']
+    amd_package_name = subpath[0]
+    amd_package_path = '/'.join(subpath[1:])
+
+    ap_base_path = _amd_package_path(amd_package_name)
+    if ap_base_path is None:
+        raise HTTPNotFound()
+
+    try:
+        return FileResponse(
+            os.path.join(ap_base_path, amd_package_path),
+            cache_max_age=3600, request=request)
+    except (OSError, IOError) as exc:
+        if exc.errno in (errno.ENOENT, errno.EISDIR):
+            raise HTTPNotFound()
+        reraise(*sys.exc_info())
+
+
+@lru_cache(maxsize=64)
+def _amd_package_path(name):
     for p, asset in amd_packages():
-        if p == amd_package_name:
+        if p == name:
             py_package, path = asset.split(':', 1)
-            file_path = resource_filename(py_package, '/'.join((path, amd_package_path)))
-            return FileResponse(file_path, cache_max_age=3600)
+            return resource_filename(py_package, path)
