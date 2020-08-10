121af57d2160e558426442d84fa9cc73d0b10c5e
edpop <ed.popov.ru@yandex.ru>
2020-08-10 19:16:24 +1000

    Do not edit user data in migration

--- migration/2020-07-22-wfsserver_layer-keyname-unique.sql
+++ migration/2020-07-22-wfsserver_layer-keyname-unique.sql
@@ -1,17 +1,2 @@
-WITH d as (
-	SELECT service_id, keyname
-	FROM (
-		SELECT service_id,
-               keyname,
-               count(keyname) OVER (PARTITION BY (service_id, keyname)) AS duplicates
-		FROM wfsserver_layer
-	) as t
-	WHERE duplicates != 1
-	GROUP BY service_id, keyname
-)
-DELETE FROM wfsserver_layer as w
-USING d
-WHERE w.service_id = d.service_id AND w.keyname = d.keyname;
-
 ALTER TABLE public.wfsserver_layer
     ADD CONSTRAINT wfsserver_layer_service_id_keyname_key UNIQUE (service_id, keyname);
