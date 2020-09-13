e8aeeee1baecfa0de634c2a60099ab8eb9b931eb
Aleksandr Dezhin <me@dezhin.net>
2020-09-14 01:38:56 +0300

    Fix ownership in migration

--- migration/2020-08-19-social.sql
+++ migration/2020-08-19-social.sql
@@ -1,19 +1,13 @@
-CREATE TABLE public.resource_social
+CREATE TABLE resource_social
 (
     resource_id integer NOT NULL,
     preview_fileobj_id integer,
     preview_description character varying,
     CONSTRAINT resource_social_pkey PRIMARY KEY (resource_id),
     CONSTRAINT resource_social_preview_fileobj_id_fkey FOREIGN KEY (preview_fileobj_id)
-        REFERENCES public.fileobj (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
+        REFERENCES fileobj (id),
     CONSTRAINT resource_social_resource_id_fkey FOREIGN KEY (resource_id)
-        REFERENCES public.resource (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+        REFERENCES resource (id)
 );
 
-ALTER TABLE public.resource_social OWNER to nextgisweb;
-
-COMMENT ON TABLE public.resource_social IS 'social';
+COMMENT ON TABLE resource_social IS 'social';
