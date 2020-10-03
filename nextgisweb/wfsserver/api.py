ccf739bbdcd4bc440c37535b95dfa6d568ed77f8
Aleksandr Dezhin <me@dezhin.net>
2020-10-03 18:28:15 +0300

    Force WWW-Authenticate header for WFS requests

--- nextgisweb/wfsserver/api.py
+++ nextgisweb/wfsserver/api.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, unicode_literals, print_function, absolute_import
+import sys
+import six
 
 from pyramid.response import Response
 
 from ..resource import resource_factory, ServiceScope
+from ..core.exception import InsufficientPermissions
 
 from .wfs_handler import WFSHandler
 from .model import Service
@@ -13,7 +16,19 @@ NS_XLINK = 'http://www.w3.org/1999/xlink'
 
 
 def wfs(resource, request):
-    request.resource_permission(ServiceScope.connect)
+    try:
+        request.resource_permission(ServiceScope.connect)
+    except InsufficientPermissions:
+        if request.authenticated_userid is None:
+            # Force 401 Unauthorized for unauthenticated users. It's useful for MapInfo
+            # because there is no way to give user credentials directly there.
+
+            # TODO: Maybe it should be implemented in the error handler with an additional
+            # option to enable this behavior.
+
+            return Response(status_code=401, headers={str('WWW-Authenticate'): str("Basic")})
+        else:
+            six.reraise(*sys.exc_info())
 
     wfsHandler = WFSHandler(resource, request)
 
