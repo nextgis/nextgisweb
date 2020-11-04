b608a0b4f4a14d30761c270312330a02471551d6
Aleksandr Dezhin <me@dezhin.net>
2020-11-04 22:39:53 +0300

    Add POINTZ WFS tests

--- nextgisweb/wfsserver/test/test_gdal.py
+++ nextgisweb/wfsserver/test/test_gdal.py
@@ -20,9 +20,9 @@ from nextgisweb.wfsserver.model import Service as WFSService, Layer as WFSLayer
 TEST_WFS_VERSIONS = ('2.0.2', '2.0.0', '1.0.0', )
 
 
-def type_geojson_dataset():
+def type_geojson_dataset(filename):
     import nextgisweb.vector_layer.test
-    path = Path(nextgisweb.vector_layer.test.__file__).parent / 'data' / 'type.geojson'
+    path = Path(nextgisweb.vector_layer.test.__file__).parent / 'data' / filename
     result = ogr.Open(str(path))
     assert result is not None, gdal.GetLastErrorMsg()
     return result
@@ -31,44 +31,61 @@ def type_geojson_dataset():
 @pytest.fixture(scope='module')
 def service(ngw_resource_group):
     with transaction.manager:
-        res_vl = VectorLayer(
-            parent_id=ngw_resource_group, display_name='test_vector_layer',
+        vl_type = VectorLayer(
+            parent_id=ngw_resource_group, display_name='type',
             owner_user=User.by_keyname('administrator'),
             srs=SRS.filter_by(id=3857).one(),
             tbl_uuid=six.text_type(uuid4().hex),
         ).persist()
 
-        dsource = type_geojson_dataset()
+        dsource = type_geojson_dataset('type.geojson')
         layer = dsource.GetLayer(0)
 
-        res_vl.setup_from_ogr(layer, lambda x: x)
-        res_vl.load_from_ogr(layer, lambda x: x)
+        vl_type.setup_from_ogr(layer, lambda x: x)
+        vl_type.load_from_ogr(layer, lambda x: x)
 
         DBSession.flush()
 
         # NOTE: GDAL doesn't support time fields in GML / WFS. It completely breaks
         # XSD schema parsing. Delete the time field to pass tests.
-        DBSession.delete(res_vl.field_by_keyname('time'))
+        DBSession.delete(vl_type.field_by_keyname('time'))
+
+        vl_pointz = VectorLayer(
+            parent_id=ngw_resource_group, display_name='pointz',
+            owner_user=User.by_keyname('administrator'),
+            srs=SRS.filter_by(id=3857).one(),
+            tbl_uuid=six.text_type(uuid4().hex),
+        ).persist()
+
+        dsource = type_geojson_dataset('pointz.geojson')
+        layer = dsource.GetLayer(0)
+
+        vl_pointz.setup_from_ogr(layer, lambda x: x)
+        vl_pointz.load_from_ogr(layer, lambda x: x)
+
+        DBSession.flush()
 
         res_wfs = WFSService(
             parent_id=ngw_resource_group, display_name='test_wfsserver_service',
             owner_user=User.by_keyname('administrator'),
         ).persist()
 
-        res_wfs.layers.append(WFSLayer(
-            resource=res_vl, keyname='test',
-            display_name='test', maxfeatures=1000,
+        res_wfs.layers.extend((
+            WFSLayer(resource=vl_type, keyname='type', display_name='type', maxfeatures=1000),
+            WFSLayer(resource=vl_pointz, keyname='pointz', display_name='pointz', maxfeatures=1000),
         ))
 
         DBSession.flush()
 
-        DBSession.expunge(res_vl)
+        DBSession.expunge(vl_type)
+        DBSession.expunge(vl_pointz)
         DBSession.expunge(res_wfs)
 
     yield res_wfs.id
 
     with transaction.manager:
-        DBSession.delete(VectorLayer.filter_by(id=res_vl.id).one())
+        DBSession.delete(VectorLayer.filter_by(id=vl_type.id).one())
+        DBSession.delete(VectorLayer.filter_by(id=vl_pointz.id).one())
         DBSession.delete(WFSService.filter_by(id=res_wfs.id).one())
 
 
@@ -89,7 +106,7 @@ def features(service, ngw_httptest_app, ngw_auth_administrator):
             wfs_layer = wfs_ds.GetLayer(0)
             assert wfs_layer is not None, gdal.GetLastErrorMsg()
 
-            ref_ds = type_geojson_dataset()
+            ref_ds = type_geojson_dataset('type.geojson')
             ref_layer = ref_ds.GetLayer(0)
 
             factory._cache[version] = (
@@ -111,7 +128,11 @@ def test_layer_name(version, service, ngw_httptest_app, ngw_auth_administrator):
 
     wfs_layer = wfs_ds.GetLayer(0)
     assert wfs_layer is not None, gdal.GetLastErrorMsg()
-    assert wfs_layer.GetName() == 'test'
+    assert wfs_layer.GetName() == 'type'
+
+    wfs_layer = wfs_ds.GetLayer(1)
+    assert wfs_layer is not None, gdal.GetLastErrorMsg()
+    assert wfs_layer.GetName() == 'pointz'
 
 
 @pytest.mark.parametrize('version, key', product(TEST_WFS_VERSIONS, (
@@ -153,27 +174,59 @@ def test_compare(version, key, features):
                 assert vtst == vref
 
 
-@pytest.mark.parametrize('version, fields', product(TEST_WFS_VERSIONS, (
-    dict(null='not null', int=42, real=-0.0, string=None, unicode='¯\\_(ツ)_/¯', geom='POINT(1 1)'),
-    dict(null=None, int=2**16, real=3.1415926535897, string='str', unicode='مرحبا بالعالم', geom='POINT(0.1 -3.1)'),  # NOQA: E501
-)))
-def test_edit(version, fields, service, ngw_httptest_app, ngw_auth_administrator):
+test_create_delete_params = []
+test_edit_params = []
+
+for version in TEST_WFS_VERSIONS:
+    for layer in ('type', 'pointz'):
+        if layer == 'type':
+            test_edit_params.append(pytest.param(
+                version, layer, dict(
+                    null='not null', int=42, real=-0.0,
+                    string=None, unicode='¯\\_(ツ)_/¯'
+                ), 'POINT (1 1)',
+                id='{}-type-f1'.format(version),
+            ))
+            test_edit_params.append(pytest.param(
+                version, layer, dict(
+                    null=None, int=2**16, real=3.1415926535897,
+                    string='str', unicode='مرحبا بالعالم',
+                ), 'POINT (0.1 -3.1)',
+                id='{}-type-f2'.format(version),
+            ))
+        elif layer == 'pointz':
+            test_edit_params.append(pytest.param(
+                version, layer, dict(int=42), 'POINT Z (0 0 -1)',
+                id='{}-points-f1'.format(version),
+            ))
+
+        if version >= '1.1.0' or layer != 'pointz':
+            test_create_delete_params.append(pytest.param(
+                version, layer, 'POINT Z (0 0 -1)' if layer == 'pointz' else 'POINT (0 0)',
+                id="{}-{}".format(version, layer),
+                marks=pytest.mark.xfail(
+                    version >= '2.0.0',
+                    reason="GDAL doesn't work correctly with WFS 2.x"
+                ),
+            ))
+
+
+@pytest.mark.parametrize('version, layer, fields, wkt', test_edit_params)
+def test_edit(version, layer, fields, wkt, service, ngw_httptest_app, ngw_auth_administrator):
     driver = ogr.GetDriverByName(six.ensure_str('WFS'))
     wfs_ds = driver.Open("WFS:{}/api/resource/{}/wfs?VERSION={}".format(
         ngw_httptest_app.base_url, service, version), True)
     assert wfs_ds is not None, gdal.GetLastErrorMsg()
 
-    wfs_layer = wfs_ds.GetLayer(0)
+    wfs_layer = wfs_ds.GetLayerByName(six.ensure_str(layer))
     assert wfs_layer is not None, gdal.GetLastErrorMsg()
 
     feature = wfs_layer.GetNextFeature()
+    geom = ogr.CreateGeometryFromWkt(wkt)
+    feature.SetGeometry(geom)
 
     for k, v in fields.items():
-        if k == 'geom':
-            geom = ogr.CreateGeometryFromWkt(v)
-            assert geom is not None, gdal.GetLastErrorMsg()
-            feature.SetGeometry(geom)
-        elif v is None:
+        if v is None:
             feature.SetFieldNull(k)
         else:
             feature.SetField(k, v)
@@ -181,8 +234,12 @@ def test_edit(version, fields, service, ngw_httptest_app, ngw_auth_administrator
     err = wfs_layer.SetFeature(feature)
     assert err == 0, gdal.GetLastErrorMsg()
 
-    vector_layer_id = ngw_httptest_app.get('/api/resource/%d' % service) \
-        .json()['wfsserver_service']['layers'][0]['resource_id']
+    wfs_service_layers = ngw_httptest_app.get('/api/resource/%d' % service) \
+        .json()['wfsserver_service']['layers']
+
+    for sl in wfs_service_layers:
+        if sl['keyname'] == layer:
+            vector_layer_id = sl['resource_id']
 
     feature_cmp = ngw_httptest_app.get('/api/resource/%d/feature/1' % vector_layer_id).json()
 
@@ -198,27 +255,25 @@ def test_edit(version, fields, service, ngw_httptest_app, ngw_auth_administrator
                 assert v_cmp == v
 
 
-@pytest.mark.parametrize('version', [
-    pytest.param(v, marks=pytest.mark.xfail(v >= '2.0.0', reason="GDAL doesn't work correctly with WFS 2.x"))  # NOQA: E501
-    for v in TEST_WFS_VERSIONS])
-def test_create_delete(version, service, ngw_httptest_app, ngw_auth_administrator):
+@pytest.mark.parametrize('version, layer, wkt', test_create_delete_params)
+def test_create_delete(version, layer, wkt, service, ngw_httptest_app, ngw_auth_administrator):
     driver = ogr.GetDriverByName(six.ensure_str('WFS'))
     wfs_ds = driver.Open("WFS:{}/api/resource/{}/wfs?VERSION={}".format(
         ngw_httptest_app.base_url, service, version), True)
 
     assert wfs_ds is not None, gdal.GetLastErrorMsg()
 
-    wfs_layer = wfs_ds.GetLayer(0)
-    assert wfs_layer is not None, gdal.GetLastErrorMsg()
+    wfslayer_type = wfs_ds.GetLayerByName(six.ensure_str(layer))
+    assert wfslayer_type is not None, gdal.GetLastErrorMsg()
 
-    feature = ogr.Feature(wfs_layer.GetLayerDefn())
+    feature = ogr.Feature(wfslayer_type.GetLayerDefn())
 
-    geom = ogr.CreateGeometryFromWkt('POINT(1 1)')
+    geom = ogr.CreateGeometryFromWkt(wkt)
     feature.SetGeometry(geom)
 
-    err = wfs_layer.CreateFeature(feature)
+    err = wfslayer_type.CreateFeature(feature)
     assert err == 0, gdal.GetLastErrorMsg()
 
     fid = feature.GetFID()
-    err = wfs_layer.DeleteFeature(fid)
+    err = wfslayer_type.DeleteFeature(fid)
     assert err == 0, gdal.GetLastErrorMsg()
