d53b6ce58c7b04c26c4fdd40826e3db38a1694ad
Eduard Popov <ed.popov.ru@yandex.ru>
2020-11-17 00:10:07 +1000

    Change svg marker download permission

--- nextgisweb/svg_marker_library/api.py
+++ nextgisweb/svg_marker_library/api.py
@@ -5,13 +5,13 @@ import zipstream
 from pyramid.httpexceptions import HTTPNotFound
 from pyramid.response import FileResponse, Response
 
-from ..resource import DataScope
+from ..resource import ResourceScope
 
 from .model import SVGMarkerLibrary
 
 
 def file_download(resource, request):
-    request.resource_permission(DataScope.read)
+    request.resource_permission(ResourceScope.read)
 
     fname = request.matchdict['name']
     svg_marker = resource.find_svg_marker(fname)
@@ -23,7 +23,7 @@ def file_download(resource, request):
 
 
 def export(resource, request):
-    request.resource_permission(DataScope.read)
+    request.resource_permission(ResourceScope.read)
 
     zip_stream = zipstream.ZipFile(mode='w', compression=zipstream.ZIP_DEFLATED, allowZip64=True)
     for f in resource.files:
