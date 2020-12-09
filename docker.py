f5669982f437a6839f0c429e212aab9e951b2982
Aleksandr Dezhin <me@dezhin.net>
2020-12-10 02:15:32 +0300

    Add fonts seeding to ngwdocker

--- docker.py
+++ docker.py
@@ -1,8 +1,16 @@
+import ngwdocker
+ngwdocker.require_version('>=2.0.0.dev10')
+
+from pathlib import Path
+
 from ngwdocker import PackageBase
+from ngwdocker.base import AppImage
+from ngwdocker.util import copyfiles
 
 
 class Package(PackageBase):
 
+    # TODO: Remove legacy method
     def debpackages(self):
         return (
             'libgdal-dev',
@@ -17,3 +25,13 @@ class Package(PackageBase):
             'postgresql-client',
             'libmagic-dev',
         )
+
+
+@AppImage.on_user_dir.handler
+def on_user_dir(event):
+    nextgisweb_package = event.image.context.packages['nextgisweb']
+
+    font = nextgisweb_package.settings.get('font')
+    if font is not None:
+        fontpath = Path(font)
+        copyfiles([fontpath], event.source / 'build' / 'font', fontpath)
