06a4a7370f711b925a2a2d76512804dc0896b7ec
Eduard Popov <ed.popov.ru@yandex.ru>
2021-01-12 03:40:46 +1000

    Modern migration

--- migration/2020-12-30-srs-catalog.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE srs ADD COLUMN catalog_id integer;
-ALTER TABLE srs ADD CONSTRAINT srs_catalog_id_key UNIQUE (catalog_id);
---
+++ nextgisweb/spatial_ref_sys/migration/2c1c1918-add-catalog-id.rw.sql
@@ -0,0 +1,4 @@
+/*** { "revision": "2c1c1918" } ***/
+
+ALTER TABLE public.srs DROP CONSTRAINT srs_catalog_id_key;
+ALTER TABLE public.srs DROP COLUMN catalog_id;
