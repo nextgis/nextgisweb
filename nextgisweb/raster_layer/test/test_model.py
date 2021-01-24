ad09985a0d3a31a8d5a2c621baa442ea713422e7
Aleksandr Dezhin <me@dezhin.net>
2021-01-24 16:11:52 +0300

    Update raster layer tests to new NODATA handling
    CU-cv0bww

--- nextgisweb/raster_layer/test/test_model.py
+++ nextgisweb/raster_layer/test/test_model.py
@@ -10,15 +10,19 @@ from nextgisweb.spatial_ref_sys import SRS
 from nextgisweb.raster_layer.model import RasterLayer
 
 
-@pytest.mark.parametrize('source, band_count', [
-    ('sochi-aster-colorized.tif', 4),
-    ('sochi-aster-dem.tif', 1),
+@pytest.mark.parametrize('source, band_count, srs_id', [
+    # Both rasters haven't NODATA value, so the alpha band is expected to be
+    # added in case of reprojection from EPSG:4326 to EPSG:3857.
+    ('sochi-aster-colorized.tif', 4, 3857),
+    ('sochi-aster-colorized.tif', 3, 4326),
+    ('sochi-aster-dem.tif', 2, 3857),
+    ('sochi-aster-dem.tif', 1, 4326),
 ])
-def test_load_file(source, band_count, ngw_env, ngw_txn, ngw_resource_group):
+def test_load_file(source, band_count, srs_id, ngw_env, ngw_txn, ngw_resource_group):
     res = RasterLayer(
         parent_id=ngw_resource_group, display_name='test:{}'.format(source),
         owner_user=User.by_keyname('administrator'),
-        srs=SRS.filter_by(id=3857).one(),
+        srs=SRS.filter_by(id=srs_id).one(),
     ).persist()
 
     res.load_file(os.path.join(os.path.split(__file__)[0], 'data', source), ngw_env)
