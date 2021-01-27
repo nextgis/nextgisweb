9e2e0e8c7ca40cdc7221b585c00c2e535ec3752c
Ivan Kavaliou <info@ikovalev.com>
2021-01-27 22:31:03 +0300

    #751 result section was added to ResourcesFilter
    CU-cx0ynr

--- nextgisweb/resource/amd/ngw-resource/ResourcesFilter/ResourcesFilter.js
+++ nextgisweb/resource/amd/ngw-resource/ResourcesFilter/ResourcesFilter.js
@@ -1,27 +1,91 @@
 define([
     "dojo/_base/declare",
     "dojo/_base/lang",
+    "dojo/_base/array",
+    "dojo/on",
+    "dojo/dom-class",
+    "dojo/html",
+    "dojo/query",
+    "dojo/debounce",
+    "dojo/request/xhr",
+    "dojox/dtl",
+    "dojox/dtl/Context",
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
     "dijit/_WidgetsInTemplateMixin",
+    "ngw/route",
     "ngw-pyramid/hbs-i18n",
     "ngw-pyramid/i18n!resource",
     "dojo/text!./ResourcesFilter.hbs",
+    "dojo/text!./ResourcesFilterResult.dtl.hbs",
     "xstyle/css!./ResourcesFilter.css",
     "dijit/form/TextBox"
 ], function (
-    declare,
-    lang,
-    _WidgetBase,
-    _TemplatedMixin,
-    _WidgetsInTemplateMixin,
-    hbsI18n,
-    i18n,
-    template
+    declare, lang, array, on, domClass, html, query, debounce, xhr,
+    dtl, dtlContext, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin,
+    route, hbsI18n, i18n, template, templateResult
 ) {
+    var TEMPLATE_RESULT_DTL = new dtl.Template(templateResult),
+        SVG_URL = ngwConfig.assetUrl + "/svg/svg-symbols.svg",
+        resourceSearchRoute = route.resource.search,
+        resourceShowRoute = route.resource.show;
+
     return declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
         templateString: hbsI18n(template, i18n),
         baseClass: "resources-filter",
-        title: i18n.gettext("Search resources")
+        title: i18n.gettext("Search resources"),
+
+        postCreate: function () {
+            var input = query('input', this.tbSearch.domNode)[0];
+            on(input, "focus", lang.hitch(this, this.onFocusInput));
+            on(input, "blur", lang.hitch(this, this.onBlurInput));
+
+            on(this.tbSearch, "keyUp", debounce(lang.hitch(this, this.onChangeSearchInput), 1000));
+        },
+
+        onFocusInput: function () {
+            domClass.add(this.domNode, "active");
+        },
+
+        onBlurInput: function () {
+            domClass.remove(this.domNode, "active");
+        },
+
+        onChangeSearchInput: function () {
+            var value = this.tbSearch.get("value");
+            if (!value) return false;
+
+            this.search(value);
+        },
+
+        search: function (value) {
+            xhr.get(resourceSearchRoute(), {
+                handleAs: "json",
+                query: {display_name: value}
+            }).then(
+                lang.hitch(this, this.onSearchSuccess),
+                lang.hitch(this, this.onSearchFail)
+            );
+        },
+
+        onSearchSuccess: function (result) {
+            var dtlContext, resultHtml;
+            
+            result = array.map(result, function (item) {
+                item.url = resourceShowRoute({id: item.resource.id});
+                return item;
+            }, this);
+
+            dtlContext = new dtlContext({
+                svgUrl: SVG_URL,
+                items: result
+            });
+            resultHtml = TEMPLATE_RESULT_DTL.render(dtlContext);
+            html.set(this.result, resultHtml);
+        },
+
+        onSearchFail: function (result) {
+            console.error(result);
+        }
     });
 });
