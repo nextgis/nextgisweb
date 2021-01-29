272713452d83adf92f8dde30bbd0b038916a4cc1
Ivan Kavaliou <info@ikovalev.com>
2021-01-29 23:12:56 +0300

    #751 linear loader was applied to ResourcesFilter; `resource not found` caption was added
    CU-cx0ynr

--- nextgisweb/resource/amd/ngw-resource/ResourcesFilter/ResourcesFilter.js
+++ nextgisweb/resource/amd/ngw-resource/ResourcesFilter/ResourcesFilter.js
@@ -1,32 +1,23 @@
 define([
-    "dojo/_base/declare",
-    "dojo/_base/lang",
-    "dojo/_base/array",
-    "dojo/on",
-    "dojo/dom-class",
-    "dojo/html",
-    "dojo/query",
-    "dojo/debounce",
-    "dojo/request/xhr",
-    "dojox/dtl",
-    "dojox/dtl/Context",
-    "dijit/_WidgetBase",
-    "dijit/_TemplatedMixin",
-    "dijit/_WidgetsInTemplateMixin",
-    "ngw/route",
-    "ngw-pyramid/hbs-i18n",
-    "ngw-pyramid/i18n!resource",
+    "dojo/_base/declare", "dojo/_base/lang", "dojo/_base/array", "dojo/_base/Deferred",
+    "dojo/on", "dojo/dom-class", "dojo/html", "dojo/query",
+    "dojo/debounce", "dojo/request/xhr", "dojo/DeferredList",
+    "dojox/dtl", "dojox/dtl/Context", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dijit/_WidgetsInTemplateMixin",
+    "ngw/route", "ngw-pyramid/hbs-i18n", "ngw-pyramid/i18n!resource",
     "dojo/text!./ResourcesFilter.hbs",
     "dojo/text!./ResourcesFilterResult.dtl.hbs",
     "xstyle/css!./ResourcesFilter.css",
     "dijit/form/TextBox"
 ], function (
-    declare, lang, array, on, domClass, html, query, debounce, xhr,
-    dtl, dtlContext, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin,
+    declare, lang, array, Deferred, on, domClass, html, query, debounce, xhr,
+    DeferredList, dtl, dtlContext, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin,
     route, hbsI18n, i18n, template, templateResult
 ) {
     var TEMPLATE_RESULT_DTL = new dtl.Template(templateResult),
         SVG_URL = ngwConfig.assetUrl + "/svg/svg-symbols.svg",
+        translateDtl = {
+            notFound: i18n.gettext("Resource not found")
+        },
         resourceSearchRoute = route.resource.search,
         resourceShowRoute = route.resource.show;
 
@@ -41,6 +32,14 @@ define([
             on(input, "blur", lang.hitch(this, this.onBlurInput));
 
             on(this.tbSearch, "keyUp", debounce(lang.hitch(this, this.onChangeSearchInput), 1000));
+        },
+
+        getMinTimePromise: function (timeMs) {
+            var deferred = new Deferred();
+            setTimeout(function () {
+                deferred.resolve();
+            }, timeMs);
+            return deferred;
         },
 
         onFocusInput: function () {
@@ -59,18 +58,32 @@ define([
         },
 
         search: function (value) {
-            xhr.get(resourceSearchRoute(), {
-                handleAs: "json",
-                query: {display_name: value}
-            }).then(
-                lang.hitch(this, this.onSearchSuccess),
-                lang.hitch(this, this.onSearchFail)
+            domClass.add(this.domNode, "loading");
+            html.set(this.result, "");
+
+            var deferredList = new DeferredList([
+                this.getMinTimePromise(1000),
+                xhr.get(resourceSearchRoute(), {
+                    handleAs: "json",
+                    query: {display_name: value}
+                })
+            ]);
+
+            deferredList.then(
+                lang.hitch(this, function (result) {
+                    var searchResult = result[1];
+                    if (searchResult[0]) {
+                        this.onSearchSuccess(searchResult[1]);
+                    } else {
+                        this.onSearchFail(searchResult[1]);
+                    }
+                })
             );
         },
 
         onSearchSuccess: function (result) {
             var templateContext, resultHtml;
-            
+
             result = array.map(result, function (item) {
                 item.url = resourceShowRoute({id: item.resource.id});
                 return item;
@@ -78,13 +91,17 @@ define([
 
             templateContext = new dtlContext({
                 svgUrl: SVG_URL,
-                items: result
+                items: result,
+                tr: translateDtl
             });
             resultHtml = TEMPLATE_RESULT_DTL.render(templateContext);
             html.set(this.result, resultHtml);
+
+            domClass.remove(this.domNode, "loading");
         },
 
         onSearchFail: function (result) {
+            domClass.remove(this.domNode, "loading");
             console.error(result);
         }
     });
