704d37e6356b8d123bef42287a806ed8a5abee86
Aleksandr Dezhin <me@dezhin.net>
2021-02-13 01:27:21 +0300

    Format migration

--- nextgisweb/feature_layer/migration/2cbdc86f-defer-unique-constraints.fw.sql
+++ nextgisweb/feature_layer/migration/2cbdc86f-defer-unique-constraints.fw.sql
@@ -9,7 +9,7 @@ ALTER TABLE layer_field
     ADD CONSTRAINT layer_field_layer_id_keyname_key UNIQUE (layer_id, keyname)
     DEFERRABLE INITIALLY DEFERRED;
 
-ALTER TABLE public.layer_field DROP CONSTRAINT layer_field_layer_id_display_name_key;
-ALTER TABLE public.layer_field
+ALTER TABLE layer_field DROP CONSTRAINT layer_field_layer_id_display_name_key;
+ALTER TABLE layer_field
     ADD CONSTRAINT layer_field_layer_id_display_name_key UNIQUE (layer_id, display_name)
     DEFERRABLE INITIALLY DEFERRED;
--- nextgisweb/feature_layer/migration/2cbdc86f-defer-unique-constraints.rw.sql
+++ nextgisweb/feature_layer/migration/2cbdc86f-defer-unique-constraints.rw.sql
@@ -4,6 +4,6 @@ ALTER TABLE layer_field DROP CONSTRAINT layer_field_layer_id_keyname_key;
 ALTER TABLE layer_field
     ADD CONSTRAINT layer_field_layer_id_keyname_key UNIQUE (layer_id, keyname);
 
-ALTER TABLE public.layer_field DROP CONSTRAINT layer_field_layer_id_display_name_key;
-ALTER TABLE public.layer_field
+ALTER TABLE layer_field DROP CONSTRAINT layer_field_layer_id_display_name_key;
+ALTER TABLE layer_field
     ADD CONSTRAINT layer_field_layer_id_display_name_key UNIQUE (layer_id, display_name);
