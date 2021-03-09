99f3cfb24320f3ffddba4e42e6b5c9697b5a27df
Eduard Popov <ed.popov.ru@yandex.ru>
2021-03-09 20:02:40 +1000

    Test 3d geom

--- nextgisweb/lib/geometry/test/test_geom.py
+++ nextgisweb/lib/geometry/test/test_geom.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, unicode_literals, print_function, absolute_import
 
+import pytest
+
 from shapely.geometry import Point
 
 from nextgisweb.lib.geometry import Geometry
 
 
-def test_geom():
-    wkt = 'POINT(1 0)'
-
+@pytest.mark.parametrize('wkt, coords', (
+    ('POINT (1 0)', (1, 0)),
+    ('POINT Z (1 0 -1)', (1, 0, -1))
+))
+def test_geom(wkt, coords):
     geom = Geometry.from_wkt(wkt)
     assert geom.wkt == wkt
 
-    shape = Point(1, 0)
+    shape = Point(*coords)
     assert geom.shape.equals(shape)
 
     assert geom.wkb == shape.wkb
 
-    assert geom.to_geojson() == dict(type='Point', coordinates=(1, 0))
+    assert geom.to_geojson() == dict(type='Point', coordinates=coords)
