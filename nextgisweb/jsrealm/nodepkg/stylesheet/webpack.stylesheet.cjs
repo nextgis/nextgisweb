319c7ae0200916ccb106285763b077aa49623578
Aleksandr Dezhin <me@dezhin.net>
2021-04-23 18:00:38 +0300

    Replace build-in fonts with node packages

--- nextgisweb/jsrealm/nodepkg/stylesheet/webpack.stylesheet.cjs
+++ nextgisweb/jsrealm/nodepkg/stylesheet/webpack.stylesheet.cjs
@@ -10,64 +10,66 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const stylesheetRoot = path.resolve(__dirname, '../../../static');
 
 module.exports = {
-  mode: config.debug ? 'development' : 'production',
-  devtool: config.debug ? 'source-map' : false,
-  entry: {
-    'layout': stylesheetRoot + '/css/layout.less',
-    'default': stylesheetRoot + '/css/default.css',
-    'pure': require.resolve('purecss/build/pure.css')
-  },
-  output: {
-    path: path.resolve(config.distPath + '/stylesheet')
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin()
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.less$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: { publicPath: './' }
-          },
-          "css-loader",
-          {
-            loader: 'less-loader',
-            options: {
-              lessOptions: {
-                rootpath: stylesheetRoot + '/css',
-                javascriptEnabled: true,
-              }
+    mode: config.debug ? 'development' : 'production',
+    devtool: config.debug ? 'source-map' : false,
+    entry: {
+        'layout': stylesheetRoot + '/css/layout.less',
+        'default': stylesheetRoot + '/css/default.css',
+        'pure': require.resolve('purecss/build/pure.css')
+    },
+    output: {
+        path: path.resolve(config.distPath + '/stylesheet')
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: { publicPath: './' }
+                    },
+                    "css-loader",
+                    {
+                        loader: 'less-loader',
+                        options: {
+                            lessOptions: {
+                                rootpath: stylesheetRoot + '/css',
+                                globalVars: {
+                                    node_modules: path.resolve('./node_modules')
+                                },
+                                javascriptEnabled: true,
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            },
+            {
+                test: /\.(woff2?|ttf|eot)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: { outputPath: 'font', name: '[name].[ext]' }
+                    }
+                ]
+            },
+            {
+                test: (fp) => fp.startsWith(stylesheetRoot + '/svg/'),
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: { outputPath: 'svg', name: '[name].[ext]' }
+                    }
+                ]
             }
-          }
         ]
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
-      },
-      {
-        test: (fp) => fp.startsWith(stylesheetRoot + '/font/'),
-        use: [
-          {
-            loader: 'file-loader',
-            options: { outputPath: 'font', name: '[name].[ext]' }
-          }
-        ]
-      },
-      {
-        test: (fp) => fp.startsWith(stylesheetRoot + '/svg/'),
-        use: [
-          {
-            loader: 'file-loader',
-            options: { outputPath: 'svg', name: '[name].[ext]' }
-          }
-        ]
-      }
-
-    ]
-  }
+    }
 };
