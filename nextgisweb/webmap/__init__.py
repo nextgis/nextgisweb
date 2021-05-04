2b32e411d49f21d4f9e6c406741b9c58cdb5b5eb
Aleksandr Dezhin <me@dezhin.net>
2021-05-05 02:15:07 +0300

    Say "good bye" to Bing default basemaps

--- nextgisweb/webmap/__init__.py
+++ nextgisweb/webmap/__init__.py
@@ -41,7 +41,6 @@ class WebMapComponent(Component):
 
         result = dict(
             basemaps=basemaps,
-            bing_apikey=self.options['bing_apikey'],
             annotation=self.options['annotation'],
             adapters=dict(
                 (i.identity, dict(display_name=i.display_name))
@@ -64,7 +63,6 @@ class WebMapComponent(Component):
         Option(
             'basemaps', default=resource_filename('nextgisweb', 'webmap/basemaps.json'),
             doc="Basemaps description file."),
-        Option('bing_apikey', default=None, doc="Bing Maps API key."),
         Option('annotation', bool, default=True, doc="Turn on / off annotations."),
         Option('enable_social_networks', bool, default=True),
     )
