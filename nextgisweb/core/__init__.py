e403aa9e91676dad27f33f0c38b1d0146b4b95b1
Eduard Popov <ed.popov.ru@yandex.ru>
2021-05-18 18:42:25 +1000

    Move workdir_filename to file storage

--- nextgisweb/core/__init__.py
+++ nextgisweb/core/__init__.py
@@ -120,22 +120,6 @@ class CoreComponent(Component):
         return os.path.join(self.options['sdir'], comp.identity) \
             if 'sdir' in self.options else None
 
-    def workdir_filename(self, comp, fobj, makedirs=False):
-        levels = (fobj.uuid[0:2], fobj.uuid[2:4])
-        dname = os.path.join(self.gtsdir(comp), *levels)
-
-        # Create folders if needed
-        if not os.path.isdir(dname):
-            os.makedirs(dname)
-
-        fname = os.path.join(dname, fobj.uuid)
-        oname = self.env.file_storage.filename(fobj, makedirs=makedirs)
-        if not os.path.isfile(fname):
-            src = os.path.relpath(oname, os.path.dirname(fname))
-            os.symlink(src, fname)
-
-        return fname
-
     def mksdir(self, comp):
         """ Create file storage folder """
         self.bmakedirs(self.options['sdir'], comp.identity)
