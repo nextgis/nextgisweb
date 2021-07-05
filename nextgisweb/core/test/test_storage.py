aa38e9962f98eef81d8f63927c14bae3bf672c53
Eduard Popov <ed.popov.ru@yandex.ru>
2021-07-05 15:42:55 +1000

    Test storage on resource delete

--- nextgisweb/core/test/test_storage.py
+++ nextgisweb/core/test/test_storage.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, absolute_import, print_function, unicode_literals
 
+from uuid import uuid4
+
 import pytest
+import six
 import transaction
 from freezegun import freeze_time
 
+from nextgisweb.auth import User
+from nextgisweb.core import KindOfData
 from nextgisweb.models import DBSession
-from nextgisweb.core.storage import KindOfData
-from nextgisweb.core.model import storage_stat_delta
+from nextgisweb.spatial_ref_sys import SRS
+from nextgisweb.vector_layer import VectorLayer
 
 
 class TestKOD1(KindOfData):
@@ -20,14 +25,18 @@ class TestKOD2(KindOfData):
     display_name = identity
 
 
-@pytest.fixture(scope='module', autouse=True)
+@pytest.fixture(scope='function', autouse=True)
 def prepare_storage(ngw_env):
     with transaction.manager:
         ngw_env.core._clear_storage_tables()
-    
+
     with ngw_env.core.options.override({'storage.enabled': True}):
         yield
 
+
+@pytest.fixture(scope='module', autouse=True)
+def reset_storage(ngw_env):
+    yield
     ngw_env.core.estimate_storage_all()
 
 
@@ -49,8 +58,54 @@ def test_storage(ngw_env, ngw_webtest_app, ngw_auth_administrator):
     cur = ngw_env.core.query_storage()
     assert cur[TestKOD1.identity] == dict(
         estimated=None, updated=dt(), data_volume=500)
-    
+
     res = ngw_webtest_app.get('/api/component/pyramid/storage', status=200)
     assert res.json['']['updated'] == dt().isoformat()
     assert res.json[TestKOD1.identity]['data_volume'] == 500
     assert res.json[TestKOD2.identity]['data_volume'] == 100
+
+
+def test_resource_delete(ngw_env, ngw_resource_group):
+    reserve_storage = ngw_env.core.reserve_storage
+    with transaction.manager:
+        res1 = VectorLayer(
+            parent_id=ngw_resource_group,
+            display_name='test-resource-group1',
+            owner_user=User.by_keyname('administrator'),
+            srs=SRS.filter_by(id=3857).one(),
+            geometry_type='POINT',
+            tbl_uuid=six.text_type(uuid4().hex),
+        ).persist()
+        res2 = VectorLayer(
+            parent_id=ngw_resource_group,
+            display_name='test-resource-group2',
+            owner_user=User.by_keyname('administrator'),
+            srs=SRS.filter_by(id=3857).one(),
+            geometry_type='POINT',
+            tbl_uuid=six.text_type(uuid4().hex),
+        ).persist()
+
+        reserve_storage('test_comp', TestKOD1, resource=res1, value_data_volume=100)
+        reserve_storage('test_comp', TestKOD2, resource=res1, value_data_volume=10)
+        reserve_storage('test_comp', TestKOD1, resource=res2, value_data_volume=200)
+
+        DBSession.flush()
+        DBSession.expunge(res1)
+        DBSession.expunge(res2)
+
+    cur = ngw_env.core.query_storage()
+
+    assert cur['']['data_volume'] == 310
+    assert cur[TestKOD1.identity]['data_volume'] == 300
+
+    with transaction.manager:
+        DBSession.delete(res1)
+
+    cur = ngw_env.core.query_storage()
+    assert cur['']['data_volume'] == 200
+
+    with transaction.manager:
+        DBSession.delete(res2)
+
+    cur = ngw_env.core.query_storage()
+    assert cur['']['data_volume'] == 0
