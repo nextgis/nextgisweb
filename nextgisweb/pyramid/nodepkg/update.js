76cb9b330fa9b3312cf8d1724f9f4f6fc997c2b3
Eduard Popov <ed.popov.ru@yandex.ru>
2021-07-12 14:09:57 +1000

    Add packages to update query; fix query params

--- nextgisweb/pyramid/nodepkg/update.js
+++ nextgisweb/pyramid/nodepkg/update.js
@@ -3,6 +3,7 @@ import "whatwg-fetch";
 import { default as domConstruct } from "dojo/dom-construct";
 import { default as query } from "dojo/query";
 import { routeURL } from "./api";
+import pkg_version from "@nextgisweb/pyramid/api/load!/api/component/pyramid/pkg_version";
 
 export function init (ngupdate_url, is_admin) {
     const sysInfoURL = routeURL('pyramid.control_panel.sysinfo');
@@ -10,23 +11,27 @@ export function init (ngupdate_url, is_admin) {
     const timeout = isSysInfo ? 0 : 3 * 60 * 1000;
 
     async function check_update () {
+        const params = new URLSearchParams({
+            type: "check", instance: ngwConfig.instance_id
+        });
         const distr_opts = ngwConfig.distribution;
-        const params = { type: "check", instance: ngwConfig.instance_id };
         if (distr_opts.name !== null) {
-            params.distribution = distr_opts.name + ':' + distr_opts.version;
+            params.append("distribution", distr_opts.name + ':' + distr_opts.version);
+        }
+        for (const [name, version] of Object.entries(pkg_version)) {
+            params.append("package", name + ":" + version);
         }
 
         let response;
         try {
-            response = await window.fetch(ngupdate_url + "/api/query", {
-                query: params,
+            response = await window.fetch(ngupdate_url + "/api/query?" + params.toString(), {
+                method: "GET",
                 headers: {"X-Requested-With": null}
             });
         } catch { return; }
 
         if (is_admin) {
             let has_update = false;
-            has_update = true;
             try {
                 const data = await response.json();
                 has_update = data.distribution.status === "has_update";
