4f3928e44737365de79096c626997caae1c13c92
slowy07 <slowy.arfy@gmail.com>
2021-07-15 12:05:23 +0700

    reformatted code for more readable code and passed tested flake8 pylint

--- nextgisweb/audit/view.py
+++ nextgisweb/audit/view.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
-from __future__ import division, absolute_import, print_function, unicode_literals
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 from datetime import datetime, timedelta
 
 from pyramid.httpexceptions import HTTPNotFound
 
 from .. import dynmenu as dm
 from .api import audit_cget
-from .util import _, es_index, audit_context
-
+from .util import _, audit_context, es_index
 
 PAGE_SIZE = 20
-DATE_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
+DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 def journal_browse(request):
@@ -22,26 +22,26 @@ def journal_browse(request):
     date_last = request.params.get("date_last")
     user = request.params.get("user")
 
-    kwargs = dict(
-        request=request,
-        user=user,
-        limit=PAGE_SIZE
-    )
+    kwargs = dict(request=request, user=user, limit=PAGE_SIZE)
 
     if date_first is not None and date_last is not None:
         raise ValueError(_("'date_first' and 'date_last' are mutually exclusive."))
 
     if date_last:
-        date_last = datetime.strptime(date_last, DATE_FORMAT) - timedelta(milliseconds=1)
+        date_last = datetime.strptime(date_last, DATE_FORMAT) - timedelta(
+            milliseconds=1
+        )
         date_last = date_last.strftime(DATE_FORMAT)
 
     if date_first:
-        kwargs['order'] = 'asc'
-        date_first = datetime.strptime(date_first, DATE_FORMAT) + timedelta(milliseconds=1)
+        kwargs["order"] = "asc"
+        date_first = datetime.strptime(date_first, DATE_FORMAT) + timedelta(
+            milliseconds=1
+        )
         date_first = date_first.strftime(DATE_FORMAT)
 
-    kwargs['date_from'] = date_first or date_from
-    kwargs['date_to'] = date_last or date_to
+    kwargs["date_from"] = date_first or date_from
+    kwargs["date_to"] = date_last or date_to
 
     hits = list(audit_cget(**kwargs))
 
@@ -56,31 +56,41 @@ def journal_browse(request):
         date_to=date_to,
         user=user,
         page_size=PAGE_SIZE,
-        dynmenu=request.env.pyramid.control_panel)
+        dynmenu=request.env.pyramid.control_panel,
+    )
 
 
 def journal_show(request):
     request.require_administrator()
-    rid = request.matchdict['id']
+    rid = request.matchdict["id"]
 
     timestamp = datetime.now()
     index = es_index(timestamp)
 
     docs = request.env.audit.es.search(
         index=index,
-        body=dict(query=dict(
-            ids=dict(values=[rid, ])
-        )))
+        body=dict(
+            query=dict(
+                ids=dict(
+                    values=[
+                        rid,
+                    ]
+                )
+            )
+        ),
+    )
 
-    hits = docs['hits']
-    if hits['total']['value'] != 1:
+    hits = docs["hits"]
+    if hits["total"]["value"] != 1:
         raise HTTPNotFound()
 
-    doc = hits['hits'][0]
+    doc = hits["hits"][0]
 
     return dict(
-        title=_("Journal record: %s") % rid, doc=doc,
-        dynmenu=request.env.pyramid.control_panel)
+        title=_("Journal record: %s") % rid,
+        doc=doc,
+        dynmenu=request.env.pyramid.control_panel,
+    )
 
 
 def setup_pyramid(comp, config):
@@ -90,22 +100,26 @@ def setup_pyramid(comp, config):
 
     if comp.audit_enabled:
         config.add_tween(
-            'nextgisweb.audit.util.audit_tween_factory',
-            under=('nextgisweb.pyramid.util.header_encoding_tween_factory',))
+            "nextgisweb.audit.util.audit_tween_factory",
+            under=("nextgisweb.pyramid.util.header_encoding_tween_factory",),
+        )
 
         if comp.audit_es_enabled:
             config.add_route(
-                'audit.control_panel.journal.browse',
-                '/control-panel/journal/'
-            ).add_view(journal_browse, renderer='nextgisweb:audit/template/browse.mako')
+                "audit.control_panel.journal.browse", "/control-panel/journal/"
+            ).add_view(journal_browse, renderer="nextgisweb:audit/template/browse.mako")
 
             config.add_route(
-                'audit.control_panel.journal.show',
-                '/control-panel/journal/{id}'
-            ).add_view(journal_show, renderer='nextgisweb:audit/template/show.mako')
+                "audit.control_panel.journal.show", "/control-panel/journal/{id}"
+            ).add_view(journal_show, renderer="nextgisweb:audit/template/show.mako")
 
             comp.env.pyramid.control_panel.add(
-                dm.Label('audit', _("Audit")),
-                dm.Link('audit/journal', _("Journal"), lambda args: (
-                    args.request.route_url('audit.control_panel.journal.browse'))),
+                dm.Label("audit", _("Audit")),
+                dm.Link(
+                    "audit/journal",
+                    _("Journal"),
+                    lambda args: (
+                        args.request.route_url("audit.control_panel.journal.browse")
+                    ),
+                ),
             )
