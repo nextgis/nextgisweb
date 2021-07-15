690bb4e8aa8ed7a01d2cf5a3a5be22aa91fccf1a
slowy07 <slowy.arfy@gmail.com>
2021-07-15 12:12:50 +0700

    reformatted code to readable code maintenance, passed tested lint flake8 test

--- nextgisweb/auth/models.py
+++ nextgisweb/auth/models.py
@@ -15,41 +15,41 @@ import six
 Base = declarative_base()
 
 tab_group_user = sa.Table(
-    'auth_group_user', Base.metadata,
+    "auth_group_user",
+    Base.metadata,
     sa.Column(
-        'group_id', sa.Integer,
-        sa.ForeignKey('auth_group.principal_id'),
-        primary_key=True
+        "group_id",
+        sa.Integer,
+        sa.ForeignKey("auth_group.principal_id"),
+        primary_key=True,
     ),
     sa.Column(
-        'user_id', sa.Integer,
-        sa.ForeignKey('auth_user.principal_id'),
-        primary_key=True
-    )
+        "user_id", sa.Integer, sa.ForeignKey("auth_user.principal_id"), primary_key=True
+    ),
 )
 
 
 class Principal(Base):
-    __tablename__ = 'auth_principal'
+    __tablename__ = "auth_principal"
 
-    id = sa.Column(sa.Integer, sa.Sequence('principal_seq'), primary_key=True)
+    id = sa.Column(sa.Integer, sa.Sequence("principal_seq"), primary_key=True)
     cls = sa.Column(sa.Unicode(1), nullable=False)
     system = sa.Column(sa.Boolean, nullable=False, default=False)
     display_name = sa.Column(sa.Unicode, nullable=False)
     description = sa.Column(sa.Unicode)
 
-    __mapper_args__ = dict(
-        polymorphic_on=cls,
-        with_polymorphic='*'
-    )
+    __mapper_args__ = dict(polymorphic_on=cls, with_polymorphic="*")
 
 
 class User(Principal):
-    __tablename__ = 'auth_user'
+    __tablename__ = "auth_user"
 
     principal_id = sa.Column(
-        sa.Integer, sa.Sequence('principal_seq'),
-        sa.ForeignKey(Principal.id), primary_key=True)
+        sa.Integer,
+        sa.Sequence("principal_seq"),
+        sa.ForeignKey(Principal.id),
+        primary_key=True,
+    )
     keyname = sa.Column(sa.Unicode, unique=True)
     superuser = sa.Column(sa.Boolean, nullable=False, default=False)
     disabled = sa.Column(sa.Boolean, nullable=False, default=False)
@@ -59,7 +59,7 @@ class User(Principal):
     last_activity = sa.Column(sa.DateTime)
     language = sa.Column(sa.Unicode)
 
-    __mapper_args__ = dict(polymorphic_identity='U')
+    __mapper_args__ = dict(polymorphic_identity="U")
 
     def __init__(self, password=None, **kwargs):
         super(Principal, self).__init__(**kwargs)
@@ -73,7 +73,7 @@ class User(Principal):
         return self.__str__()
 
     def compare(self, other):
-        """ Compare two users regarding special users """
+        """Compare two users regarding special users"""
 
         # If neither user is special use regular comparison
         if not self.system and not other.system:
@@ -87,67 +87,81 @@ class User(Principal):
 
         # Now a - special user, b - common
 
-        if a.keyname == 'everyone':
+        if a.keyname == "everyone":
             return True
 
-        elif a.keyname == 'authenticated':
-            return b.keyname != 'guest'
+        elif a.keyname == "authenticated":
+            return b.keyname != "guest"
 
-        elif b.keyname == 'authenticated':
-            return a.keyname != 'guest'
+        elif b.keyname == "authenticated":
+            return a.keyname != "guest"
 
         else:
             return a.principal_id == b.principal_id and a.principal_id is not None
 
     @property
     def is_administrator(self):
-        """ Is user member of 'administrators' """
+        """Is user member of 'administrators'"""
         if self.principal_id is None:
             return False
 
         # To reduce number of DB requests, cache
         # 'administrators' group in the instance
-        if not hasattr(self, '_admins'):
-            self._admins = Group.filter_by(keyname='administrators').one()
+        if not hasattr(self, "_admins"):
+            self._admins = Group.filter_by(keyname="administrators").one()
 
-        return any([
-            user for user in self._admins.members
-            if user.principal_id == self.principal_id])
+        return any(
+            [
+                user
+                for user in self._admins.members
+                if user.principal_id == self.principal_id
+            ]
+        )
 
     @property
     def password(self):
         return PasswordHashValue(self.password_hash)
 
-    @password.setter # NOQA
+    @password.setter  # NOQA
     def password(self, value):
         self.password_hash = sha256_crypt.encrypt(value)
 
     def serialize(self):
-        return OrderedDict((
-            ('id', self.id),
-            ('system', self.system),
-            ('display_name', self.display_name),
-            ('description', self.description),
-            ('keyname', self.keyname),
-            ('superuser', self.superuser),
-            ('disabled', self.disabled),
-            ('last_activity', self.last_activity),
-            ('language', self.language),
-            ('oauth_subject', self.oauth_subject),
-            ('oauth_tstamp', self.oauth_tstamp),
-            ('member_of', [g.id for g in self.member_of])
-        ))
+        return OrderedDict(
+            (
+                ("id", self.id),
+                ("system", self.system),
+                ("display_name", self.display_name),
+                ("description", self.description),
+                ("keyname", self.keyname),
+                ("superuser", self.superuser),
+                ("disabled", self.disabled),
+                ("last_activity", self.last_activity),
+                ("language", self.language),
+                ("oauth_subject", self.oauth_subject),
+                ("oauth_tstamp", self.oauth_tstamp),
+                ("member_of", [g.id for g in self.member_of]),
+            )
+        )
 
     def deserialize(self, data):
-        attrs = ('display_name', 'description', 'keyname',
-                 'superuser', 'disabled', 'language', 'password')
+        attrs = (
+            "display_name",
+            "description",
+            "keyname",
+            "superuser",
+            "disabled",
+            "language",
+            "password",
+        )
         for a in attrs:
             if a in data:
                 setattr(self, a, data[a])
 
-        if 'member_of' in data:
-            self.member_of = [Group.filter_by(id=gid).one()
-                              for gid in data['member_of']]
+        if "member_of" in data:
+            self.member_of = [
+                Group.filter_by(id=gid).one() for gid in data["member_of"]
+            ]
 
         if not self.system and not self.disabled:
             env.auth.check_user_limit(self.id)
@@ -157,24 +171,30 @@ class User(Principal):
         return cls.filter(sa.func.lower(User.keyname) == keyname.lower()).one()
 
     __table_args__ = (
-        sa.Index('auth_user_lower_keyname_idx', sa.func.lower(keyname), unique=True),
+        sa.Index("auth_user_lower_keyname_idx", sa.func.lower(keyname), unique=True),
     )
 
 
 class Group(Principal):
-    __tablename__ = 'auth_group'
+    __tablename__ = "auth_group"
 
     principal_id = sa.Column(
-        sa.Integer, sa.Sequence('principal_seq'),
-        sa.ForeignKey(Principal.id), primary_key=True)
+        sa.Integer,
+        sa.Sequence("principal_seq"),
+        sa.ForeignKey(Principal.id),
+        primary_key=True,
+    )
     keyname = sa.Column(sa.Unicode, unique=True)
     register = sa.Column(sa.Boolean, nullable=False, default=False)
 
     members = orm.relationship(
-        User, secondary=tab_group_user, cascade_backrefs=False,
-        backref=orm.backref('member_of'))
+        User,
+        secondary=tab_group_user,
+        cascade_backrefs=False,
+        backref=orm.backref("member_of"),
+    )
 
-    __mapper_args__ = dict(polymorphic_identity='G')
+    __mapper_args__ = dict(polymorphic_identity="G")
 
     def __str__(self):
         return self.display_name
@@ -183,35 +203,36 @@ class Group(Principal):
         return self.__str__()
 
     def is_member(self, user):
-        if self.keyname == 'authorized':
-            return user is not None and user.keyname != 'guest'
+        if self.keyname == "authorized":
+            return user is not None and user.keyname != "guest"
 
-        elif self.keyname == 'everyone':
+        elif self.keyname == "everyone":
             return user is not None
 
         else:
             return user in self.members
 
     def serialize(self):
-        return OrderedDict((
-            ('id', self.id),
-            ('system', self.system),
-            ('display_name', self.display_name),
-            ('description', self.description),
-            ('keyname', self.keyname),
-            ('register', self.register),
-            ('members', [u.id for u in self.members])
-        ))
+        return OrderedDict(
+            (
+                ("id", self.id),
+                ("system", self.system),
+                ("display_name", self.display_name),
+                ("description", self.description),
+                ("keyname", self.keyname),
+                ("register", self.register),
+                ("members", [u.id for u in self.members]),
+            )
+        )
 
     def deserialize(self, data):
-        attrs = ('display_name', 'description', 'keyname', 'register')
+        attrs = ("display_name", "description", "keyname", "register")
         for a in attrs:
             if a in data:
                 setattr(self, a, data[a])
 
-        if 'members' in data:
-            self.members = [User.filter_by(id=uid).one()
-                            for uid in data['members']]
+        if "members" in data:
+            self.members = [User.filter_by(id=uid).one() for uid in data["members"]]
 
 
 @lru_cache(maxsize=256)
@@ -224,7 +245,7 @@ def _password_hash_cache(a, b):
 
 
 class PasswordHashValue(object):
-    """ Automatic password hashes comparison class """
+    """Automatic password hashes comparison class"""
 
     def __init__(self, value):
         self.value = value
