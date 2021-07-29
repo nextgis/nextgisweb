5eedc1d504caf55777875454f5408b0b61ea92de
Eduard Popov <ed.popov.ru@yandex.ru>
2021-07-29 13:01:24 +1000

    Test resource search

--- nextgisweb/resource/test/test_api.py
+++ nextgisweb/resource/test/test_api.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, absolute_import, print_function, unicode_literals
 
+import pytest
+import transaction
+
+from nextgisweb.auth import User
+from nextgisweb.models import DBSession
+from nextgisweb.resource import ResourceGroup
+
 
 def test_disable_resources(
     ngw_env, ngw_webtest_app,
@@ -17,3 +24,33 @@ def test_disable_resources(
 
     with ngw_env.resource.options.override({'disabled_cls': ['resource_group', ]}):
         create_resource_group('diabled_cls', 422)
+
+
+@pytest.fixture(scope='module')
+def resource(ngw_resource_group):
+    with transaction.manager:
+        obj = ResourceGroup(
+            parent_id=ngw_resource_group, display_name='Test Symbols ~%',
+            keyname='Test-Keyname',
+            owner_user=User.by_keyname('administrator'),
+        ).persist()
+
+        DBSession.flush()
+        DBSession.expunge(obj)
+
+    yield obj.id
+
+    with transaction.manager:
+        DBSession.delete(ResourceGroup.filter_by(id=obj.id).one())
+
+
+def test_resource_search(resource, ngw_webtest_app, ngw_auth_administrator):
+    api_url = '/api/resource/search/'
+
+    resp = ngw_webtest_app.get(api_url, dict(
+        display_name='Test Symbols ~%'), status=200)
+    assert len(resp.json) == 1
+
+    resp = ngw_webtest_app.get(api_url, dict(
+        display_name='Test Symbols ~%', keyname='other'), status=200)
+    assert len(resp.json) == 0
