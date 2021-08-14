8e7c0e2f74a45a9d3e93c4dc23e0bf3e4b599579
Aleksandr Dezhin <me@dezhin.net>
2021-08-14 07:41:04 +0300

    Add message extraction from typescript files
    CU-u9fhvm

--- nextgisweb/pyramid/nodepkg/example/typescript.ts
+++ nextgisweb/pyramid/nodepkg/example/typescript.ts
@@ -1,10 +1,12 @@
 /** @entrypoint */
+import i18n from "@nextgisweb/pyramid/i18n!";
+
 interface Foo {
     some: number;
     other: string;
 }
 
 export default async () => {
-    let x: Foo = { some: 1, other: "text" };
+    let x: Foo = { some: 1, other: i18n.gettext("Create") };
     alert(x.other);
 }
