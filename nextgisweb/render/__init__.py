58ece49b302b41bebd6300a7163de962b6d87317
Eduard Popov <ed.popov.ru@yandex.ru>
2021-08-26 15:59:56 +1000

    Estimate tile cache

--- nextgisweb/render/__init__.py
+++ nextgisweb/render/__init__.py
@@ -4,7 +4,10 @@ import os
 import os.path
 
 from ..lib.config import Option
+from ..compat import Path
 from ..component import Component, require
+from ..core import KindOfData
+from ..models import DBSession
 
 from . import command  # NOQA
 from .interface import (
@@ -13,11 +16,12 @@ from .interface import (
     ITileRenderRequest,
     ILegendableStyle,
 )
-from .model import Base
+from .model import Base, ResourceTileCache
 from .event import (
     on_style_change,
     on_data_change,
 )
+from .util import _
 
 
 __all__ = [
@@ -29,6 +33,11 @@ __all__ = [
     'on_style_change',
     'on_data_change',
 ]
+
+
+class TileCacheData(KindOfData):
+    identity = 'tile_cache'
+    display_name = _("Tile cache")
 
 
 class RenderComponent(Component):
@@ -61,6 +70,17 @@ class RenderComponent(Component):
     def backup_configure(self, config):
         super(RenderComponent, self).backup_configure(config)
         config.exclude_table_data('tile_cache', '*')
+
+    def estimate_storage(self):
+        for tc in ResourceTileCache.filter_by(enabled=True).all():
+            path = Path(tc.tilestor_path)
+            if path.exists():
+                size_img = os.stat(str(path)).st_size
+                count = DBSession.execute(
+                    'SELECT count(1) FROM tile_cache."{}"'.format(tc.uuid.hex)
+                ).scalar()
+                size_color = count * 20  # 5x int columns
+                yield TileCacheData, tc.resource_id, size_img + size_color
 
     option_annotations = (
         Option('tile_cache.enabled', bool, default=True),
