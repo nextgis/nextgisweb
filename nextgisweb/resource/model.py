ef3832b4c1417b609e54cf3878d64b005bd831a6
Eduard Popov <ed.popov.ru@yandex.ru>
2021-09-03 15:28:12 +1000

    Validate resource owner in SA

--- nextgisweb/resource/model.py
+++ nextgisweb/resource/model.py
@@ -283,6 +283,14 @@ class Resource(six.with_metaclass(ResourceMeta, Base)):
 
         return value
 
+    @db.validates('owner_user')
+    def _validate_owner_user(self, key, value):
+        with DBSession.no_autoflush:
+            if value.system:
+                raise ValidationError("System user cannot be a resource owner.")
+
+        return value
+
     # Preview
 
     @classmethod
@@ -355,9 +363,6 @@ class _owner_user_attr(SR):
         if not srlzr.user.is_administrator:
             raise ForbiddenError(
                 "Membership in group 'administrators' required!")
-        user = User.filter_by(id=value['id']).one()
-        if user.system:
-            raise ValidationError("System user cannot be a resource owner.")
         return super(_owner_user_attr, self).setter(srlzr, value)
 
 
