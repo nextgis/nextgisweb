37dad598e7947d044113288299875346aa627c22
Eduard Popov <ed.popov.ru@yandex.ru>
2021-09-29 17:38:53 +1000

    Use MEM dataset for image transform

--- nextgisweb/wmsserver/view.py
+++ nextgisweb/wmsserver/view.py
@@ -208,9 +208,6 @@ def _get_map(obj, request):
         distance = xmax - xmin
     w_scale = scale(distance, p_width)
 
-    mem = gdal.GetDriverByName(ensure_str('MEM'))
-    dst_geo = (xmin, (xmax - xmin) / p_width, 0, ymax, 0, (ymin - ymax) / p_height)
-
     for lname in p_layers:
         try:
             lobj = lmap[lname]
@@ -228,22 +225,28 @@ def _get_map(obj, request):
             if srs.id == res.srs.id:
                 limg = req.render_extent(p_bbox, p_size)
             else:
+                mem = gdal.GetDriverByName(ensure_str('MEM'))
+
+                dst_geo = (xmin, (xmax - xmin) / p_width, 0, ymax, 0, (ymin - ymax) / p_height)
                 dst_ds = mem.Create(ensure_str(''), p_width, p_height, 4, gdal.GDT_Byte)
                 dst_ds.SetGeoTransform(dst_geo)
 
-                src_ds = gdal.AutoCreateWarpedVRT(dst_ds, srs.wkt, res.srs.wkt)
-                src_width = src_ds.RasterXSize
-                src_height = src_ds.RasterYSize
-                src_geo = src_ds.GetGeoTransform()
+                vrt = gdal.AutoCreateWarpedVRT(dst_ds, srs.wkt, res.srs.wkt)
+                src_width = vrt.RasterXSize
+                src_height = vrt.RasterYSize
+                src_geo = vrt.GetGeoTransform()
+                vrt = None
+
                 src_bbox = (src_geo[0], src_geo[3] + src_geo[5] * src_height,
                             src_geo[0] + src_geo[1] * src_width, src_geo[3])
-
                 limg = req.render_extent(src_bbox, (src_width, src_height))
 
                 if limg is not None:
                     data = numpy.asarray(limg)
                     img_h, img_w, band_count = data.shape
 
+                    src_ds = mem.Create(ensure_str(''), src_width, src_height, band_count, gdal.GDT_Byte)
+                    src_ds.SetGeoTransform(src_geo)
                     for i in range(band_count):
                         bandArray = data[:, :, i]
                         src_ds.GetRasterBand(i + 1).WriteArray(bandArray)
