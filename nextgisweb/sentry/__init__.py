3d11ac826a645bd17254ec179bb1e2d481f261e1
Aleksandr Dezhin <me@dezhin.net>
2021-11-04 00:53:22 +0300

    Improve setting sentry user id from instance_id
    CU-vhmkn7

--- nextgisweb/sentry/__init__.py
+++ nextgisweb/sentry/__init__.py
@@ -1,37 +1,79 @@
-import sentry_sdk
-from sentry_sdk.integrations.pyramid import PyramidIntegration
-from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
+import json
+from logging import getLogger
+
+import sqlalchemy
 
 from ..component import Component
+from ..core.model import Setting
 from ..lib.config import Option
 
 __all__ = ['SentryComponent', ]
+
+logger = getLogger(__name__)
 
 
 class SentryComponent(Component):
     identity = 'sentry'
 
-    def __init__(self, env, settings):
-        super().__init__(env, settings)
+    def initialize(self):
+        super().initialize()
+        self.enabled = 'dsn' in self.options
+        if self.enabled:
+            self.initialize_sentry_sdk()
+    
+    def initialize_sentry_sdk(self):
+        import sentry_sdk
+        from sentry_sdk.integrations.pyramid import PyramidIntegration
+        from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 
-        if 'dsn' in self.options:
-            # Patch MAX_STRING_LENGTH to prevent clipping of big error messages,
-            # especially for mako template tracebacks.
-            sentry_sdk.utils.MAX_STRING_LENGTH = 8192
+        # Patch MAX_STRING_LENGTH to prevent clipping of big error messages,
+        # especially for mako template tracebacks.
+        sentry_sdk.utils.MAX_STRING_LENGTH = 8192
 
-            sentry_sdk.init(
-                self.options['dsn'],
-                integrations=[
-                    PyramidIntegration(),
-                    SqlalchemyIntegration(),
-                ],
-                ignore_errors=[KeyboardInterrupt],
-                environment=self.options['environment'],
-                shutdown_timeout=self.options['shutdown_timeout'],
-            )
+        sentry_sdk.init(
+            self.options['dsn'],
+            integrations=[
+                PyramidIntegration(),
+                SqlalchemyIntegration(),
+            ],
+            ignore_errors=[KeyboardInterrupt],
+            environment=self.options['environment'],
+            shutdown_timeout=self.options['shutdown_timeout'],
+        )
+        
+        ts = Setting.__table__
+        qs = ts.select().where(sqlalchemy.and_(
+            ts.c.component == 'core',
+            ts.c.name == 'instance_id'))
 
-    def setup_pyramid(self, config):
-        sentry_sdk.set_user(dict(id=self.env.core.instance_id))
+        event_user = None
+
+        def add_instance_id(event, hint):
+            nonlocal event_user
+            if event_user is None:
+                try:
+                    row = self.env.core.DBSession.connection() \
+                        .execute(qs).fetchone()
+                except sqlalchemy.exc.ProgrammingError:
+                    logger.debug(
+                        "Failed to get instance_id, the database may not "
+                        "have been initialized yet")
+                except Exception as exc:
+                    logger.error("Got an exception while getting instance_id")
+                else:
+                    if row is not None:
+                        instance_id = json.loads(row.value)
+                        event_user = dict(id=instance_id)
+                    else:
+                        logger.debug("Missing instance_id")
+
+            if event_user is not None:
+                event['user'] = event_user
+
+            return event
+
+        with sentry_sdk.configure_scope() as scope:
+            scope.add_event_processor(add_instance_id)
 
     option_annotations = (
         Option('dsn'),
