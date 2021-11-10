84cb213f4711b495ca2e20a8be1a2342987502dc
Ivan Kavaliou <info@ikovalev.com>
2021-11-10 21:54:20 +0300

    Simplify updating address migration
    CU-nhewa5

--- nextgisweb/webmap/migration/3206493e-add-yandex-geocoder.fw.sql
+++ nextgisweb/webmap/migration/3206493e-add-yandex-geocoder.fw.sql
@@ -4,49 +4,14 @@
     "message": "Add Yandex geocoder"
 } ***/
 
-DO
-$do$
-    BEGIN
-        -- Get old "nominatim_enabled" value
-        -- Put the value in "address_search_enabled"
-        -- Remove old "nominatim_enabled"
-        IF EXISTS(
-                SELECT 1
-                FROM setting s
-                WHERE s.component = 'webmap'
-                  AND name = 'nominatim_enabled')
-        THEN
-            INSERT INTO setting (component, name, value)
-            SELECT component, 'address_search_enabled', value = '"on"'
-            FROM setting
-            WHERE component = 'webmap'
-              AND name = 'nominatim_enabled';
+UPDATE setting
+SET value = CASE WHEN value = '"on"' THEN 'true' ELSE 'false' END,
+    name  = 'address_search_enabled'
+WHERE component = 'webmap'
+  AND name = 'nominatim_enabled';
 
-            DELETE
-            FROM setting
-            WHERE component = 'webmap'
-              AND name = 'nominatim_enabled';
-        END IF;
-
-        -- Get old "nominatim_extent" value
-        -- Put the value in "address_search_extent"
-        -- Remove old "nominatim_extent"
-        IF EXISTS(
-                SELECT 1
-                FROM setting s
-                WHERE s.component = 'webmap'
-                  AND name = 'nominatim_extent')
-        THEN
-            INSERT INTO setting (component, name, value)
-            SELECT component, 'address_search_extent', value = '"on"'
-            FROM setting
-            WHERE component = 'webmap'
-              AND name = 'nominatim_extent';
-
-            DELETE
-            FROM setting
-            WHERE component = 'webmap'
-              AND name = 'nominatim_extent';
-        END IF;
-    END
-$do$
+UPDATE setting
+SET value = CASE WHEN value = '"on"' THEN 'true' ELSE 'false' END,
+    name  = 'address_search_extent'
+WHERE component = 'webmap'
+  AND name = 'nominatim_extent';
