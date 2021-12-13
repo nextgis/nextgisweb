210857431b78755f05cdf0498e7ee647d26fb06b
Eduard Popov <ed.popov.ru@yandex.ru>
2021-12-13 14:41:25 +1000

    Fix tmsclient layer connection validate

--- nextgisweb/tmsclient/amd/ngw-tmsclient/LayerWidget.js
+++ nextgisweb/tmsclient/amd/ngw-tmsclient/LayerWidget.js
@@ -55,6 +55,8 @@ define([
         },
 
         postCreate: function () {
+            this.inherited(arguments);
+
             this.wConnection.on("update", function (event) {
                 var connection = event.value;
                 xhr.get(route.tmsclient.connection.layers(connection), {
@@ -80,35 +82,8 @@ define([
         },
 
         serializeInMixin: function (data) {
-            if (data[this.serializePrefix] === undefined) { data[this.serializePrefix] = {}; }
-            var value = data[this.serializePrefix];
-
-            value.connection = this.wConnection.get("value");
-            value.srs = {id: this.wSRS.get("value")};
-            value.tilesize = this.wTileSize.get("value");
-            value.layer_name = this.wLayerName.get("value");
-            value.minzoom = this.wMinZoom.get("value");
-            value.maxzoom = this.wMaxZoom.get("value");
-            value.extent_left = this.wExtentLeft.get("value");
-            value.extent_right = this.wExtentRight.get("value");
-            value.extent_bottom = this.wExtentBottom.get("value");
-            value.extent_top = this.wExtentTop.get("value");
-        },
-
-        deserializeInMixin: function (data) {
-            var value = data[this.serializePrefix];
-            if (value === undefined) { return; }
-
-            this.wConnection.set("value", value.connection);
-            this.wSRS.set("value", value.srs.id);
-            this.wTileSize.set("value", value.tilesize);
-            this.wLayerName.set("value", value.layer_name);
-            this.wMinZoom.set("value", value.minzoom);
-            this.wMaxZoom.set("value", value.maxzoom);
-            this.wExtentLeft.set("value", value.extent_left);
-            this.wExtentRight.set("value", value.extent_right);
-            this.wExtentBottom.set("value", value.extent_bottom);
-            this.wExtentTop.set("value", value.extent_top);
+            this.inherited(arguments);
+            data[this.serializePrefix].srs = {id: this.wSRS.get("value")};
         }
     });
 });
