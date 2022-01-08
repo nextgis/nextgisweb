5414fb96098e02a26ffc03031d48f08cf04234b0
Aleksandr Dezhin <me@dezhin.net>
2022-01-08 05:26:36 +0300

    Add function-scoped test resource group fixture

--- nextgisweb/resource/test/__init__.py
+++ nextgisweb/resource/test/__init__.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from uuid import uuid4
 
 import pytest
 import transaction
@@ -20,10 +21,26 @@ def ngw_resource_group(ngw_env):
     yield res.id
 
     with transaction.manager:
-        def recurse_children(res):
-            for child in res.children:
-                DBSession.delete(child)
-            DBSession.delete(res)
-        
         with DBSession.no_autoflush:
-            recurse_children(ResourceGroup.filter_by(id=res.id).one())
+            delete_recursive(ResourceGroup.filter_by(id=res.id).one())
+
+
+@pytest.fixture(scope='function')
+def ngw_resource_group_sub(ngw_resource_group, ngw_env):
+    with transaction.manager:
+        res = ResourceGroup(
+            parent_id=ngw_resource_group, owner_user=User.by_keyname('administrator'),
+            display_name=str(uuid4()),
+        ).persist()
+
+    yield res.id
+
+    with transaction.manager:
+        with DBSession.no_autoflush:
+            delete_recursive(ResourceGroup.filter_by(id=res.id).one())
+
+
+def delete_recursive(res):
+    for child in res.children:
+        DBSession.delete(child)
+    DBSession.delete(res)
