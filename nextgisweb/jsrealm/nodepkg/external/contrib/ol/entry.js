c4a05b6aada73b210c8d14d898648cd3f619abcf
Aleksandr Dezhin <me@dezhin.net>
2022-01-08 16:02:54 +0300

    Update OpenLayers update instructions

--- nextgisweb/jsrealm/nodepkg/external/contrib/ol/entry.js
+++ nextgisweb/jsrealm/nodepkg/external/contrib/ol/entry.js
@@ -1,13 +1,18 @@
 import "ol/ol.css"
 
-// File "index.js" was extracted from OpenLayers build processs. Steps to update
-// the file:
-//
-// $ git clone git@github.com:openlayers/openlayers.git ol-tmp
-// $ cd ol-tmp
-// $ npm i && npm build-index
-// $ sed -E 's/= ([\$_]ol.+);/= \1 || {};/g' build/index.js > \
-//   /path/to/nextgisweb/nextgisweb/jsrealm/nodepkg/external/contrib/ol/index.js
+/*
+
+File "index.js" was extracted from OpenLayers build processs. Steps to update
+the file:
+
+$ git clone git@github.com:openlayers/openlayers.git ol-tmp
+$ cd ol-tmp
+$ git checkout v6.10.0
+$ npm i && npm build-index
+$ sed -E 's/= ([\$_]ol.+);/= \1 || {};/g' build/index.js > \
+    /path/to/nextgisweb/nextgisweb/jsrealm/nodepkg/external/contrib/ol/index.js
+
+*/
 
 import ol from "./index.js";
 
