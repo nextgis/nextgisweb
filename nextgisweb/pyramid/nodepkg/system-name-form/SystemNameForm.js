4a2f17a5b27095b6d6c88e643377b15774a6865b
Aleksandr Dezhin <me@dezhin.net>
2022-02-02 15:58:53 +0300

    Set indentation to 4 spaces

--- nextgisweb/pyramid/nodepkg/system-name-form/SystemNameForm.js
+++ nextgisweb/pyramid/nodepkg/system-name-form/SystemNameForm.js
@@ -6,52 +6,52 @@ import ErrorDialog from "ngw-pyramid/ErrorDialog/ErrorDialog";
 import { useEffect, useState } from "react";
 
 export function SystemNameForm() {
-  const [fullName, setFullName] = useState();
-  const [loading, setLoading] = useState(false);
+    const [fullName, setFullName] = useState();
+    const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    route("pyramid.system_name")
-      .get()
-      .then((data) => {
-        setFullName(data.full_name);
-      });
-  }, []);
+    useEffect(() => {
+        route("pyramid.system_name")
+            .get()
+            .then((data) => {
+                setFullName(data.full_name);
+            });
+    }, []);
 
-  const saveSystemName = () => {
-    setLoading(true);
+    const saveSystemName = () => {
+        setLoading(true);
 
-    const data = { full_name: fullName };
-    route("pyramid.system_name")
-      .put({ json: data })
-      .then(() => {
-        window.location.reload(true);
-      })
-      .catch((err) => {
-        new ErrorDialog(err).show();
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+        const data = { full_name: fullName };
+        route("pyramid.system_name")
+            .put({ json: data })
+            .then(() => {
+                window.location.reload(true);
+            })
+            .catch((err) => {
+                new ErrorDialog(err).show();
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    };
 
-  return (
-    <>
-      <Input.Group compact>
-        <Input
-          value={fullName}
-          style={{ width: "calc(100% - 200px)" }}
-          onChange={(e) => setFullName(e.target.value)}
-        />
-        <Button
-          type="primary"
-          loading={loading}
-          disabled={!fullName}
-          icon={<SaveOutlined />}
-          onClick={saveSystemName}
-        >
-          {i18n.gettext("Save")}
-        </Button>
-      </Input.Group>
-    </>
-  );
+    return (
+        <>
+            <Input.Group compact>
+                <Input
+                    value={fullName}
+                    style={{ width: "calc(100% - 200px)" }}
+                    onChange={(e) => setFullName(e.target.value)}
+                />
+                <Button
+                    type="primary"
+                    loading={loading}
+                    disabled={!fullName}
+                    icon={<SaveOutlined />}
+                    onClick={saveSystemName}
+                >
+                    {i18n.gettext("Save")}
+                </Button>
+            </Input.Group>
+        </>
+    );
 }
