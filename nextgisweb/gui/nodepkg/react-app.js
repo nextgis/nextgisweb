e504e2eb327c7acf2c27a1f1f537a365a1e43a78
Aleksandr Dezhin <me@dezhin.net>
2022-02-08 01:13:28 +0300

    Disable strict mode for react

--- nextgisweb/gui/nodepkg/react-app.js
+++ nextgisweb/gui/nodepkg/react-app.js
@@ -1,15 +1,12 @@
 /** @entrypoint */
-import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { ConfigProvider } from "./antd";
 
 export default function (Component, props, domNode) {
     ReactDOM.render(
-        <StrictMode>
-            <ConfigProvider>
-                <Component {...props} />
-            </ConfigProvider>
-        </StrictMode>,
+        <ConfigProvider>
+            <Component {...props} />
+        </ConfigProvider>,
         domNode
     );
 }
