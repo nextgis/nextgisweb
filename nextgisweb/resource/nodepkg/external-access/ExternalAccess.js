8514fe98b6c54443b03658e809d27d71ffc84652
Aleksandr Dezhin <me@dezhin.net>
2022-05-05 00:53:08 +0300

    Remove unused imports and styles

--- nextgisweb/resource/nodepkg/external-access/ExternalAccess.js
+++ nextgisweb/resource/nodepkg/external-access/ExternalAccess.js
@@ -1,12 +1,7 @@
 import {PropTypes} from "prop-types";
-import {useState, useEffect} from "react";
-import {Radio, Button, Space, Row, Typography, Col, Skeleton, Popover} from "@nextgisweb/gui/antd";
+import {Popover} from "@nextgisweb/gui/antd";
 import {InfoCircleOutlined, CopyOutlined} from "@ant-design/icons";
 import {CopyToClipboard} from 'react-copy-to-clipboard';
-import {SaveOutlined} from "@ant-design/icons";
-import {route} from "@nextgisweb/pyramid/api";
-import ErrorDialog from "ngw-pyramid/ErrorDialog/ErrorDialog";
-import i18n from "@nextgisweb/pyramid/i18n!";
 
 import "./ExternalAccess.less";
 
@@ -22,7 +17,6 @@ export function ExternalAccess({htmlDataset}) {
                href={externalDocUrl}>
                 {externalDocText}
             </a>
-            <div className="material-icons icon-exitToApp"></div>
         </>;
     }
 
