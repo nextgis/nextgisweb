097c621f4706d89e4cb56fdf2d3bbd8fe6a47b98
Aleksandr Dezhin <me@dezhin.net>
2022-05-15 19:47:36 +0300

    Remove deprecated modules: compat and geojson

--- nextgisweb/geojson.py
+++
@@ -1,9 +0,0 @@
-import warnings
-
-from .lib.json import dumps, loads  # NOQA
-
-
-warnings.warn(
-    "The 'nextgisweb.geojson' module deprecated now and it's going to be "
-    "removed in 4.2.0. Use the 'nextgisweb.lib.json' module instead.",
-    DeprecationWarning, stacklevel=2)
