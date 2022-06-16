ea891b4f72be302476f5f18991142e41d4ff6012
Artemiy <rendrom@gmail.com>
2022-06-16 22:49:31 +0200

    Fix model browse selection
    (#CU-2m7mv4a)

--- nextgisweb/gui/nodepkg/model-browse/ModelBrowse.js
+++ nextgisweb/gui/nodepkg/model-browse/ModelBrowse.js
@@ -244,7 +244,7 @@ export function ModelBrowse({
 
     const SelectedControl = () => (
         <Space direction="horizontal">
-            {selectedControls.map((control) => (
+            {selectedControls.map((control, idx) => (
                 <React.Fragment key={idx}>
                     control({(selected, rows, setRows)})
                 </React.Fragment>
