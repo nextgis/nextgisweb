4d9efbea233e249620cc201d9d3394efa9293a7d
Eduard Popov <ed.popov.ru@yandex.ru>
2022-10-18 19:05:46 +1000

    Fix PIL warnings

--- nextgisweb/feature_attachment/exif.py
+++ nextgisweb/feature_attachment/exif.py
@@ -1,7 +1,7 @@
 # Borrowed from https://github.com/kylefox/python-image-orientation-patch
 
 from collections import namedtuple
-from PIL import Image
+from PIL.Image import Transpose
 
 Orientation = namedtuple('Orientation', ['description', 'degrees'])
 
@@ -11,10 +11,10 @@ EXIF_ORIENTATION_TAG = 274
 ORIENTATIONS = {
     1: Orientation("Normal", 0),
     2: Orientation("Mirrored left-to-right", 0),
-    3: Orientation("Rotated 180 degrees", Image.ROTATE_180),
+    3: Orientation("Rotated 180 degrees", Transpose.ROTATE_180),
     4: Orientation("Mirrored top-to-bottom", 0),
     5: Orientation("Mirrored along top-left diagonal", 0),
-    6: Orientation("Rotated 90 degrees", Image.ROTATE_270),
+    6: Orientation("Rotated 90 degrees", Transpose.ROTATE_270),
     7: Orientation("Mirrored along top-right diagonal", 0),
-    8: Orientation("Rotated 270 degrees", Image.ROTATE_90)
+    8: Orientation("Rotated 270 degrees", Transpose.ROTATE_90)
 }
