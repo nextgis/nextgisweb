3cd1e68a25fee9ab0b8d24bf55b29303d403f459
Artemiy <rendrom@gmail.com>
2022-11-24 02:46:13 +0300

    Add LoaderCache.clean() method

--- nextgisweb/pyramid/nodepkg/util/loader.js
+++ nextgisweb/pyramid/nodepkg/util/loader.js
@@ -1,7 +1,6 @@
 export class LoaderCache {
     constructor() {
         this.promises = {};
-        this.errors = {};
     }
 
     promiseFor(key, loader) {
@@ -9,18 +8,30 @@ export class LoaderCache {
         if (promise === undefined) {
             promise = loader();
             this.promises[key] = promise;
+            promise.catch(() => {
+                delete this.promises[key];
+            });
         }
         return promise;
+    }
+
+    clean(key = null) {
+        if (key) {
+            delete this.promises[key];
+        } else {
+            this.promises = {};
+        }
     }
 }
 
 export function callingComponent(arg, toAbsMid) {
     const abs = toAbsMid(".");
     const parts = abs.split("/");
-    const component = (parts[0] == "@nextgisweb"
-        ? parts[1]
-        : parts[0].replace(/^ngw-/, "")
-    ).replace(/\-/g, "_").replace(".", "");
+    const component = (
+        parts[0] === "@nextgisweb" ? parts[1] : parts[0].replace(/^ngw-/, "")
+    )
+        .replace(/-/g, "_")
+        .replace(".", "");
 
     // if (component == req) {
     //     console.debug(
@@ -29,7 +40,7 @@ export function callingComponent(arg, toAbsMid) {
     //     );
     // }
 
-    if (arg == "") {
+    if (arg === "") {
         return component;
     } else {
         return arg;
