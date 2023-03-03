0c3f8a7d4937e365455345ba7da6aff7eb495dfe
Ivan Kavaliou <info@ikovalev.com>
2023-03-03 22:16:33 +0300

    Apply map.zoomToNgwExtent to layer zooming
    CU-2c5xteg

--- nextgisweb/webmap/amd/ngw-webmap/plugin/ZoomToLayer.js
+++ nextgisweb/webmap/amd/ngw-webmap/plugin/ZoomToLayer.js
@@ -35,13 +35,7 @@ define([
             xhr.get(route.layer.extent({id: item.layerId}), {
                 handleAs: "json"
             }).then(function ({extent}) {
-                const displayExtent = ol.proj.transformExtent([
-                        extent.minLon, extent.minLat,
-                        extent.maxLon, extent.maxLat
-                    ],
-                    plugin.display.lonlatProjection,
-                    plugin.display.displayProjection);
-                plugin.display.map.zoomToExtent(displayExtent);
+                plugin.display.map.zoomToNgwExtent(extent, plugin.display.displayProjection);
             });
         }
     });
