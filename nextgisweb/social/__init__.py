48753df6ea44943c1c737cc93fd46bf04f8f194f
Aleksandr Dezhin <me@dezhin.net>
2023-05-30 05:17:37 +0300

    Move SocialComponent to component.py

--- nextgisweb/social/__init__.py
+++ nextgisweb/social/__init__.py
@@ -1,14 +1,1 @@
-from ..env import Component, require
-
-from .util import COMP_ID
-from .model import Base
-
-
-class SocialComponent(Component):
-    identity = COMP_ID
-    metadata = Base.metadata
-
-    @require('resource')
-    def setup_pyramid(self, config):
-        from . import api, view # NOQA
-        api.setup_pyramid(self, config)
+from .component import SocialComponent
