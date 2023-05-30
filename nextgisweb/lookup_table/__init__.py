2a58e0c48c962d06f58c489be38e26ec548e767f
Aleksandr Dezhin <me@dezhin.net>
2023-05-30 05:13:00 +0300

    Move LookupTableComponent to component.py

--- nextgisweb/lookup_table/__init__.py
+++ nextgisweb/lookup_table/__init__.py
@@ -1,17 +1,2 @@
-from ..env import Component
-
-from .model import Base, LookupTable
-
-__all__ = [
-    'LookupTableComponent',
-    'LookupTable',
-]
-
-
-class LookupTableComponent(Component):
-    identity = 'lookup_table'
-    metadata = Base.metadata
-
-    def setup_pyramid(self, config):
-        from . import view  # NOQA
-        view.setup_pyramid(self, config)
+from .component import LookupTableComponent
+from .model import LookupTable
