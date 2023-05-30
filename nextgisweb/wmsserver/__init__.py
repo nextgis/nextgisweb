99fe476d70f80d02ce34857f25b5e7d8f35c2e12
Aleksandr Dezhin <me@dezhin.net>
2023-05-30 05:20:21 +0300

    Move WMSServerComponent to component.py

--- nextgisweb/wmsserver/__init__.py
+++ nextgisweb/wmsserver/__init__.py
@@ -1,15 +1,2 @@
-from ..env import Component
-
-from .model import Base, Service, Layer
-
-__all__ = ['Service', 'Layer', ]
-
-
-class WMSServerComponent(Component):
-    identity = 'wmsserver'
-    metadata = Base.metadata
-
-    def setup_pyramid(self, config):
-        from . import api, view
-        api.setup_pyramid(self, config)
-        view.setup_pyramid(self, config)
+from .component import WMSServerComponent
+from .model import Service, Layer
---
+++ nextgisweb/wmsserver/component.py
@@ -0,0 +1,13 @@
+from ..env import Component
+
+from .model import Base
+
+
+class WMSServerComponent(Component):
+    identity = 'wmsserver'
+    metadata = Base.metadata
+
+    def setup_pyramid(self, config):
+        from . import api, view
+        api.setup_pyramid(self, config)
+        view.setup_pyramid(self, config)
