81cf4a779aa6d113b428f11f55de33b5f080f58c
Aleksandr Dezhin <me@dezhin.net>
2023-05-30 05:11:00 +0300

    Move FeatureDescriptionComponent to component.py

--- nextgisweb/feature_description/__init__.py
+++ nextgisweb/feature_description/__init__.py
@@ -1,14 +1,2 @@
-from ..env import Component, require
-
-from .model import Base, FeatureDescription
-
-__all__ = ['FeatureDescriptionComponent', 'FeatureDescription']
-
-
-class FeatureDescriptionComponent(Component):
-    identity = 'feature_description'
-    metadata = Base.metadata
-
-    @require('feature_layer')
-    def initialize(self):
-        from . import extension # NOQA
+from .component import FeatureDescriptionComponent
+from .model import FeatureDescription
---
+++ nextgisweb/feature_description/component.py
@@ -0,0 +1,12 @@
+from ..env import Component, require
+
+from .model import Base
+
+
+class FeatureDescriptionComponent(Component):
+    identity = 'feature_description'
+    metadata = Base.metadata
+
+    @require('feature_layer')
+    def initialize(self):
+        from . import extension # NOQA
