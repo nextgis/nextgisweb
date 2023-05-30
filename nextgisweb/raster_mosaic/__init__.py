c2d4f1c0fdf0401a2c5da29562790f36652d1744
Aleksandr Dezhin <me@dezhin.net>
2023-05-30 05:14:11 +0300

    Move RasterMosaicComponent to component.py

--- nextgisweb/raster_mosaic/__init__.py
+++ nextgisweb/raster_mosaic/__init__.py
@@ -1,16 +1,1 @@
-from ..env import Component
-
-from .model import Base
-
-
-class RasterMosaicComponent(Component):
-    identity = 'raster_mosaic'
-    metadata = Base.metadata
-
-    def setup_pyramid(self, config):
-        from . import api
-        from . import view  # NOQA: F401
-        api.setup_pyramid(self, config)
-
-    def workdir_filename(self, fobj, makedirs=False):
-        return self.env.file_storage.workdir_filename(self, fobj, makedirs)
+from .component import RasterMosaicComponent
