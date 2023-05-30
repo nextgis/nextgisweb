6ce42a903c7098df5dd4237163a9e97b9558bd50
Aleksandr Dezhin <me@dezhin.net>
2023-05-30 05:11:49 +0300

    Move GUIComponent to component.py

--- nextgisweb/gui/__init__.py
+++ nextgisweb/gui/__init__.py
@@ -1,14 +1,2 @@
-from ..env import Component
-
-from .util import COMP_ID, REACT_RENDERER
-
-
-__all__ = ['GUIComponent', 'REACT_RENDERER']
-
-
-class GUIComponent(Component):
-    identity = COMP_ID
-
-    def setup_pyramid(self, config):
-        from . import view
-        view.setup_pyramid(self, config)
+from .component import GUIComponent
+from .util import REACT_RENDERER
---
+++ nextgisweb/gui/component.py
@@ -0,0 +1,11 @@
+from ..env import Component
+
+from .util import COMP_ID
+
+
+class GUIComponent(Component):
+    identity = COMP_ID
+
+    def setup_pyramid(self, config):
+        from . import view
+        view.setup_pyramid(self, config)
