775c832458fffefa027f061cbe28166de569c081
Artemiy <rendrom@gmail.com>
2023-06-09 00:40:47 +0300

    Set loading = false by default for SaveButton

--- nextgisweb/gui/nodepkg/component/SaveButton.js
+++ nextgisweb/gui/nodepkg/component/SaveButton.js
@@ -3,14 +3,20 @@ import i18n from "@nextgisweb/pyramid/i18n!gui";
 import { Button } from "@nextgisweb/gui/antd";
 import { PropTypes } from "prop-types";
 
-export function SaveButton(props) {
+export function SaveButton({ children, loading = false, ...rest }) {
     return (
-        <Button type="primary" icon={<SaveOutlineIcon />} {...props}>
-            {props.children || i18n.gettext("Save")}
+        <Button
+            type="primary"
+            icon={<SaveOutlineIcon />}
+            loading={loading}
+            {...rest}
+        >
+            {children || i18n.gettext("Save")}
         </Button>
     );
 }
 
 SaveButton.propTypes = {
+    loading: PropTypes.boolean,
     children: PropTypes.node,
 };
