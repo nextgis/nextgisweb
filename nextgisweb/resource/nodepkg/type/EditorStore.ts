d6673a9ab2a03c5b50f175f8df52a246d9c654e7
Artemiy <rendrom@gmail.com>
2023-07-13 17:44:36 +0000

    Add editor store resource interface

---
+++ nextgisweb/resource/nodepkg/type/EditorStore.ts
@@ -0,0 +1,27 @@
+import type { LunkwillParam } from "@nextgisweb/pyramid/api/request";
+
+export type Operations = 'create' | 'update' | 'delete';
+
+interface DumpParams {
+    lunkwill: LunkwillParam;
+}
+
+export interface EditorStoreOptions {
+    composite: unknown;
+    operation: Operations;
+}
+
+export interface EditorStore {
+
+    identity: string;
+
+    uploading: boolean;
+
+    load: (value: unknown) => unknown;
+
+    dump: (val: DumpParams) => unknown;
+
+    isValid: boolean;
+
+    suggestedDisplayName?: string;
+}
