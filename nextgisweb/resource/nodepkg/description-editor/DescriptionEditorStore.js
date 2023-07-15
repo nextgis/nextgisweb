0b531c26f320d9070af82b6e621236122e04d0c7
Aleksandr Dezhin <me@dezhin.net>
2023-07-15 19:51:12 +0000

    Don't save description if it isn't modified

--- nextgisweb/resource/nodepkg/description-editor/DescriptionEditorStore.js
+++ nextgisweb/resource/nodepkg/description-editor/DescriptionEditorStore.js
@@ -4,16 +4,19 @@ export class DescriptionEditorStore {
     identity = "resource.description";
 
     value = null;
+    loaded = null;
 
     constructor() {
         makeAutoObservable(this, { identity: false });
+        this.loader = null;
     }
 
     load(value) {
-        this.value = value;
+        this.value = this.loaded = value;
     }
 
     dump() {
+        if (this.value === this.loaded) return undefined;
         return toJS(this.value);
     }
 
