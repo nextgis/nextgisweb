901a37dc2ae6109f0d30388bbcc22711700bdd13
Artemiy <rendrom@gmail.com>
2023-07-18 14:59:37 +0000

    Rewrite modified antd Table to TS

--- nextgisweb/gui/nodepkg/antd/table/index.tsx
+++ nextgisweb/gui/nodepkg/antd/table/index.tsx
@@ -3,13 +3,19 @@ import { Table as TableBase } from "antd";
 
 import "./index.less";
 
+type TableBaseProps = Parameters<typeof TableBase>[0];
+
+interface TableProps extends TableBaseProps {
+    parentHeight?: boolean;
+}
+
 export default function Table({
     className,
     parentHeight = false,
     pagination = false,
     ...props
-}) {
-    const ref = useRef();
+}: TableProps) {
+    const ref = useRef<HTMLDivElement>();
 
     useEffect(() => {
         if (!parentHeight) return;
@@ -25,20 +31,17 @@ export default function Table({
         return () => {
             resizeObserver.disconnect();
         };
-    }, []);
+    }, [parentHeight]);
 
     if (parentHeight) {
         className = (className ? className.split(" ") : [])
             .concat("ant-table-parent-height")
             .join(" ");
     }
-    return (
-        <TableBase
-            {...(parentHeight ? { ref } : {})}
-            {...{ pagination, className }}
-            {...props}
-        />
-    );
+
+    const tableProps: TableProps = { ...props, pagination, className };
+
+    return <TableBase ref={ref} {...tableProps} />;
 }
 
 Table.EXPAND_COLUMN = TableBase.EXPAND_COLUMN;
