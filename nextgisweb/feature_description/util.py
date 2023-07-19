34cbf3b5be9f89f2bcdc9e0280cd01da1112dab5
Eduard Popov <ed.popov.ru@yandex.ru>
2023-07-19 22:01:44 +0000

    Feature description export API and import util
    CU-85ztjkjxk

---
+++ nextgisweb/feature_description/util.py
@@ -0,0 +1,31 @@
+from contextlib import contextmanager
+from zipfile import BadZipFile, ZipFile
+
+from nextgisweb.env import DBSession, _
+from nextgisweb.lib.json import loadb
+
+from nextgisweb.core.exception import ValidationError
+
+from .model import FeatureDescription
+
+
+def descriptions_import(resource, filename):
+
+    @contextmanager
+    def open_zip_file():
+        try:
+            with ZipFile(filename, mode='r') as zf:
+                yield zf
+        except BadZipFile:
+            raise ValidationError(message=_("Invalid ZIP archive."))
+
+    with open_zip_file() as z:
+        metadata = loadb(z.read('metadata.json'))
+
+    with DBSession.no_autoflush:
+        for fid_str, description in metadata['items'].items():
+            FeatureDescription(
+                resource=resource,
+                feature_id=int(fid_str),
+                value=description,
+            ).persist()
