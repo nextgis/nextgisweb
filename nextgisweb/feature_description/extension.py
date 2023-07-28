4a5b4f20a881d39001fe94b613de9a16b853bb1f
Aleksandr Dezhin <me@dezhin.net>
2023-07-28 22:08:59 +0000

    Rename @nextgisweb/feature_description to @nextgisweb/feature-description

--- nextgisweb/feature_description/extension.py
+++ nextgisweb/feature_description/extension.py
@@ -8,7 +8,7 @@ from .model import FeatureDescription
 class FeatureDescriptionExtension(FeatureExtension):
     identity = 'description'
 
-    editor_widget = '@nextgisweb/feature_description/description-editor'
+    editor_widget = '@nextgisweb/feature-description/description-editor'
     display_widget = 'ngw-feature-description/DisplayWidget'
 
     def serialize(self, feature):
