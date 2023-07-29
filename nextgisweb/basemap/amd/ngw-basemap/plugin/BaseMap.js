c04a36327caaef1b270adb384470489c26fcf3c5
drnextgis <rykovd@gmail.com>
2023-07-29 21:46:42 +0000

    Remove proj4js
    CU-85ztn04k0

--- nextgisweb/basemap/amd/ngw-basemap/plugin/BaseMap.js
+++ nextgisweb/basemap/amd/ngw-basemap/plugin/BaseMap.js
@@ -4,41 +4,72 @@ define([
     "dojo/_base/lang",
     "dojo/json",
     "ngw-webmap/plugin/_PluginBase",
-    "dojo/dom-construct",
-    "@nextgisweb/pyramid/i18n!",
     "openlayers/ol",
-    "../contrib/proj4"
-], function (
-    declare,
-    array,
-    lang,
-    json,
-    _PluginBase,
-    domConstruct,
-    i18n,
-    ol,
-    proj4
-) {
-    return declare([_PluginBase], {
+], function (declare, array, lang, json, _PluginBase, ol) {
+    const a = 6378137;
+    const b = 6356752.3142;
+    const e = Math.sqrt(1 - (b * b) / (a * a));
 
+    function toEPSG3395fromEPSG4326(input, output, dimension) {
+        const length = input.length;
+        dimension = dimension > 1 ? dimension : 2;
+
+        if (output === undefined) {
+            output = dimension > 2 ? input.slice() : new Array(length);
+        }
+
+        for (let i = 0; i < length; i += dimension) {
+            output[i] = (a * (input[i] * Math.PI)) / 180;
+            const phi = (input[i + 1] * Math.PI) / 180;
+            const c = Math.pow(
+                (1 - e * Math.sin(phi)) / (1 + e * Math.sin(phi)),
+                e / 2
+            );
+            output[i + 1] = a * Math.log(Math.tan(Math.PI / 4 + phi / 2) * c);
+        }
+
+        return output;
+    }
+
+    function toEPSG3395fromEPSG3857(input, output, dimension) {
+        const transform = ol.proj.getTransform("EPSG:3857", "EPSG:4326");
+        const transformed = transform(input, output, dimension);
+        return toEPSG3395fromEPSG4326(transformed, output, dimension);
+    }
+
+    return declare([_PluginBase], {
         constructor: function () {
             var wmplugin = this.display.config.webmapPlugin[this.identity];
             var settings = this.display.clientSettings;
 
-            // Yandex.Maps
-            proj4.defs("EPSG:3395","+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs");
-            ol.proj.proj4.register(proj4);
-            ol.proj.get('EPSG:3395').setExtent([-20037508.342789244,
-                                                -20037508.342789244,
-                                                 20037508.342789244,
-                                                 20037508.342789244]);
+            ol.proj.addProjection(
+                new ol.proj.Projection({
+                    code: "EPSG:3395",
+                    units: "m",
+                    extent: [
+                        -20037508.342789244, -20037508.342789244,
+                        20037508.342789244, 20037508.342789244,
+                    ],
+                    getPointResolution: function (resolution, point) {
+                        return resolution / Math.cosh(point[1] / a);
+                    },
+                })
+            );
+
+            ol.proj.addCoordinateTransforms(
+                "EPSG:3857",
+                "EPSG:3395",
+                toEPSG3395fromEPSG3857
+            );
 
             if (wmplugin.basemaps.length) {
                 settings.basemaps = [];
 
                 array.forEach(wmplugin.basemaps, function (bm, idx) {
                     var opts = { "base": {}, "layer": {}, "source": {} },
-                        qms, copyright_text, copyright_url;
+                        qms,
+                        copyright_text,
+                        copyright_url;
 
                     opts.base.keyname = bm.keyname;
                     opts.layer.title = bm.display_name;
@@ -51,10 +82,15 @@ define([
                         qms = json.parse(bm.qms);
 
                         if (qms.epsg !== 3857 && qms.epsg !== 3395) {
-                            console.warn(lang.replace("CRS {epsg} is not supported, {name} layer.", {
-                                epsg: qms.epsg,
-                                name: bm.display_name
-                            }));
+                            console.warn(
+                                lang.replace(
+                                    "CRS {epsg} is not supported, {name} layer.",
+                                    {
+                                        epsg: qms.epsg,
+                                        name: bm.display_name,
+                                    }
+                                )
+                            );
                             return;
                         }
 
@@ -69,7 +105,11 @@ define([
                         };
 
                         if (!qms.y_origin_top) {
-                            opts.source.url = lang.replace(opts.source.url, {"x": "{x}", "y": "{-y}", "z": "{z}"});
+                            opts.source.url = lang.replace(opts.source.url, {
+                                "x": "{x}",
+                                "y": "{-y}",
+                                "z": "{z}",
+                            });
                         }
                     }
 
@@ -82,13 +122,17 @@ define([
                     if (copyright_text) {
                         opts.source.attributions = copyright_text;
                         if (copyright_url) {
-                            opts.source.attributions = '<a href="' + copyright_url + '">'
-                                + opts.source.attributions + '</a>';
+                            opts.source.attributions =
+                                '<a href="' +
+                                copyright_url +
+                                '">' +
+                                opts.source.attributions +
+                                "</a>";
                         }
                     }
 
                     opts.layer.opacity = bm.opacity ? bm.opacity : undefined;
-                    opts.layer.visible = (idx === 0) ? true : false;
+                    opts.layer.visible = idx === 0 ? true : false;
                     opts.source.crossOrigin = "anonymous";
 
                     if (bm.enabled) {
@@ -99,15 +143,15 @@ define([
                 settings.basemaps.push({
                     "base": {
                         "keyname": "blank",
-                        "mid": "ngw-webmap/ol/layer/XYZ"
+                        "mid": "ngw-webmap/ol/layer/XYZ",
                     },
                     "layer": {
                         "title": "None",
-                        "visible": settings.basemaps.length ? false : true
+                        "visible": settings.basemaps.length ? false : true,
                     },
-                    source: {}
+                    source: {},
                 });
             }
-        }
+        },
     });
 });
