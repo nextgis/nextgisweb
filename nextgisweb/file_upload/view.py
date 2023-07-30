f0f908ece1707c26c89b169219d0d0a5a4bf6f52
Aleksandr Dezhin <me@dezhin.net>
2023-07-30 02:06:11 +0000

    Remove unused file_upload test page

--- nextgisweb/file_upload/view.py
+++ nextgisweb/file_upload/view.py
@@ -1,18 +1,2 @@
-from nextgisweb.env import _
-
-from nextgisweb.pyramid import viewargs
-
-
-@viewargs(renderer='react')
-def test(request):
-    return dict(
-        title=_("File Upload testing page"),
-        entrypoint="@nextgisweb/file-upload/test",
-    )
-
-
 def setup_pyramid(comp, config):
-    config.add_route(
-        'file_upload.test',
-        '/test/file_upload'
-    ).add_view(test)
+    pass
