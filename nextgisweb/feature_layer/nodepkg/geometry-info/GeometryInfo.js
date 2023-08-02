b6b11ca8b18e2b5757e8d69c79f154897fc10aa5
Aleksandr Dezhin <me@dezhin.net>
2023-08-02 06:50:51 +0000

    Apply formatting and fix ESLint issues

--- nextgisweb/feature_layer/nodepkg/geometry-info/GeometryInfo.js
+++ nextgisweb/feature_layer/nodepkg/geometry-info/GeometryInfo.js
@@ -1,42 +1,41 @@
-import {useEffect, useState} from "react";
-import PropTypes from "prop-types";
-import {Spin} from "@nextgisweb/gui/antd";
+import { useEffect, useState } from "react";
 
-import {route} from "@nextgisweb/pyramid/api";
-import i18n from "@nextgisweb/pyramid/i18n";
+import { Spin } from "@nextgisweb/gui/antd";
+import { route } from "@nextgisweb/pyramid/api";
+import { gettext } from "@nextgisweb/pyramid/i18n";
 import webmapSettings from "@nextgisweb/pyramid/settings!webmap";
 import {
-    formatMetersLength,
+    formatCoordinatesValue,
     formatMetersArea,
-    formatCoordinatesValue
+    formatMetersLength,
 } from "@nextgisweb/webmap/utils/format-units";
-import {
-    getGeometryTypeTitle
-} from "@nextgisweb/webmap/utils/geometry-types";
+import { getGeometryTypeTitle } from "@nextgisweb/webmap/utils/geometry-types";
 
 import "./GeometryInfo.less";
 
 const loadGeometryInfo = async (layerId, featureId) => {
     const geoInfoRoute = route("feature_layer.feature.geometry_info", {
         id: layerId,
-        fid: featureId
+        fid: featureId,
     });
     const query = {
-        srs: webmapSettings.measurement_srid
+        srs: webmapSettings.measurement_srid,
     };
-    return await geoInfoRoute.get({query});
+    return await geoInfoRoute.get({ query });
 };
 
 const locale = window.dojoConfig.locale;
 const formatConfig = {
     format: "jsx",
-    locale
+    locale,
 };
 
-const formatLength = (length) => formatMetersLength(length, webmapSettings.units_length, formatConfig);
-const formatArea = (area) => formatMetersArea(area, webmapSettings.units_area, formatConfig);
+const formatLength = (length) =>
+    formatMetersLength(length, webmapSettings.units_length, formatConfig);
+const formatArea = (area) =>
+    formatMetersArea(area, webmapSettings.units_area, formatConfig);
 
-export function GeometryInfo({layerId, featureId}) {
+export function GeometryInfo({ layerId, featureId }) {
     const [status, setStatus] = useState("loading");
     const [geometryInfo, setGeometryInfo] = useState(undefined);
 
@@ -55,10 +54,8 @@ export function GeometryInfo({layerId, featureId}) {
     if (status === "loading") {
         return (
             <div className="loading">
-                <Spin/>
-                <div>
-                    {i18n.gettext("Load geometry info...")}
-                </div>
+                <Spin />
+                <div>{gettext("Load geometry info...")}</div>
             </div>
         );
     }
@@ -67,45 +64,34 @@ export function GeometryInfo({layerId, featureId}) {
         return (
             <div className="error">
                 <div>
-                    {i18n.gettext("Failed to get information about the geometry")}
+                    {gettext("Failed to get information about the geometry")}
                 </div>
             </div>
         );
     }
 
-    const length = geometryInfo.length == null ? <></> : (
+    const length = geometryInfo.length !== null && (
         <tr>
             <td>
-                {
-                    geometryInfo.type.toLowerCase().includes("polygon") ?
-                        i18n.gettext("Perimeter") : i18n.gettext("Length")
-                }
+                {geometryInfo.type.toLowerCase().includes("polygon")
+                    ? gettext("Perimeter")
+                    : gettext("Length")}
             </td>
-            <td>
-                {formatLength(geometryInfo.length)}
-            </td>
+            <td>{formatLength(geometryInfo.length)}</td>
         </tr>
     );
 
-    const area = geometryInfo.area == null ? <></> : (
+    const area = geometryInfo.area !== null && (
         <tr>
-            <td>
-                {i18n.gettext("Area")}
-            </td>
-            <td>
-                {formatArea(geometryInfo.area)}
-            </td>
+            <td>{gettext("Area")}</td>
+            <td>{formatArea(geometryInfo.area)}</td>
         </tr>
     );
-
     return (
-        <>
-            <table className="geometry-info-table">
-                <tbody>
+        <table className="geometry-info-table">
+            <tbody>
                 <tr>
-                    <td>
-                        {i18n.gettext("Geometry type")}
-                    </td>
+                    <td>{gettext("Geometry type")}</td>
                     <td>
                         {getGeometryTypeTitle(geometryInfo.type.toLowerCase())}
                     </td>
@@ -113,44 +99,22 @@ export function GeometryInfo({layerId, featureId}) {
                 {length}
                 {area}
                 <tr>
-                    <td>
-                        {i18n.gettext("Extent (xMin)")}
-                    </td>
-                    <td>
-                        {formatCoordinatesValue(geometryInfo.extent.minX)}
-                    </td>
+                    <td>{gettext("Extent (xMin)")}</td>
+                    <td>{formatCoordinatesValue(geometryInfo.extent.minX)}</td>
                 </tr>
                 <tr>
-                    <td>
-                        {i18n.gettext("Extent (yMin)")}
-                    </td>
-                    <td>
-                        {formatCoordinatesValue(geometryInfo.extent.minY)}
-                    </td>
+                    <td>{gettext("Extent (yMin)")}</td>
+                    <td>{formatCoordinatesValue(geometryInfo.extent.minY)}</td>
                 </tr>
                 <tr>
-                    <td>
-                        {i18n.gettext("Extent (xMax)")}
-                    </td>
-                    <td>
-                        {formatCoordinatesValue(geometryInfo.extent.maxX)}
-                    </td>
+                    <td>{gettext("Extent (xMax)")}</td>
+                    <td>{formatCoordinatesValue(geometryInfo.extent.maxX)}</td>
                 </tr>
                 <tr>
-                    <td>
-                        {i18n.gettext("Extent (yMax)")}
-                    </td>
-                    <td>
-                        {formatCoordinatesValue(geometryInfo.extent.maxY)}
-                    </td>
+                    <td>{gettext("Extent (yMax)")}</td>
+                    <td>{formatCoordinatesValue(geometryInfo.extent.maxY)}</td>
                 </tr>
-                </tbody>
-            </table>
-        </>
+            </tbody>
+        </table>
     );
 }
-
-GeometryInfo.propTypes = {
-    layerId: PropTypes.number,
-    featureId: PropTypes.number
-};
