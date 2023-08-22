cfd1cd8019b477978313eae8f514f08862fc1db4
Aleksandr Dezhin <me@dezhin.net>
2023-08-22 19:07:47 +0000

    Configure antd design token
    CU-85ztgewyn

--- nextgisweb/gui/nodepkg/antd/config-provider/index.tsx
+++ nextgisweb/gui/nodepkg/antd/config-provider/index.tsx
@@ -5,8 +5,26 @@ import type { ParamsOf } from "../../type";
 
 type Props = ParamsOf<typeof Base>;
 
+const computed = window.getComputedStyle(document.body);
+const cvar = (name: string): string => computed.getPropertyValue("--" + name);
+
+const token: Props["theme"]["token"] = {
+    borderRadius: 4,
+    colorError: cvar("error"),
+    colorInfo: cvar("primary"),
+    colorPrimary: cvar("primary"),
+    colorSuccess: cvar("success"),
+    colorWarning: cvar("warning"),
+    motion: false,
+};
+
+const components: Props["theme"]["components"] = {
+    Modal: { titleFontSize: 20 },
+};
+
 const defaults: Props = {
     locale: antd,
+    theme: { token, components },
 };
 
 export default function ConfigProvider(props: Props) {
