a7d6dfe15ffb52e2e372298f8dfad54487b3c9de
Aleksandr Dezhin <me@dezhin.net>
2023-09-17 01:59:03 +0000

    Add black and move ruff config to pyproject.toml

--- setup.py
+++ setup.py
@@ -8,88 +8,88 @@ from setuptools.command.install import install
 from stat import S_IXGRP, S_IXOTH, S_IXUSR
 from subprocess import CalledProcessError, check_output
 
-with io.open('VERSION', 'r') as fd:
+with io.open("VERSION", "r") as fd:
     VERSION = fd.read().rstrip()
 
 try:
-    gv = check_output(['gdal-config', '--version'], universal_newlines=True).strip()
+    gv = check_output(["gdal-config", "--version"], universal_newlines=True).strip()
 except CalledProcessError:
     gv = None
 
 requires = [
-    'affine==2.4.0',
-    'attrs==23.1.0',
-    'babel==2.12.1',
-    'cachetools==5.3.1',
-    'docstring-parser==0.15',
-    'geoalchemy2==0.14.1',
-    'httpx[http2]==0.24.1',
-    'humanize==4.6.0',
+    "affine==2.4.0",
+    "attrs==23.1.0",
+    "babel==2.12.1",
+    "cachetools==5.3.1",
+    "docstring-parser==0.15",
+    "geoalchemy2==0.14.1",
+    "httpx[http2]==0.24.1",
+    "humanize==4.6.0",
     'importlib_metadata; python_version<"3.10"',
-    'lxml==4.9.3',
-    'msgspec==0.16.0',
-    'numpy',
-    'networkx',
-    'orjson==3.9.2',
-    'OWSLib==0.29.2',
-    'passlib==1.7.4',
-    'pillow==9.5.0',
-    'poeditor',
-    'psutil==5.9.5',
-    'psycopg2==2.9.6',
-    'pygdal' + (f'=={gv}.*,' if gv else '') + '>=3',
-    'pyproj==3.5.0',
-    'pyramid==2.0.1',
-    'pyramid-mako==1.1.0',
-    'pyramid-tm==2.5',
-    'python-magic==0.4.27',
-    'python-ulid==1.1.0',
-    'requests==2.31.0',
-    'sentry-sdk==1.9.10',
-    'shapely==2.0.1',
-    'SQLAlchemy==1.4.49',
-    'transaction==3.1.0',
-    'typing-extensions',
-    'ua-parser==0.18.0',
-    'urllib3<2',
-    'waitress==2.1.2',
-    'zipstream-new==1.1.8',
-    'zope.sqlalchemy==3.0',
-    'zope.interface==6.0',
-    'zope.event==5.0',
+    "lxml==4.9.3",
+    "msgspec==0.16.0",
+    "numpy",
+    "networkx",
+    "orjson==3.9.2",
+    "OWSLib==0.29.2",
+    "passlib==1.7.4",
+    "pillow==9.5.0",
+    "poeditor",
+    "psutil==5.9.5",
+    "psycopg2==2.9.6",
+    "pygdal" + (f"=={gv}.*," if gv else "") + ">=3",
+    "pyproj==3.5.0",
+    "pyramid==2.0.1",
+    "pyramid-mako==1.1.0",
+    "pyramid-tm==2.5",
+    "python-magic==0.4.27",
+    "python-ulid==1.1.0",
+    "requests==2.31.0",
+    "sentry-sdk==1.9.10",
+    "shapely==2.0.1",
+    "SQLAlchemy==1.4.49",
+    "transaction==3.1.0",
+    "typing-extensions",
+    "ua-parser==0.18.0",
+    "urllib3<2",
+    "waitress==2.1.2",
+    "zipstream-new==1.1.8",
+    "zope.sqlalchemy==3.0",
+    "zope.interface==6.0",
+    "zope.event==5.0",
 ]
 
-extras_require = dict(development=[
-    'coverage',
-    'flake8-future-import',
-    'flake8',
-    'freezegun',
-    'pyramid-debugtoolbar==4.10',
-    'pytest-flake8',
-    'pytest-watch',
-    'pytest==7.3.*',
-    'ruff',
-    'webtest',
-])
+extras_require = dict(
+    development=[
+        "black",
+        "coverage",
+        "flake8-future-import",
+        "flake8",
+        "freezegun",
+        "pyramid-debugtoolbar==4.10",
+        "pytest-flake8",
+        "pytest-watch",
+        "pytest==7.3.*",
+        "ruff",
+        "webtest",
+    ]
+)
 
 entry_points = {
-    'paste.app_factory': [
-        'main = nextgisweb:main'
+    "paste.app_factory": ["main = nextgisweb:main"],
+    "babel.extractors": [
+        "hbs = nextgisweb.i18n.hbs:extract",
     ],
-
-    'babel.extractors': [
-        'hbs = nextgisweb.i18n.hbs:extract',
+    "pytest11": [
+        "nextgisweb = nextgisweb.pytest",
+        "nextgisweb.core = nextgisweb.core.test",
+        "nextgisweb.pyramid = nextgisweb.pyramid.test",
+        "nextgiswev.auth = nextgisweb.auth.test",
+        "nextgiswev.resource = nextgisweb.resource.test",
     ],
-
-    'pytest11': [
-        'nextgisweb = nextgisweb.pytest',
-        'nextgisweb.core = nextgisweb.core.test',
-        'nextgisweb.pyramid = nextgisweb.pyramid.test',
-        'nextgiswev.auth = nextgisweb.auth.test',
-        'nextgiswev.resource = nextgisweb.resource.test',
+    "nextgisweb.packages": [
+        "nextgisweb = nextgisweb:pkginfo",
     ],
-
-    'nextgisweb.packages': ['nextgisweb = nextgisweb:pkginfo', ],
 }
 
 
@@ -102,12 +102,12 @@ class DevelopCommand(develop):
 
         bin_dir, _ = os.path.split(sys.executable)
         for name, module, func in (
-            ('nextgisweb', 'nextgisweb.script', 'main'),
-            ('nextgisweb-config', 'nextgisweb.script', 'config'),
-            ('nextgisweb-i18n', 'nextgisweb.i18n.script', 'main'),
+            ("nextgisweb", "nextgisweb.script", "main"),
+            ("nextgisweb-config", "nextgisweb.script", "config"),
+            ("nextgisweb-i18n", "nextgisweb.i18n.script", "main"),
         ):
             sf = os.path.join(bin_dir, name)
-            with open(sf, 'w') as fd:
+            with open(sf, "w") as fd:
                 fd.write("#!{}\n".format(sys.executable))
                 fd.write("from {} import {} as main\n".format(module, func))
                 fd.write("if __name__ == '__main__': main()\n")
@@ -118,19 +118,16 @@ class DevelopCommand(develop):
 
 class InstallCommand(install):
     def run(self):
-        raise RuntimeError(
-            "Only development mode installation "
-            "(pip install -e ...) is supported!")
-        install.run(self)
+        raise RuntimeError("Only development mode installation (pip install -e ...) is supported!")
 
 
 setup(
-    name='nextgisweb',
+    name="nextgisweb",
     version=VERSION,
-    description='NextGIS Web main package',
-    author='NextGIS',
-    author_email='info@nextgis.com',
-    url='https://nextgis.com/nextgis-web',
+    description="NextGIS Web main package",
+    author="NextGIS",
+    author_email="info@nextgis.com",
+    url="https://nextgis.com/nextgis-web",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
@@ -141,5 +138,5 @@ setup(
     cmdclass=dict(
         develop=DevelopCommand,
         install=InstallCommand,
-    )
+    ),
 )
