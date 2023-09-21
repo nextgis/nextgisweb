28ff066143b0241fcf499ea659dcc3c2ac6569bb
Eduard Popov <ed.popov.ru@yandex.ru>
2023-09-21 18:03:57 +1000

    Strip more NTFS filename cases

--- nextgisweb/lib/fs/__init__.py
+++ nextgisweb/lib/fs/__init__.py
@@ -1,5 +1,11 @@
 import re
 
+reserved = re.compile(r'^(aux|con[1-9]|lpt[1-9]|con|nul|prn)$')
+
 
 def filename_strip(name):
-    return re.sub(r'[\\/:*?"<>|]', '', name)[:255]
+    name = name.rstrip('. ')
+    name = re.sub(r'[\\/:*?"<>|]', '', name)
+    if reserved.match(name):
+        return name + '_'
+    return name[:255]
--- nextgisweb/lib/fs/test/test_strip.py
+++ nextgisweb/lib/fs/test/test_strip.py
@@ -4,11 +4,13 @@ from .. import filename_strip
 
 
 @pytest.mark.parametrize('name, filename', (
-    ('name', 'name',),
-    ('!name?', '!name',),
-    ('***', '',),
-    ('\\n:a|m"\'e/', 'nam\'e',),
-    ('ชื่อ', 'ชื่อ',),
+    ('name', 'name'),
+    ('!name?', '!name'),
+    ('***', ''),
+    ('\\n:a|m"\'e/', 'nam\'e'),
+    ('ชื่อ', 'ชื่อ'),
+    ('name. . .', 'name'),
+    ('lpt3', 'lpt3_'),
 ))
 def test_strip(name, filename):
     assert filename_strip(name) == filename
