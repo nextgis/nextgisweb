de025a18622e8e5e1d11dfa50da789bdb91ad2b5
Aleksandr Dezhin <me@dezhin.net>
2023-11-04 17:21:51 +0000

    Extract attachment metadata on maintenance
    CU-862kbdkmn

--- nextgisweb/feature_attachment/component.py
+++ nextgisweb/feature_attachment/component.py
@@ -1,3 +1,5 @@
+import transaction
+
 from nextgisweb.env import Component, _, require
 
 from nextgisweb.core import KindOfData
@@ -6,20 +8,25 @@ from .model import FeatureAttachment
 
 
 class FeatureAttachmentData(KindOfData):
-    identity = 'feature_attachment'
+    identity = "feature_attachment"
     display_name = _("Feature attachments")
 
 
 class FeatureAttachmentComponent(Component):
-
-    @require('feature_layer')
+    @require("feature_layer")
     def initialize(self):
         from . import extension  # noqa: F401
 
     def setup_pyramid(self, config):
         from . import api, view
+
         api.setup_pyramid(self, config)
         view.setup_pyramid(self, config)
+
+    def maintenance(self):
+        with transaction.manager:
+            for obj in FeatureAttachment.filter_by(file_meta=None):
+                obj.extract_meta()
 
     def estimate_storage(self):
         for obj in FeatureAttachment.query():
