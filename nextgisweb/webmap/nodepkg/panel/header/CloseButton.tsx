c9a89ddbaa1823ce806253cd5d079e76ff20b55e
Artemiy <rendrom@gmail.com>
2023-11-20 02:17:24 +0000

    Rewrite CloseButton to TypeScript

--- nextgisweb/webmap/nodepkg/panel/header/CloseButton.tsx
+++ nextgisweb/webmap/nodepkg/panel/header/CloseButton.tsx
@@ -1,17 +1,23 @@
 import { Button } from "@nextgisweb/gui/antd";
+import type { ButtonProps } from "@nextgisweb/gui/antd";
 
 import CloseIcon from "@nextgisweb/icon/material/close";
-
 import "./CloseButton.less";
 
-export function CloseButton({ close, ...props }) {
+interface CloseButtonProps extends ButtonProps {
+    close?: () => void;
+}
+
+export function CloseButton({ close, ...props }: CloseButtonProps) {
     return (
         <Button
             className="ngw-webmap-close-button"
             shape="circle"
             type="text"
             icon={<CloseIcon />}
-            onClick={() => close()}
+            onClick={() => {
+                if (close) close();
+            }}
             {...props}
         />
     );
