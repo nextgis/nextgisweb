3f5146d09283287a26c32302c75e64643d496997
Artemiy <rendrom@gmail.com>
2023-11-22 18:47:46 +0000

    Refactor lookup table utils

---
+++ nextgisweb/lookup_table/nodepkg/util/getLookupTableItems.ts
@@ -0,0 +1,30 @@
+import { route } from "@nextgisweb/pyramid/api";
+import type { RequestOptions } from "@nextgisweb/pyramid/api/type";
+import type { ResourceItem } from "@nextgisweb/resource/type/Resource";
+
+const EXCLUDED_IDS: number[] = [];
+
+export async function getLookupTableItems(
+    lookupId: number,
+    requestOptions?: RequestOptions
+): Promise<Record<string, string>> {
+    if (!EXCLUDED_IDS.includes(lookupId)) {
+        try {
+            const resourceItem = await route(
+                "resource.item",
+                lookupId
+            ).get<ResourceItem>({
+                cache: true,
+                ...requestOptions,
+            });
+            if (!resourceItem.lookup_table) {
+                throw new Error(`Resource ${lookupId} is not lookup table`);
+            }
+            return resourceItem.lookup_table.items;
+        } catch (er) {
+            EXCLUDED_IDS.push(lookupId);
+            throw er;
+        }
+    }
+    throw new Error(`Lookupp table ${lookupId} not founded`);
+}
