bdc56eabcd1b42278572dd47c593c00141d6930c
Artemiy <rendrom@gmail.com>
2023-11-23 22:58:45 +0000

    Add raster layer resource global type

---
+++ nextgisweb/raster_layer/nodepkg/type/RasterlayerResource.ts
@@ -0,0 +1,14 @@
+
+interface Srs {
+    id: number;
+}
+
+export interface RasterlayerResource {
+    srs: Srs;
+    xsize: number;
+    ysize: number;
+    band_count: number;
+    color_interpretation: string[];
+    cog: boolean;
+}
+
