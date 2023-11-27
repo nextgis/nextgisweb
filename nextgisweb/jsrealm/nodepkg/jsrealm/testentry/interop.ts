fb2f6b1f8e13147429e6bf830a5f43109148458d
Artemiy <rendrom@gmail.com>
2023-11-27 08:48:53 +0000

    Fix interop testentry types

--- nextgisweb/jsrealm/nodepkg/jsrealm/testentry/interop.ts
+++ nextgisweb/jsrealm/nodepkg/jsrealm/testentry/interop.ts
@@ -1,19 +1,20 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 /** @testentry mocha */
-// @ts-ignore
 import Deferred from "dojo/Deferred";
-// @ts-ignore
 import { publish, subscribe } from "dojo/topic";
 
 describe("Interoperability with Dojo", () => {
     it("dojo/Deferred", (done) => {
         const deferred = new Deferred();
         deferred.then(() => done());
-        setTimeout(() => deferred.resolve(), 50);
+        setTimeout(() => {
+            deferred.resolve(undefined);
+        }, 50);
     });
 
     it("dojo/topic", (done) => {
         subscribe("test", () => done());
-        setTimeout(() => publish("test"), 50);
+        setTimeout(() => {
+            publish("test", {});
+        }, 50);
     });
 });
