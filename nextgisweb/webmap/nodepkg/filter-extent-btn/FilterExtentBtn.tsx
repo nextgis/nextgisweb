8b47a0d1f749277d754ec885612ef286a7b261c1
Artemiy <rendrom@gmail.com>
2023-12-19 22:23:47 +0000

    Refactor filter-extent-btn to strict TS

--- nextgisweb/webmap/nodepkg/filter-extent-btn/FilterExtentBtn.tsx
+++ nextgisweb/webmap/nodepkg/filter-extent-btn/FilterExtentBtn.tsx
@@ -3,6 +3,7 @@ import { WKT } from "ol/format";
 import type { Circle, Geometry } from "ol/geom";
 import { fromCircle } from "ol/geom/Polygon";
 import Draw, { createBox } from "ol/interaction/Draw";
+import type { Options as DrawOptions } from "ol/interaction/Draw";
 import type { DrawEvent } from "ol/interaction/Draw";
 import { Vector as VectorLayer } from "ol/layer";
 import type { Vector as OlVectorLayer } from "ol/layer";
@@ -16,7 +17,8 @@ import { Button, Dropdown, Space } from "@nextgisweb/gui/antd";
 import type { MenuProps, SizeType } from "@nextgisweb/gui/antd";
 import { gettext } from "@nextgisweb/pyramid/i18n";
 
-import type { DojoDisplay, MapStateControl } from "../type";
+import type { DojoDisplay } from "../type";
+import type { MapStateControl } from "../type/MapState";
 
 import CloseIcon from "@nextgisweb/icon/material/close/outline";
 import CropFreeIcon from "@nextgisweb/icon/material/crop_free/outline";
@@ -29,18 +31,14 @@ import VisibilityIcon from "@nextgisweb/icon/material/visibility/outline";
 import VisibilityOffIcon from "@nextgisweb/icon/material/visibility_off/outline";
 import ZoomInIcon from "@nextgisweb/icon/material/zoom_in/outline";
 
-interface FilterExtentBtnProps {
+export interface FilterExtentBtnProps {
     id: number;
     display: DojoDisplay;
     size?: SizeType;
-    onGeomChange?: (geom: Geometry, geomWKT: string) => void;
+    onGeomChange?: (geom?: Geometry, geomWKT?: string) => void;
 }
 
-enum FilterExtentBtnMode {
-    "default",
-    "draw",
-    "geometry",
-}
+type FilterExtentBtnMode = "default" | "draw" | "geometry";
 
 const formatWKT = new WKT();
 
@@ -109,7 +107,7 @@ const geomTypesOptions = geomTypesInfo.map(({ icon, key, label }) => {
     return { icon, key, label };
 });
 
-const geomActionOptions = (visible) => [
+const geomActionOptions = (visible: boolean) => [
     {
         label: visible ? gettext("Hide") : gettext("Show"),
         key: "visible",
@@ -132,11 +130,10 @@ const msgChangeGeom = gettext("Change filtering geometry");
 const msgStopDrawing = gettext("Stop drawing...");
 
 interface InteractionInfo {
-    interaction: Draw;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    layer: OlVectorLayer<any>;
+    interaction?: Draw;
+    layer: OlVectorLayer<OlVectorSource>;
     source: OlVectorSource;
-    mapStateKey: string;
+    mapStateKey?: string;
     control: MapStateControl;
 }
 
@@ -144,9 +141,9 @@ const buildInteraction = (
     display: DojoDisplay,
     layerId: number,
     geomType: string,
-    onDrawEnd: (event: DrawEvent) => void,
-    onTerminate: () => void
-): InteractionInfo => {
+    onDrawEnd?: (event: DrawEvent) => void,
+    onTerminate?: () => void
+): InteractionInfo | undefined => {
     if (!display || !geomType) return;
 
     const geomTypeInfo = geomTypesMap.get(geomType);
@@ -166,8 +163,7 @@ const buildInteraction = (
     olMap.addLayer(vectorLayer);
     vectorLayer.setZIndex(1000);
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const drawOptions: any = {
+    const drawOptions: DrawOptions = {
         source,
         type: geomTypeInfo.geomType,
         stopClick: true,
@@ -183,9 +179,7 @@ const buildInteraction = (
     olMap.addInteraction(drawInteraction);
     drawInteraction.on("drawend", (e) => {
         if (onDrawEnd) {
-            setTimeout(() => {
-                onDrawEnd(e);
-            });
+            onDrawEnd(e);
         }
     });
 
@@ -212,20 +206,24 @@ const buildInteraction = (
 const clearDrawInteraction = (
     display: DojoDisplay,
     interactionInfo: InteractionInfo
-): InteractionInfo => {
+): InteractionInfo | undefined => {
     if (!display || !interactionInfo) return;
 
-    const olMap = display.map.olMap;
-    olMap.removeInteraction(interactionInfo.interaction);
+    if (interactionInfo.interaction) {
+        const olMap = display.map.olMap;
+        olMap.removeInteraction(interactionInfo.interaction);
+    }
 
     const { mapStates } = display;
     const { mapStateKey } = interactionInfo;
-    if (mapStates.getActiveState() === mapStateKey) {
-        display.mapStates.deactivateState(mapStateKey);
+    if (mapStateKey) {
+        if (mapStates.getActiveState() === mapStateKey) {
+            display.mapStates.deactivateState(mapStateKey);
+        }
+        display.mapStates.removeState(mapStateKey);
     }
-    display.mapStates.removeState(mapStateKey);
 
-    const newInteractionInfo = {
+    const newInteractionInfo: InteractionInfo = {
         ...interactionInfo,
         ...{
             interaction: undefined,
@@ -235,106 +233,77 @@ const clearDrawInteraction = (
     return newInteractionInfo;
 };
 
-const clearInteraction = (
-    display: DojoDisplay,
-    interactionInfo: InteractionInfo
-): void => {
-    if (!display || !interactionInfo) return;
-
-    const olMap = display.map.olMap;
-    clearDrawInteraction(display, interactionInfo);
-    olMap.removeLayer(interactionInfo.layer);
-};
-
-const useIsMounted = () => {
-    const isMounted = useRef(false);
-    useEffect(() => {
-        isMounted.current = true;
-        return () => {
-            isMounted.current = false;
-        };
-    }, []);
-    return () => isMounted.current;
-};
-
 export const FilterExtentBtn = ({
     id,
     display,
     size = "middle",
     onGeomChange,
 }: FilterExtentBtnProps) => {
-    const mounted = useIsMounted();
-    const [mode, setMode] = useState<FilterExtentBtnMode>(
-        FilterExtentBtnMode.default
-    );
-    const [geomType, setGeomType] = useState<string>(undefined);
-    const [interactionInfo, setInteractionInfo] =
-        useState<InteractionInfo>(undefined);
+    const interactionInfo = useRef<InteractionInfo>();
+    const [mode, setMode] = useState<FilterExtentBtnMode>("default");
+    const [geomType, setGeomType] = useState<string>();
     const [visibleGeom, setVisibleGeom] = useState<boolean>(true);
-    const [drawEnd, setDrawEnd] = useState<DrawEvent>(undefined);
-    const [externalTerminate, setExternalTerminate] = useState<boolean>(false);
+    const [drawEnd, setDrawEnd] = useState<DrawEvent>();
 
-    const clearGeometry = () => {
-        if (!interactionInfo) return;
-        clearInteraction(display, interactionInfo);
-        setInteractionInfo(undefined);
+    const clearInteraction = useCallback(() => {
+        const info = interactionInfo.current;
+        interactionInfo.current = undefined;
+        if (!display || !info) return;
+
+        const olMap = display.map.olMap;
+        clearDrawInteraction(display, info);
+        olMap.removeLayer(info.layer);
+    }, [display]);
+
+    const clearGeometry = useCallback(() => {
+        if (!interactionInfo.current) return;
+        clearInteraction();
         setDrawEnd(undefined);
         setGeomType(undefined);
-        setMode(FilterExtentBtnMode.default);
-        onGeomChange(undefined, undefined);
-    };
+        setMode("default");
+        if (onGeomChange) {
+            onGeomChange(undefined, undefined);
+        }
+    }, [clearInteraction, onGeomChange]);
 
     useEffect(() => {
         return () => {
-            if (!mounted()) {
-                if (!interactionInfo) return;
-                clearInteraction(display, interactionInfo);
-            }
+            clearInteraction();
         };
-    }, [mounted]);
+    }, [clearInteraction]);
 
     useEffect(() => {
         if (!drawEnd) return;
 
         const { feature } = drawEnd;
         let geometry = feature.getGeometry();
-        if (geometry.getType() === "Circle") {
-            geometry = fromCircle(geometry as Circle);
+        if (geometry) {
+            if (geometry.getType() === "Circle") {
+                geometry = fromCircle(geometry as Circle);
+            }
+            const geometryWKT = formatWKT.writeGeometry(geometry);
+            if (onGeomChange) {
+                onGeomChange(geometry, geometryWKT);
+            }
         }
-        const geometryWKT = formatWKT.writeGeometry(geometry);
-        onGeomChange(geometry, geometryWKT);
-        const newInteractionInfo = clearDrawInteraction(
-            display,
-            interactionInfo
-        );
-        setInteractionInfo(newInteractionInfo);
-        setMode(FilterExtentBtnMode.geometry);
+        if (interactionInfo.current) {
+            const newInteractionInfo = clearDrawInteraction(
+                display,
+                interactionInfo.current
+            );
+            interactionInfo.current = newInteractionInfo;
+        }
+        setMode("geometry");
         setDrawEnd(undefined);
-    }, [drawEnd]);
-
-    useEffect(() => {
-        if (!interactionInfo || mode !== FilterExtentBtnMode.draw) {
-            return;
-        }
-        setMode(FilterExtentBtnMode.default);
-        setTimeout(() => {
-            clearGeometry();
-        });
-    }, [externalTerminate]);
+    }, [display, drawEnd, onGeomChange]);
 
     const geomTypesMenuItems: MenuProps = {
         items: geomTypesOptions,
         onClick: (item) => {
             setGeomType(item.key);
-            setMode(FilterExtentBtnMode.draw);
-            const info = buildInteraction(
-                display,
-                id,
-                item.key,
-                (e) => setDrawEnd(e),
-                () => setExternalTerminate(!externalTerminate)
-            );
-            setInteractionInfo(info);
+            setMode("draw");
+            const info = buildInteraction(display, id, item.key, setDrawEnd);
+            interactionInfo.current = info;
         },
     };
 
@@ -349,37 +318,47 @@ export const FilterExtentBtn = ({
     );
 
     const changeVisibleGeom = (v: boolean) => {
-        const { layer } = interactionInfo;
-        layer.setVisible(v);
-        setVisibleGeom(v);
-    };
-
-    const zoomToGeom = () => {
-        const { source } = interactionInfo;
-        const map = display.map.olMap;
-        map.getView().fit(source.getExtent());
-    };
-
-    const handleGeomAction = (key: string) => {
-        switch (key) {
-            case "visible":
-                changeVisibleGeom(!visibleGeom);
-                break;
-            case "zoom":
-                zoomToGeom();
-                break;
-            case "clear":
-                clearGeometry();
-                break;
+        const layer = interactionInfo.current?.layer;
+        if (layer) {
+            layer.setVisible(v);
+            setVisibleGeom(v);
         }
     };
 
-    const geomActionProps = (visible) => ({
-        items: geomActionOptions(visible),
-        onClick: (item) => {
-            handleGeomAction(item.key);
+    const zoomToGeom = useCallback(() => {
+        const source = interactionInfo.current?.source;
+        if (source) {
+            const map = display.map.olMap;
+            map.getView().fit(source.getExtent());
+        }
+    }, [display]);
+
+    const handleGeomAction = useCallback(
+        (key: string) => {
+            switch (key) {
+                case "visible":
+                    changeVisibleGeom(!visibleGeom);
+                    break;
+                case "zoom":
+                    zoomToGeom();
+                    break;
+                case "clear":
+                    clearGeometry();
+                    break;
+            }
         },
-    });
+        [clearGeometry, visibleGeom, zoomToGeom]
+    );
+
+    const geomActionProps = useCallback(
+        (visible: boolean): MenuProps => ({
+            items: geomActionOptions(visible),
+            onClick: (item) => {
+                handleGeomAction(item.key);
+            },
+        }),
+        [handleGeomAction]
+    );
 
     const buildGeomSection = useCallback(() => {
         if (!geomType) return undefined;
@@ -393,7 +372,7 @@ export const FilterExtentBtn = ({
                 </Button>
             </Dropdown>
         );
-    }, [size, geomType, visibleGeom]);
+    }, [geomType, geomActionProps, visibleGeom, size]);
 
     const buildDrawSection = () => {
         return (
@@ -412,11 +391,11 @@ export const FilterExtentBtn = ({
     };
 
     let result;
-    if (mode === FilterExtentBtnMode.default) {
+    if (mode === "default") {
         result = buildDropdown();
-    } else if (mode === FilterExtentBtnMode.draw) {
+    } else if (mode === "draw") {
         result = buildDrawSection();
-    } else if (mode === FilterExtentBtnMode.geometry) {
+    } else if (mode === "geometry") {
         result = buildGeomSection();
     }
 
