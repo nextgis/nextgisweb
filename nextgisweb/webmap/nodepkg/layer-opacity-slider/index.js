68d36c316c0873a3b5dc320ff2c2adfa4f959c3e
Artemiy <rendrom@gmail.com>
2023-12-21 09:06:10 +0000

    Fix webmap layers tree dropdown render action

--- nextgisweb/webmap/nodepkg/layer-opacity-slider/index.js
+++
@@ -1,4 +0,0 @@
-/** @entrypoint */
-import { LayerOpacitySlider } from "./LayerOpacitySlider";
-
-export default LayerOpacitySlider;
--- nextgisweb/webmap/nodepkg/layers-tree/DropdownActions.tsx
+++ nextgisweb/webmap/nodepkg/layers-tree/DropdownActions.tsx
@@ -82,7 +82,7 @@ export function DropdownActions({
                     label: title,
                 });
             } else if (render) {
-                const RenderedPlugin = () => render(pluginInfo);
+                const RenderedPlugin = () => render.call(plugin, pluginInfo);
                 customMenuItems.push(<RenderedPlugin key={keyPlugin} />);
             }
         }
