64124ac2b8d5f4daf6db2f080e714015272cfdef
Artemiy <rendrom@gmail.com>
2024-01-26 22:40:58 +0000

    Rewrite CopyToClipboardButton to TS

--- nextgisweb/gui/nodepkg/buttons/CopyToClipboardButton.tsx
+++ nextgisweb/gui/nodepkg/buttons/CopyToClipboardButton.tsx
@@ -1,30 +1,40 @@
 import { Button, message } from "@nextgisweb/gui/antd";
+import type { ButtonProps } from "@nextgisweb/gui/antd";
 import { gettext } from "@nextgisweb/pyramid/i18n";
 
 import ContentCopyIcon from "@nextgisweb/icon/material/content_copy";
+
+interface CopyToClipboardButtonProps extends ButtonProps {
+    children?: React.ReactNode;
+    iconOnly?: boolean;
+    messageInfo?: string;
+    getTextToCopy: () => string;
+}
 
 export function CopyToClipboardButton({
     children,
     messageInfo,
     getTextToCopy,
-    type,
     iconOnly,
-}) {
+    ...restParams
+}: CopyToClipboardButtonProps) {
     const copyToClipboard = async () => {
         await navigator.clipboard.writeText(getTextToCopy());
         message.info(messageInfo || gettext("Copied to clipboard"));
     };
 
-    let buttonContent = null;
+    let buttonContent: React.ReactNode | null = null;
     if (!iconOnly) {
         buttonContent = children || gettext("Copy to clipboard");
     }
 
     return (
         <Button
-            type={type}
             icon={<ContentCopyIcon />}
-            onClick={() => copyToClipboard()}
+            onClick={() => {
+                copyToClipboard();
+            }}
+            {...restParams}
         >
             {buttonContent}
         </Button>
