d311869ba6d9154e84bb020de57295b861cf71e5
Artemiy <rendrom@gmail.com>
2024-01-26 22:40:58 +0000

    Rewrite EditorWidget to TS

--- nextgisweb/resource/nodepkg/editor-widget/EditorStore.ts
+++ nextgisweb/resource/nodepkg/editor-widget/EditorStore.ts
@@ -1,26 +1,57 @@
 import { makeAutoObservable, toJS } from "mobx";
 
+import type { EditorStoreOptions, Operation, Resource } from "../type";
+import type { Composite } from "../type/Composite";
+
+type NullableProperties<T> = {
+    [P in keyof T]?: T[P] | null;
+};
+
+type Value = NullableProperties<Omit<Resource, "parent" | "owner_user">> & {
+    parent?: { id: number | null };
+    owner_user?: { id: number | null };
+};
+
+interface Loaded {
+    ownerUser: number | null;
+    displayName: string | null;
+    keyname: string | null;
+    parent: number | null;
+}
+
 export class EditorStore {
     identity = "resource";
 
-    cls = null;
-    displayName = null;
-    keyname = null;
-    parent = null;
-    ownerUser = null;
+    cls: string | null = null;
+    displayName: string | null = null;
+    keyname: string | null = null;
+    parent: number | null = null;
+    ownerUser: number | null = null;
 
     sdnBase = null;
     sdnDynamic = null;
 
-    constructor({ composite, operation }) {
+    operation?: Operation;
+    composite?: Composite;
+
+    _loaded: Loaded;
+
+    constructor({ composite, operation }: EditorStoreOptions) {
         makeAutoObservable(this, { identity: false });
         this.composite = composite;
         this.operation = operation;
 
-        this._loaded = {};
+        const ownerUser = this.composite.owner_user;
+        this._loaded = {
+            ownerUser,
+            displayName: null,
+            keyname: null,
+            parent: null,
+        };
+        this.ownerUser = ownerUser;
+
         this.cls = this.composite.cls;
         this.parent = this.composite.parent;
-        this.ownerUser = this._loaded.ownerUser = this.composite.owner_user;
 
         this.sdnBase = composite.sdnBase;
         this.composite.watch("sdnDynamic", (attr, oldVal, newVal) => {
@@ -28,17 +59,25 @@ export class EditorStore {
         });
     }
 
-    load(value) {
-        const loaded = {};
-        this.displayName = loaded.displayName = value.display_name;
-        this.keyname = loaded.keyname = value.keyname;
-        this.parent = loaded.parent = value.parent ? value.parent.id : null;
-        this.ownerUser = loaded.ownerUser = value.owner_user.id;
+    load(value: Value) {
+        const loaded: Loaded = {
+            displayName: value.display_name || null,
+            keyname: value.keyname || null,
+            parent: value.parent ? value.parent.id : null,
+            ownerUser: value.owner_user ? value.owner_user.id : null,
+        };
+
+        this.displayName = loaded.displayName;
+        this.keyname = loaded.keyname;
+        this.parent = loaded.parent;
+        this.ownerUser = loaded.ownerUser;
+
+        // Update the _loaded property
         this._loaded = loaded;
     }
 
     dump() {
-        const result = {};
+        const result: Value = {};
         const c = this._loaded;
 
         if (this.operation === "create") {
--- nextgisweb/resource/nodepkg/editor-widget/EditorWidget.tsx
+++ nextgisweb/resource/nodepkg/editor-widget/EditorWidget.tsx
@@ -5,15 +5,21 @@ import { Input } from "@nextgisweb/gui/antd";
 import { gettext } from "@nextgisweb/pyramid/i18n";
 import { ResourceSelect } from "@nextgisweb/resource/component";
 
+import type { EditorWidgetComponent, EditorWidgetProps } from "../type";
+
+import type { EditorStore } from "./EditorStore";
+
 import "./EditorWidget.less";
 
-export const EditorWidget = observer(({ store }) => {
+export const EditorWidget: EditorWidgetComponent<
+    EditorWidgetProps<EditorStore>
+> = observer(({ store }) => {
     return (
         <div className="ngw-resource-editor-widget">
             <label>{gettext("Display name")}</label>
             <Input
-                value={store.displayName}
-                placeholder={store.sdnDynamic || store.sdnBase}
+                value={store.displayName || undefined}
+                placeholder={store.sdnDynamic || store.sdnBase || undefined}
                 onChange={(e) => {
                     store.displayName = e.target.value;
                 }}
@@ -22,9 +28,9 @@ export const EditorWidget = observer(({ store }) => {
 
             <label>{gettext("Parent")}</label>
             <ResourceSelect
-                value={store.parent}
+                value={store.parent || undefined}
                 onChange={(v) => {
-                    store.parent = v;
+                    store.parent = typeof v === "number" ? v : null;
                 }}
                 allowClear={false}
                 disabled={store.operation === "create"}
@@ -34,9 +40,9 @@ export const EditorWidget = observer(({ store }) => {
             <PrincipalSelect
                 model={"user"}
                 systemUsers={["guest"]}
-                value={store.ownerUser}
+                value={store.ownerUser || undefined}
                 onChange={(v) => {
-                    store.ownerUser = v;
+                    store.ownerUser = v as number;
                 }}
                 allowClear={false}
                 disabled={!ngwConfig.isAdministrator}
@@ -44,7 +50,7 @@ export const EditorWidget = observer(({ store }) => {
 
             <label>{gettext("Keyname")}</label>
             <Input
-                value={store.keyname}
+                value={store.keyname || ""}
                 status={store.keynameIsValid ? undefined : "error"}
                 placeholder={gettext(
                     "Identifier for API integration (optional)"
