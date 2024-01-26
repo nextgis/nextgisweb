45fd23f79dc6b406e5ad059739ed69e70c81e412
Artemiy <rendrom@gmail.com>
2024-01-26 22:40:58 +0000

    Trivial TS translations

--- nextgisweb/resource/nodepkg/icon.tsx
+++ nextgisweb/resource/nodepkg/icon.tsx
@@ -1,16 +1,21 @@
+import type { SVGProps } from "react";
+
 import { SvgIcon } from "@nextgisweb/gui/svg-icon";
 
 import MAllowIcon from "@nextgisweb/icon/material/check_circle";
 import MDenyIcon from "@nextgisweb/icon/material/remove_circle";
 
-export function ResourceIcon({ identity, ...props }) {
+export function ResourceIcon({
+    identity,
+    ...props
+}: SVGProps<SVGSVGElement> & { identity?: string }) {
     return <SvgIcon icon={"rescls-" + identity} {...props} />;
 }
 
-export function AllowIcon(props) {
+export function AllowIcon(props: SVGProps<SVGSVGElement>) {
     return <MAllowIcon style={{ color: "var(--success)" }} {...props} />;
 }
 
-export function DenyIcon(props) {
+export function DenyIcon(props: SVGProps<SVGSVGElement>) {
     return <MDenyIcon style={{ color: "var(--danger)" }} {...props} />;
 }
