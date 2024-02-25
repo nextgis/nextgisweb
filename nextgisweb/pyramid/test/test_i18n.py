73c03d2717ce9545f5188f35f31be9fda49c24a9
Aleksandr Dezhin <me@dezhin.net>
2024-02-25 19:17:24 +0000

    Skip i18n test if MO-file missing

--- nextgisweb/pyramid/test/test_i18n.py
+++ nextgisweb/pyramid/test/test_i18n.py
@@ -1,10 +1,17 @@
+from pathlib import Path
+
+import pytest
+
 from nextgisweb.env import npgettext
 
 
 def test_npgettext(ngw_env):
+    mofile = Path(__file__).parent.parent / "locale/ru.mo"
+    if not mofile.exists():
+        pytest.skip(f"{mofile} not found")
+
     en = ngw_env.core.localizer("en").translate
     ru = ngw_env.core.localizer("ru").translate
-
     n = lambda n: npgettext("test", "unit", "units", n)
 
     assert (en(n(1)), en(n(2)), en(n(5))) == ("unit", "units", "units")
