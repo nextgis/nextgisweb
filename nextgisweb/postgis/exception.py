c8701a9ed6fd7e9971295579aa2a1c86cf542dc8
Aleksandr Dezhin <me@dezhin.net>
2024-03-18 20:06:31 +0000

    Fix ExternalDatabaseError arguments

--- nextgisweb/postgis/exception.py
+++ nextgisweb/postgis/exception.py
@@ -8,11 +8,10 @@ from nextgisweb.core.exception import ExternalServiceError
 class ExternalDatabaseError(ExternalServiceError):
     title = _("External database error")
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, sa_error=None, **kwargs):
         super().__init__(*args, **kwargs)
 
-        if not hasattr(self, "detail") and "sa_error" in kwargs:
-            sa_error = kwargs["sa_error"]
+        if not hasattr(self, "detail") and sa_error is not None:
             if (
                 isinstance(sa_error, StatementError)
                 and sa_error.orig is not None
