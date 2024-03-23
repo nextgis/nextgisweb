ed87c0e7bad01164fc8467a0cf80a06a284ac329
Aleksandr Dezhin <me@dezhin.net>
2024-03-23 21:16:35 +0000

    Add resource API types: social
    CU-863hbwg78

--- nextgisweb/social/model.py
+++ nextgisweb/social/model.py
@@ -1,4 +1,5 @@
 from io import BytesIO
+from typing import Union
 
 from PIL import Image
 
@@ -6,7 +7,7 @@ from nextgisweb.env import COMP_ID, Base
 from nextgisweb.lib import db
 
 from nextgisweb.file_storage import FileObj
-from nextgisweb.file_upload import FileUpload
+from nextgisweb.file_upload import FileUploadRef
 from nextgisweb.resource import Resource, ResourceScope, Serializer
 from nextgisweb.resource import SerializedProperty as SP
 
@@ -33,14 +34,14 @@ class ResourceSocial(Base):
     preview_fileobj = db.relationship(FileObj, lazy="joined")
 
 
-class _preview_file_upload_attr(SP):
-    def setter(self, srlzr, value):
+class FileUploadAttr(SP, apitype=True):
+    def set(self, srlzr, value: Union[FileUploadRef, None], *, create: bool):
         if srlzr.obj.social is None:
             srlzr.obj.social = ResourceSocial()
 
         social = srlzr.obj.social
         if value is not None:
-            fupload = FileUpload(id=value["id"])
+            fupload = value()
             with Image.open(fupload.data_path) as image:
                 width, height = image.size
                 resize = width > MAX_SIZE[0] or height > MAX_SIZE[1]
@@ -57,30 +58,27 @@ class _preview_file_upload_attr(SP):
             social.preview_fileobj = None
 
 
-class _preview_image_exists(SP):
-    def getter(self, srlzr):
+class ImageExistsAttr(SP, apitype=True):
+    def get(self, srlzr) -> bool:
         social = srlzr.obj.social
         return social is not None and social.preview_fileobj_id is not None
 
 
-class _preview_description_attr(SP):
-    def getter(self, srlzr):
+class DescriptionAttr(SP, apitype=True):
+    def get(self, srlzr) -> Union[str, None]:
         social = srlzr.obj.social
         return social.preview_description if social is not None else None
 
-    def setter(self, srlzr, value):
+    def set(self, srlzr, value: Union[str, None], *, create: bool):
         if srlzr.obj.social is None:
             srlzr.obj.social = ResourceSocial()
         srlzr.obj.social.preview_description = value
 
 
-class SocialSerializer(Serializer):
+class SocialSerializer(Serializer, apitype=True):
     identity = COMP_ID
     resclass = Resource
 
-    preview_file_upload = _preview_file_upload_attr(write=ResourceScope.update)
-    preview_image_exists = _preview_image_exists(read=ResourceScope.read)
-    preview_description = _preview_description_attr(
-        read=ResourceScope.read,
-        write=ResourceScope.update,
-    )
+    preview_file_upload = FileUploadAttr(write=ResourceScope.update)
+    preview_image_exists = ImageExistsAttr(read=ResourceScope.read)
+    preview_description = DescriptionAttr(read=ResourceScope.read, write=ResourceScope.update)
