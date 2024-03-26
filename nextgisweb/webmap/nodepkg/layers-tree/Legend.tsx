b6ab39cef57af77821fe243a648a7e4df230b047
Aleksandr Dezhin <me@dezhin.net>
2024-03-26 20:00:27 +0000

    Style legend checkboxes
    CU-86945fkav

--- nextgisweb/webmap/nodepkg/layers-tree/Legend.tsx
+++ nextgisweb/webmap/nodepkg/layers-tree/Legend.tsx
@@ -1,4 +1,6 @@
-import { Checkbox } from "@nextgisweb/gui/antd";
+import { theme } from "antd";
+
+import { Checkbox, ConfigProvider } from "@nextgisweb/gui/antd";
 
 import type WebmapStore from "../store";
 import type { TreeItem } from "../type/TreeItems";
@@ -10,7 +12,11 @@ interface LegendProps {
     store: WebmapStore;
 }
 
+const { useToken } = theme;
+
 export function Legend({ nodeData, store }: LegendProps) {
+    const { token } = useToken();
+
     const legendInfo = "legendInfo" in nodeData && nodeData.legendInfo;
     if (!nodeData || !legendInfo || !legendInfo.open) {
         return <></>;
@@ -18,35 +24,47 @@ export function Legend({ nodeData, store }: LegendProps) {
 
     return (
         <div className="legend-block">
-            {legendInfo.symbols.map((s, idx) => {
-                const id = nodeData.id;
-                const symbols = store._legendSymbols[id];
-                const render = (symbols && symbols[s.index]) ?? s.render;
-                return (
-                    <div
-                        key={idx}
-                        className="legend-symbol"
-                        title={s.display_name}
-                    >
-                        <Checkbox
-                            defaultChecked={render}
-                            onChange={(e) => {
-                                store.setLayerLegendSymbol(
-                                    id,
-                                    s.index,
-                                    e.target.checked
-                                );
-                            }}
-                        />
-                        <img
-                            width={20}
-                            height={20}
-                            src={"data:image/png;base64," + s.icon.data}
-                        />
-                        <div className="legend-title">{s.display_name}</div>
-                    </div>
-                );
-            })}
+            <ConfigProvider
+                theme={{
+                    components: {
+                        Checkbox: {
+                            colorPrimary: token.colorWhite,
+                            colorPrimaryHover: token.colorWhite,
+                            colorWhite: token.colorPrimary,
+                        },
+                    },
+                }}
+            >
+                {legendInfo.symbols.map((s, idx) => {
+                    const id = nodeData.id;
+                    const symbols = store._legendSymbols[id];
+                    const render = (symbols && symbols[s.index]) ?? s.render;
+                    return (
+                        <div
+                            key={idx}
+                            className="legend-symbol"
+                            title={s.display_name}
+                        >
+                            <Checkbox
+                                defaultChecked={render}
+                                onChange={(e) => {
+                                    store.setLayerLegendSymbol(
+                                        id,
+                                        s.index,
+                                        e.target.checked
+                                    );
+                                }}
+                            />
+                            <img
+                                width={20}
+                                height={20}
+                                src={"data:image/png;base64," + s.icon.data}
+                            />
+                            <div className="legend-title">{s.display_name}</div>
+                        </div>
+                    );
+                })}
+            </ConfigProvider>
         </div>
     );
 }
