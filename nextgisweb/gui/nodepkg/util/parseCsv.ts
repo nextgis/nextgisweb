1ba9d152f53ee5f4b020f94f3afe3a71d866d4c5
Artemiy <rendrom@gmail.com>
2024-05-13 21:38:36 +0000

    Fix parseCsv function to work in all browsers
    CU-8694hvwrf

--- nextgisweb/gui/nodepkg/util/parseCsv.ts
+++ nextgisweb/gui/nodepkg/util/parseCsv.ts
@@ -9,7 +9,13 @@ export async function parseCsv<T>(
         return Papa.parse(file, options);
     }
     return new Promise((resolve, reject) => {
-        if (file.type === "text/csv") {
+        const textType = /text.*/;
+        const csvType = "application/vnd.ms-excel";
+
+        if (
+            file instanceof File &&
+            (file.type.match(textType) || file.type === csvType)
+        ) {
             const reader = new FileReader();
             reader.onload = (e) => {
                 const text = e.target?.result;
