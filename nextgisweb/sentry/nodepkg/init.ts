8be9bfb57c12286bec149937c2be634f7f9f85c9
Aleksandr Dezhin <me@dezhin.net>
2024-06-28 17:52:08 +0000

    WIP

--- nextgisweb/sentry/nodepkg/init.ts
+++ nextgisweb/sentry/nodepkg/init.ts
@@ -2,13 +2,19 @@
 import * as Sentry from "@sentry/browser";
 import type { Integration } from "@sentry/types";
 
-export function init(opts: { dsn: string }) {
+export function init(opts: { dsn: string; routeName: string }) {
     const integrations: Integration[] = [];
+    const { routeName, ...sentryOpts } = opts;
 
     Sentry.init({
-        ...opts,
+        ...sentryOpts,
         integrations,
         maxBreadcrumbs: 50,
         debug: false,
+        initialScope: (scope) => {
+            scope.setUser({ id: ngwConfig.instanceId, ip_address: "{{auto}}" });
+            scope.setTransactionName(routeName);
+            return scope;
+        },
     });
 }
--- nextgisweb/sentry/template/init.mako
+++ nextgisweb/sentry/template/init.mako
@@ -2,7 +2,8 @@
 <% dsn = opts["dsn"] %>
 
 %if dsn:
-    <% init_opts = dict(dsn=dsn) %>
+    <% route = request.matched_route %>
+    <% init_opts = dict(dsn=dsn, routeName=route.name if route else None) %>
     <script type="text/javascript">
         require(["@nextgisweb/sentry/init"], function ({ init }) {
             init(${json_js(init_opts)});
