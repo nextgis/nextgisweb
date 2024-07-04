bc73b4ba1539a2715425a4090677e907c51b777c
Artemiy <rendrom@gmail.com>
2024-07-04 07:36:30 +0000

    Fix usePrevious initial value

--- nextgisweb/gui/nodepkg/hook/usePrevious.ts
+++ nextgisweb/gui/nodepkg/hook/usePrevious.ts
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 
-export function usePrevious<T>(value: T, initialValue: T) {
-    const ref = useRef(initialValue);
+export function usePrevious<T>(value: T, initialValue?: T) {
+    const ref = useRef(initialValue || value);
     useEffect(() => {
         ref.current = value;
     });
