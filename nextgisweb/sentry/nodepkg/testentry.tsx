e17f3afd93cc46ab064b605742f1de2462b6aee8
Aleksandr Dezhin <me@dezhin.net>
2024-07-21 23:33:53 +0000

    Add custom error class test exception

--- nextgisweb/sentry/nodepkg/testentry.tsx
+++ nextgisweb/sentry/nodepkg/testentry.tsx
@@ -1,14 +1,27 @@
 /** @testentry react */
-import { Button } from "@nextgisweb/gui/antd";
+import { BaseError } from "make-error";
+
+import { Button, Space } from "@nextgisweb/gui/antd";
+
+class CustomError extends BaseError {}
 
 export default function Testentry() {
     return (
-        <Button
-            onClick={() => {
-                throw new Error("Hello, Sentry!");
-            }}
-        >
-            Hello, Sentry!
-        </Button>
+        <Space.Compact>
+            <Button
+                onClick={() => {
+                    throw new Error("Hello, Sentry!");
+                }}
+            >
+                Hello, Sentry!
+            </Button>
+            <Button
+                onClick={() => {
+                    throw new CustomError("Custom error message");
+                }}
+            >
+                CustomException
+            </Button>
+        </Space.Compact>
     );
 }
