570953e7ee94d6ab8ba71630df729b9c76e27937
Aleksandr Dezhin <me@dezhin.net>
2024-07-21 23:33:29 +0000

    Fix uploading of zero-length files
    CU-85ztftna2 CU-86955bkac

--- nextgisweb/file_upload/test/test_api.py
+++ nextgisweb/file_upload/test/test_api.py
@@ -1,14 +1,13 @@
 from subprocess import check_call, check_output
 
+import pytest
 import webtest
 
 from ..model import FileUpload
 
-TEST_FILENAME = "file.ext"
-TEST_CONTENT = "content".encode("utf-8")
-
-TEST_FILENAME2 = "file2.ext"
-TEST_CONTENT2 = "content2".encode("utf-8")
+FN0, FC0 = "zero.txt", b""
+FN1, FC1 = "foo.ext", b"content-1"
+FN2, FC2 = "bar.ext", b"content-2"
 
 
 def test_options(ngw_env, ngw_webtest_app):
@@ -25,7 +24,7 @@ def test_tus_method(ngw_webtest_app):
         "/api/component/file_upload/",
         headers={
             "Tus-Resumable": "1.0.0",
-            "Upload-Length": str(len(TEST_CONTENT)),
+            "Upload-Length": str(len(FC1)),
             "Upload-Metadata": "name dGVzdA==",
         },
         status=201,
@@ -40,7 +39,7 @@ def test_tus_method(ngw_webtest_app):
     # Content type missing
     ngw_webtest_app.patch(
         location,
-        TEST_CONTENT,
+        FC1,
         headers={"Tus-Resumable": "1.0.0", "Upload-Offset": str(0)},
         status=415,
     )
@@ -48,7 +47,7 @@ def test_tus_method(ngw_webtest_app):
     # Conflict
     ngw_webtest_app.patch(
         location,
-        TEST_CONTENT[1:],
+        FC1[1:],
         headers={
             "Tus-Resumable": "1.0.0",
             "Content-Type": "application/offset+octet-stream",
@@ -59,7 +58,7 @@ def test_tus_method(ngw_webtest_app):
 
     patch = ngw_webtest_app.patch(
         location,
-        TEST_CONTENT,
+        FC1,
         headers={
             "Tus-Resumable": "1.0.0",
             "Content-Type": "application/offset+octet-stream",
@@ -69,7 +68,7 @@ def test_tus_method(ngw_webtest_app):
     )
 
     assert patch.headers.get("Tus-Resumable") == "1.0.0"
-    assert patch.headers.get("Upload-Offset") == str(len(TEST_CONTENT))
+    assert patch.headers.get("Upload-Offset") == str(len(FC1))
 
     head = ngw_webtest_app.head(
         location,
@@ -79,22 +78,23 @@ def test_tus_method(ngw_webtest_app):
     )
 
     assert head.status_code == 200
-    assert head.headers.get("Upload-Offset") == str(len(TEST_CONTENT))
-    assert head.headers.get("Upload-Length") == str(len(TEST_CONTENT))
+    assert head.headers.get("Upload-Offset") == str(len(FC1))
+    assert head.headers.get("Upload-Length") == str(len(FC1))
 
     get = ngw_webtest_app.get(location, status=200)
 
     assert get.status_code == 200
-    assert get.json["size"] == len(TEST_CONTENT)
+    assert get.json["size"] == len(FC1)
     assert get.json["name"] == "test"
 
     ngw_webtest_app.delete(location, status=204)
     ngw_webtest_app.delete(location, status=404)
 
 
-def test_tus_client(ngw_httptest_app, tmp_path):
-    of = tmp_path / "sample"
-    check_call(["dd", "if=/dev/zero", "of=" + str(of), "bs=1M", "count=16"])
+@pytest.mark.parametrize("size_m", (0, 1, 16))
+def test_tus_client(size_m, ngw_httptest_app, tmp_path):
+    of = tmp_path / f"sample-{size_m}"
+    check_call(["dd", "if=/dev/zero", f"of={of}", "bs=1M", f"count={size_m}"])
 
     burl = ngw_httptest_app.base_url + "/api/component/file_upload/"
     furl = check_output(["tusc", "client", burl, str(of)], encoding="utf-8").strip()
@@ -104,36 +104,43 @@ def test_tus_client(ngw_httptest_app, tmp_path):
     response.raise_for_status()
 
     data = response.json()
-    assert data["size"] == 16 * 1024 * 1024
+    assert data["size"] == size_m * 2**20
 
 
 def test_post_single(ngw_webtest_app):
     resp = ngw_webtest_app.post(
         "/api/component/file_upload/",
-        dict(file=webtest.Upload(TEST_FILENAME, TEST_CONTENT)),
+        dict(file=webtest.Upload(FN1, FC1)),
     )
 
     fupload = FileUpload(id=resp.json["upload_meta"][0]["id"])
-    assert fupload.data_path.read_bytes() == TEST_CONTENT
+    assert fupload.data_path.read_bytes() == FC1
 
 
 def test_post_multi(ngw_webtest_app):
     resp = ngw_webtest_app.post(
         "/api/component/file_upload/",
         [
-            ["files[]", webtest.Upload(TEST_FILENAME, TEST_CONTENT)],
-            ["files[]", webtest.Upload(TEST_FILENAME2, TEST_CONTENT2)],
+            ["files[]", webtest.Upload(FN0, FC0)],
+            ["files[]", webtest.Upload(FN1, FC1)],
+            ["files[]", webtest.Upload(FN2, FC2)],
         ],
     )
 
     fupload = FileUpload(id=resp.json["upload_meta"][0]["id"])
-    assert fupload.data_path.read_bytes() == TEST_CONTENT
+    assert fupload.name == FN0
+    assert fupload.data_path.read_bytes() == FC0
 
     fupload = FileUpload(id=resp.json["upload_meta"][1]["id"])
-    assert fupload.data_path.read_bytes() == TEST_CONTENT2
+    assert fupload.name == FN1
+    assert fupload.data_path.read_bytes() == FC1
+
+    fupload = FileUpload(id=resp.json["upload_meta"][2]["id"])
+    assert fupload.name == FN2
+    assert fupload.data_path.read_bytes() == FC2
 
 
 def test_put(ngw_webtest_app):
-    resp = ngw_webtest_app.put("/api/component/file_upload/", TEST_CONTENT)
+    resp = ngw_webtest_app.put("/api/component/file_upload/", FC1)
     fupload = FileUpload(id=resp.json["id"])
-    assert fupload.data_path.read_bytes() == TEST_CONTENT
+    assert fupload.data_path.read_bytes() == FC1
