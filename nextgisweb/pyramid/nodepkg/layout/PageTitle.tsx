2bbf078d6df5e3e4b2ef5f0e663a7a17ad193264
Aleksandr Dezhin <me@dezhin.net>
2024-07-26 20:21:48 +0000

    Remove existing page title if hijacked

--- nextgisweb/pyramid/nodepkg/layout/PageTitle.tsx
+++ nextgisweb/pyramid/nodepkg/layout/PageTitle.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useLayoutEffect, useMemo, useRef } from "react";
 import type { ReactNode } from "react";
 
 import { mergeClasses } from "@nextgisweb/gui/util";
@@ -10,10 +10,18 @@ interface PageTitleProps {
 }
 
 export function PageTitle({ title, pullRight, children }: PageTitleProps) {
+    const titleRef = useRef<HTMLElement | null>();
+
     const ititle = useMemo(() => {
         // Capture an existing page title if not set
-        return title || document.getElementById("title")?.innerText;
+        titleRef.current = document.getElementById("title");
+        return title || titleRef.current?.innerText;
     }, [title]);
+
+    useLayoutEffect(() => {
+        // Delete existing header if it isn't replaced
+        titleRef.current?.remove();
+    }, []);
 
     return (
         <h1
