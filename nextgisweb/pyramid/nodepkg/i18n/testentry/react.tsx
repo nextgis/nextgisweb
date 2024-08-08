33417035e246d23a66fd86cab0dd4a5b992906a8
Boris Obinyakov <andthenyouadd@gmail.com>
2024-08-08 10:38:55 +0000

    Both arg types for gettext react sample

--- nextgisweb/pyramid/nodepkg/i18n/testentry/react.tsx
+++ nextgisweb/pyramid/nodepkg/i18n/testentry/react.tsx
@@ -3,15 +3,37 @@
 import { SvgIcon } from "@nextgisweb/gui/svg-icon";
 import { gettext, gettextf } from "@nextgisweb/pyramid/i18n";
 
-const templateReactNodes = gettextf("Fot this task try {icon} {serviceName}");
+const templateNamedReactNodes = gettextf(
+    "Fot this task try {icon} {serviceName}, {username}."
+);
 
-const Component = templateReactNodes({
+const templatePositionedReactNodes = gettextf("Do you prefer {0} or {1}?");
+
+const PositionedParamsComponent = templatePositionedReactNodes(
+    <>
+        <SvgIcon icon={"rescls-wfsserver_service"} />{" "}
+        <b>{gettext("WFS connection")}</b>
+    </>,
+    <>
+        <SvgIcon icon={"rescls-ogcfserver_service"} />{" "}
+        <b>{gettext("OGC API Features")}</b>
+    </>
+);
+
+const NamedParamsComponent = templateNamedReactNodes({
     icon: <SvgIcon icon={"rescls-wfsserver_service"} />,
     serviceName: <b>{gettext("WFS connection")}</b>,
+    username: "Ford",
 });
 
 function InterpolationWithReactNodeTest() {
-    return Component;
+    return (
+        <>
+            {NamedParamsComponent}
+            <br />
+            {PositionedParamsComponent}
+        </>
+    );
 }
 
 export default InterpolationWithReactNodeTest;
