dfa9212e62d3af316b763d3c9caf2cdefc633a97
Boris Obinyakov <andthenyouadd@gmail.com>
2024-08-09 12:56:23 +0000

    Fix testentry after rebase

--- nextgisweb/pyramid/nodepkg/i18n/testentry/core.ts
+++ nextgisweb/pyramid/nodepkg/i18n/testentry/core.ts
@@ -12,11 +12,51 @@ import {
 } from "@nextgisweb/pyramid/i18n";
 
 describe("Gettext implementation", () => {
+    it("gettextf implementation", () => {
+        const f = gettextf("Hello, {first} {last}");
+
+        assert.equal(
+            f({ first: "Arthur", last: "Dent" }),
+            "Hello, Arthur Dent"
+        );
+    });
+
     it("pgettext returns something", () => {
         assert.isNotEmpty(pgettext("test", "unit"));
     });
 
-    it("translates plurals", () => {
+    it("pgettextf implementation", () => {
+        assert.equal(
+            pgettextf("test", "units {}")("on ellipsoid"),
+            "units on ellipsoid"
+        );
+    });
+
+    it("ngettext implementation", () => {
+        assert.equal(
+            ngettext(
+                "The resource has been moved",
+                "Resources have been moved",
+                2
+            ),
+            "Resources have been moved"
+        );
+    });
+
+    it("ngettextf implementation", () => {
+        const num = 3;
+
+        assert.equal(
+            ngettextf(
+                "The resource has been deleted",
+                "{} resources have been deleted",
+                num
+            )(num),
+            "3 resources have been deleted"
+        );
+    });
+
+    it("translates plurals (npgettext)", () => {
         const f = (num: number) => npgettext("test", "unit", "units", num);
         assert.notStrictEqual(f(1), f(2));
     });
