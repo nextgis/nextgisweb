2477c88b4f7b81fa852e06f41deff7e5bd9522eb
Artemiy <rendrom@gmail.com>
2024-08-28 00:07:28 +0000

    Add layer request cancel on map move
    CU-86940je6j

--- nextgisweb/webmap/amd/ngw-webmap/TileAdapter.js
+++ nextgisweb/webmap/amd/ngw-webmap/TileAdapter.js
@@ -6,7 +6,25 @@ define([
 ], function (declare, Adapter, api, XYZ) {
     return declare(Adapter, {
         createLayer: function (item) {
-            return new XYZ(
+            let tilesLoading = {};
+
+            const getZoom = (l) => {
+                const map = l.olLayer.getMapInternal();
+                const view = map.getView();
+                return view.getZoom();
+            };
+
+            const abortLoading = function ({ ignoreZoom }) {
+                Object.keys(tilesLoading).forEach((key) => {
+                    const img = tilesLoading[key];
+                    if (img && img._zoom !== ignoreZoom) {
+                        tilesLoading[key].src = "";
+                        delete tilesLoading[key];
+                    }
+                });
+            };
+
+            const layer = new XYZ(
                 item.id,
                 {
                     visible: item.visibility,
@@ -28,8 +46,28 @@ define([
                         item.styleId +
                         "&nd=204",
                     crossOrigin: "anonymous",
+                    tileLoadFunction: function (tile, src) {
+                        let zoom = getZoom(layer);
+
+                        const [z, x, y] = tile.getTileCoord();
+                        const tileKey = [z, x, y].join("-");
+                        const img = tile.getImage();
+
+                        img._zoom = z;
+
+                        abortLoading({ ignoreZoom: zoom });
+
+                        img.src = src;
+                        tilesLoading[tileKey] = img;
+
+                        img.onload = img.onerror = function () {
+                            delete tilesLoading[tileKey];
+                        };
+                    },
                 }
             );
+
+            return layer;
         },
     });
 });
