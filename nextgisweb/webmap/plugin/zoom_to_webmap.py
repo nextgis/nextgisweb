d80d5cb731d12a931e06db3ec8944a327f3e14ca
Artemiy <rendrom@gmail.com>
2024-11-25 08:13:41 +0000

    Rewrite ZoomToWebmapPlugin to nodepkg

--- nextgisweb/webmap/plugin/zoom_to_webmap.py
+++ nextgisweb/webmap/plugin/zoom_to_webmap.py
@@ -4,4 +4,4 @@ from .base import WebmapLayerPlugin
 class ZoomToWebmapPlugin(WebmapLayerPlugin):
     @classmethod
     def is_layer_supported(cls, layer, webmap):
-        return ("ngw-webmap/plugin/ZoomToWebmap", dict())
+        return ("@nextgisweb/webmap/plugin/zoom-to-webmap", dict())
