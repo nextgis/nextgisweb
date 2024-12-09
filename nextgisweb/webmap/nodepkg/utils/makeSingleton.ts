a349963d0fa1fe1f5682d8238c3f39f0625acc0b
Artemiy <rendrom@gmail.com>
2024-12-09 13:53:25 +0000

    Rewrite makeSingleton function to typescript

---
+++ nextgisweb/webmap/nodepkg/utils/makeSingleton.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export function makeSingleton<T extends new (...args: any[]) => any>(ctor: T) {
+    let instance: InstanceType<T> | null = null;
+
+    const singletonCtor = {
+        getInstance: (args?: ConstructorParameters<T>): InstanceType<T> => {
+            if (!instance) {
+                instance = new ctor(...(args || []));
+                Object.defineProperty(instance, "constructor", {
+                    value: null,
+                    writable: false,
+                });
+            }
+            return instance as InstanceType<T>;
+        },
+    };
+
+    return singletonCtor;
+}
