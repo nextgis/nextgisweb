6629e5b305485a3fb795749b1e427d4552905571
Artemiy <rendrom@gmail.com>
2025-01-01 05:03:48 +0000

    Fix BasemapSelector default layer show

--- nextgisweb/webmap/nodepkg/ol/layer/Vector.ts
+++ nextgisweb/webmap/nodepkg/ol/layer/Vector.ts
@@ -2,10 +2,10 @@ import { Vector as VectorLayer } from "ol/layer";
 import { Vector as VectorSource } from "ol/source";
 import type { Options as VectorSourceOptions } from "ol/source/Vector";
 
-import { BaseLayer } from "./_Base";
+import { CoreLayer } from "./_Base";
 import type { LayerOptions } from "./_Base";
 
-export default class Vector extends BaseLayer<
+export default class Vector extends CoreLayer<
     VectorSource,
     VectorLayer<VectorSource>,
     VectorSourceOptions
