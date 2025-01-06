852a9bc8439bc5c2aeb7f1f695fb1cd2044e6494
Artemiy <rendrom@gmail.com>
2025-01-06 09:06:20 +0000

    Rewrite LayerInfoPlugin to nodepkg

---
+++ nextgisweb/webmap/nodepkg/plugin/layer-info/LayerInfoPlugin.ts
@@ -0,0 +1,70 @@
+import { gettext } from "@nextgisweb/pyramid/i18n";
+import type { PluginState } from "@nextgisweb/webmap/type";
+import type { LayerItem } from "@nextgisweb/webmap/type/TreeItems";
+import reactPanel from "@nextgisweb/webmap/ui/react-panel";
+
+import { PluginBase } from "../PluginBase";
+
+export class LayerInfoPlugin extends PluginBase {
+    getPluginState(nodeData: LayerItem): PluginState {
+        const state = super.getPluginState(nodeData);
+        const data = this.display.get("itemConfig").plugin[this.identity];
+        return {
+            ...state,
+            enabled: state.enabled && data.description,
+        };
+    }
+
+    async run() {
+        this.openLayerInfo();
+        return;
+    }
+
+    getMenuItem() {
+        return {
+            icon: "material-article",
+            title: gettext("Description"),
+            onClick: () => {
+                return this.run();
+            },
+        };
+    }
+
+    openLayerInfo() {
+        const pm = this.display.panelsManager;
+        const pkey = "resource-description";
+        const item = this.display.dumpItem();
+        const data = this.display.get("itemConfig").plugin[this.identity];
+        if (data !== undefined) {
+            const content = data.description;
+            let panel = pm.getPanel(pkey);
+            if (panel) {
+                if (panel.app) {
+                    panel.app.update({ content });
+                } else {
+                    panel.props = { content };
+                }
+            } else {
+                const cls = reactPanel(
+                    () => import("@nextgisweb/webmap/panel/description"),
+                    {
+                        props: { content },
+                    }
+                );
+                pm.addPanels([
+                    {
+                        cls,
+                        params: {
+                            title: item.label,
+                            name: pkey,
+                            order: 100,
+                            menuIcon: "material-article",
+                        },
+                    },
+                ]);
+                panel = pm.getPanel(pkey);
+            }
+            pm.activatePanel(pkey);
+        }
+    }
+}
