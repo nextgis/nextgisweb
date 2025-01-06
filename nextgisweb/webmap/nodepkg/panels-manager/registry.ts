e0b1e096ee0ae663e05871a5162e2a6cc893dac4
Artemiy <rendrom@gmail.com>
2025-01-06 09:06:20 +0000

    Fix registry plugin load before render

--- nextgisweb/webmap/nodepkg/panels-manager/registry.ts
+++ nextgisweb/webmap/nodepkg/panels-manager/registry.ts
@@ -13,14 +13,9 @@ import type { PanelMeta } from "../type";
 
 import type { PanelComponentProps } from "./type";
 
-export type PanelWidget<T = unknown> = ComponentType<
-    PanelComponentProps<T>
->;
+export type PanelWidget<T = unknown> = ComponentType<PanelComponentProps<T>>;
 
-export type PanelPlugin<T = unknown> = Plugin<
-    PanelWidget<T>,
-    PanelMeta<T>
->;
+export type PanelPlugin<T = unknown> = Plugin<PanelWidget<T>, PanelMeta<T>>;
 
 export const registry = pluginRegistry<PanelWidget<any>, PanelMeta<any>>(
     MODULE_NAME
