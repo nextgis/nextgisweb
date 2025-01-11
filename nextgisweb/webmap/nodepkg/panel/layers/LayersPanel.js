276c9712a611d659b9c9c50262d373aa50606a3e
Aleksandr Dezhin <me@dezhin.net>
2025-01-11 20:44:04 +0000

    Fix issue with layers panel not closing properly

--- nextgisweb/webmap/nodepkg/panel/layers/LayersPanel.js
+++ nextgisweb/webmap/nodepkg/panel/layers/LayersPanel.js
@@ -29,6 +29,7 @@ export function LayersPanel({ title, close, display, ...props }) {
                     />
                 </>
             }
+            close={close}
             epilog={
                 <BasemapSelector
                     {...{
