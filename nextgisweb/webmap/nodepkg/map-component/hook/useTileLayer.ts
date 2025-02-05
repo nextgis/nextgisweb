580d427847a623800a3d40e15803f58497a82add
floatline <poltewski@gmail.com>
2025-02-05 21:50:45 +0000

    Use cross-origin requests for basemap preview
    NGW-1345

--- nextgisweb/webmap/nodepkg/map-component/hook/useTileLayer.ts
+++ nextgisweb/webmap/nodepkg/map-component/hook/useTileLayer.ts
@@ -2,6 +2,8 @@ import TileLayer from "ol/layer/Tile";
 import Source from "ol/source/TileImage";
 import { useEffect, useMemo, useRef } from "react";
 
+import { tileLoadFunction } from "@nextgisweb/pyramid/util";
+
 export function useTileLayer({
     url,
     opacity,
@@ -11,7 +13,25 @@ export function useTileLayer({
     opacity?: number;
     attributions?: string | null;
 }) {
-    const source = useRef(new Source({ wrapX: true }));
+    const source = useRef(
+        new Source({
+            wrapX: true,
+            crossOrigin: "anonymous",
+            tileLoadFunction: (image, src) => {
+                // @ts-expect-error Property 'getImage' does not exist on type 'Tile'.
+                const img = image.getImage() as HTMLImageElement;
+                const abortController = new AbortController();
+
+                tileLoadFunction({
+                    src,
+                    signal: abortController.signal,
+                    cache: "force-cache",
+                }).then((imageUrl) => {
+                    img.src = imageUrl;
+                });
+            },
+        })
+    );
 
     useEffect(() => {
         if (attributions === null) {
