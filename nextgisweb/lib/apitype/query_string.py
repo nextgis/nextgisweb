3e06f2502c1ffa6648c21de8292e681777589cfa
Aleksandr Dezhin <me@dezhin.net>
2025-02-09 10:45:26 +0000

    Fix deep dict query param decoding

--- nextgisweb/lib/apitype/query_string.py
+++ nextgisweb/lib/apitype/query_string.py
@@ -73,7 +73,7 @@ def deep_dict(qs: QueryString, *, name: str, loadk: StringDecoder, loadv: String
         return {
             loadk(unquote_strict(pn[name_len + 1 : -1])): loadv(unquote_strict(pvs[-1]))
             for pn, pvs in qs.grouped.items()
-            if len(pn) > name_len and pn[name_len] == "[" and pn[-1] == "]"
+            if pn.startswith(name) and len(pn) > name_len and pn[name_len] == "[" and pn[-1] == "]"
         }
     except ValidationError as exc:
         raise QueryParamInvalidValue(name) from exc
