1cbcd9162a47fd7d61262b2b4f5886a504f185c8
Artemiy <rendrom@gmail.com>
2025-02-17 17:58:02 +0000

    Add TabLabel isValid notify

--- nextgisweb/feature_layer/nodepkg/feature-editor/component/TabLabel.tsx
+++ nextgisweb/feature_layer/nodepkg/feature-editor/component/TabLabel.tsx
@@ -4,23 +4,32 @@ import { Badge, Space } from "@nextgisweb/gui/antd";
 
 import { DirtyMark } from "./DirtyMark";
 
+import ErrorIcon from "@nextgisweb/icon/material/error";
+
 interface TabLabelProps {
-    dirty: boolean;
+    dirty?: boolean;
     label: string | ReactNode;
     counter?: number | string | null;
+    isValid?: boolean;
 }
 
-export function TabLabel({ dirty, label, counter }: TabLabelProps) {
+export function TabLabel({ dirty, label, counter, isValid }: TabLabelProps) {
     return (
         <Space>
             {label}
-            {(counter && (
+            {counter ? (
                 <Badge
                     count={counter}
                     color={dirty ? "var(--primary)" : "var(--text-secondary)"}
                     size="small"
                 />
-            )) || <DirtyMark dirty={dirty} />}
+            ) : dirty ? (
+                <DirtyMark dirty={dirty} />
+            ) : isValid === false ? (
+                <span style={{ color: "var(--error)" }}>
+                    <ErrorIcon />
+                </span>
+            ) : null}
         </Space>
     );
 }
