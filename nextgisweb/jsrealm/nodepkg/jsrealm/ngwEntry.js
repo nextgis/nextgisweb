b928f22525269d736df16a235fa35f3c33072912
Aleksandr Dezhin <me@dezhin.net>
2025-02-27 10:43:21 +0000

    Lookup entrypoints registry first for ngwEntry
    NGW-1419

--- nextgisweb/jsrealm/nodepkg/jsrealm/ngwEntry.js
+++ nextgisweb/jsrealm/nodepkg/jsrealm/ngwEntry.js
@@ -1,9 +1,41 @@
+const registryLoader = import("./entrypoint/registry").then(({ registry }) => {
+    const mapping = Object.fromEntries(
+        registry.queryAll().map((i) => [i.identity, i.value])
+    );
+
+    return (name) => {
+        const plugin = mapping[name];
+        if (plugin) return plugin();
+    };
+});
+
+const entrypointLoader = import("@nextgisweb/jsrealm/entrypoint").then(
+    ({ default: entrypoint }) => {
+        return (name) => entrypoint(name);
+    }
+);
+
+const cache = {};
+
 window.ngwEntry = (name) => {
-    return new Promise((resolve, reject) => {
-        import("@nextgisweb/jsrealm/entrypoint").then(
-            ({ default: entrypoint }) => {
-                entrypoint(name).then((m) => resolve(m), reject);
-            }
-        );
-    });
+    const cached = cache[name];
+    if (cached) return cached;
+
+    let resolve, reject;
+    const promise = (cache[name] = new Promise((res, rej) => {
+        [resolve, reject] = [res, rej];
+    }));
+
+    registryLoader.then((c) => {
+        const fromRegistry = c(name);
+        if (fromRegistry) {
+            fromRegistry.then(resolve, reject);
+        } else {
+            entrypointLoader.then((c) => {
+                c(name).then(resolve, reject);
+            });
+        }
+    }, reject);
+
+    return promise;
 };
