7043eb51e7a3935589268a9e18c65768a7fe74d0
Aleksandr Dezhin <me@dezhin.net>
2025-03-16 11:44:36 +0000

    Optimize useThemeVariables rendering

--- nextgisweb/gui/nodepkg/hook/useThemeVariables.ts
+++ nextgisweb/gui/nodepkg/hook/useThemeVariables.ts
@@ -1,20 +1,40 @@
+import { isEqual } from "lodash-es";
+import { useRef } from "react";
 import type { CSSProperties } from "react";
 
 import { useToken } from "../antd";
 import type { AliasToken } from "../antd";
 
+/**
+ * Generate CSS properties based on theme variables mapping
+ *
+ * @param mapping - Variable name to theme token mapping
+ * @returns CSS properties with theme token values
+ */
 export function useThemeVariables(
     mapping: Record<string, keyof AliasToken>
 ): CSSProperties {
-    const { token } = useToken();
-    return Object.fromEntries(
-        Object.entries(mapping).map(([vname, tkey]) => [
-            `--${vname}`,
-            `${token[tkey]}${
-                tkey.startsWith("borderRadius") || tkey.startsWith("fontSize")
-                    ? "px"
-                    : ""
-            }`,
-        ])
-    );
+    const { token, hashId } = useToken();
+
+    type MemoKey = { hashId: string; mapping: typeof mapping };
+    const memo = useRef<[MemoKey, CSSProperties] | undefined>();
+
+    if (!memo.current || !isEqual(memo.current[0], { hashId, mapping })) {
+        memo.current = [
+            { hashId, mapping },
+            Object.fromEntries(
+                Object.entries(mapping).map(([vname, tkey]) => [
+                    `--${vname}`,
+                    `${token[tkey]}${
+                        tkey.startsWith("borderRadius") ||
+                        tkey.startsWith("fontSize")
+                            ? "px"
+                            : ""
+                    }`,
+                ])
+            ),
+        ];
+    }
+
+    return memo.current[1];
 }
