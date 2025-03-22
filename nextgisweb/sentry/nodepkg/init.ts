30b16215a983040ffa0bd067a73bc1333a81dd12
Aleksandr Dezhin <me@dezhin.net>
2025-03-22 15:05:33 +0000

    Enable captureConsole Sentry integration
    NGW-1480

--- nextgisweb/sentry/nodepkg/init.ts
+++ nextgisweb/sentry/nodepkg/init.ts
@@ -4,14 +4,15 @@ import type { Integration } from "@sentry/core";
 import { BaseAPIError } from "@nextgisweb/pyramid/api";
 
 export function init(opts: { dsn: string; routeName: string }) {
-    const integrations: Integration[] = [];
     const { routeName, ...sentryOpts } = opts;
+
+    const integrations: Integration[] = [
+        Sentry.captureConsoleIntegration({ levels: ["error"] }),
+    ];
 
     Sentry.init({
         ...sentryOpts,
         integrations,
-        maxBreadcrumbs: 50,
-        debug: false,
 
         initialScope: (scope) => {
             scope.setUser({ id: ngwConfig.instanceId, ip_address: "{{auto}}" });
