c277f7cc3b0cfc455700883bac4b369fe80a1ddc
Aleksandr Dezhin <me@dezhin.net>
2025-03-30 02:38:23 +0000

    Remove unused ContentBox

--- nextgisweb/gui/nodepkg/component/index.ts
+++ nextgisweb/gui/nodepkg/component/index.ts
@@ -1,6 +1,5 @@
 export { AutoCompleteHoneypot } from "./AutoCompleteHoneypot";
 export { AutoCompleteInput } from "./AutoCompleteInput";
-export { ContentBox } from "./ContentBox";
 export { InputOpacity } from "./InputOpacity";
 export { InputScaleDenom } from "./InputScaleDenom";
 export { LoadingWrapper } from "./LoadingWrapper";
@@ -8,6 +7,6 @@ export { SaveButton } from "./SaveButton";
 export { TemplateLink } from "./TemplateLink";
 
 export * from "./content-card";
+export * from "./extent-row";
 export * from "./simple-table";
 export * from "./tabs-label-badge";
-export * from "./extent-row";
