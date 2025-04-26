5d310b9142f4198bffe66fc170d1c6b315f7bb1c
Boris.Obinyakov <andthenyouadd@gmail.com>
2025-04-26 23:17:19 +0000

    Prop for hadling links usage

--- nextgisweb/gui/nodepkg/description/DescriptionComponent.tsx
+++ nextgisweb/gui/nodepkg/description/DescriptionComponent.tsx
@@ -1,20 +1,22 @@
 import type { Ref } from "react";
 
 import "./description.css";
-import { processHtmlWithImage } from "./processHTML";
+import { processHtml } from "./processHTML";
 
 type DescriptionComponent = {
     content: string;
+    onLinkClick?: (() => void) | null;
     elementRef?: Ref<HTMLDivElement> | null;
     className?: string;
 };
 
 const DescriptionComponent = ({
     content,
+    onLinkClick = null,
     elementRef = null,
     className = "",
 }: DescriptionComponent) => {
-    const processedContent = processHtmlWithImage({ htmlString: content });
+    const processedContent = processHtml(content, onLinkClick);
 
     return (
         <div ref={elementRef} className={`description-html ${className}`}>
