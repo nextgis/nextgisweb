2b3e5b0ef8cd2d4ee972a044835989a9a31d3582
Denis <rykovd@gmail.com>
2025-05-12 09:17:34 +0000

    Implement raster layer identification HTTP API
    NGW-454

---
+++ nextgisweb/raster_layer/api_identify.py
@@ -0,0 +1,85 @@
+from typing import Annotated, Any
+
+from msgspec import Struct
+from osgeo import gdal
+
+from nextgisweb.env import DBSession
+from nextgisweb.lib.apitype import Query
+
+from nextgisweb.resource import DataScope, ResourceRef
+
+from .model import COLOR_INTERPRETATION, RasterLayer
+
+
+class Point(Struct, kw_only=True):
+    x: float
+    y: float
+
+
+class RasterLayerIdentifyItem(Struct, kw_only=True):
+    resource: ResourceRef
+    color_interpretation: list[str]
+    values: list[Any]
+
+
+class RasterLayerIdentifyResponse(Struct, kw_only=True):
+    items: list[RasterLayerIdentifyItem]
+
+
+def identify(
+    request,
+    *,
+    resources: list[int],
+    point: Annotated[Point, Query(spread=True)],
+) -> RasterLayerIdentifyResponse:
+    """Get raster values at specific point for list of resources"""
+
+    query = DBSession.query(RasterLayer).filter(RasterLayer.id.in_(resources))
+
+    items: list[RasterLayerIdentifyItem] = []
+    for res in query:
+        if res.has_permission(DataScope.read, request.user):
+            ds = res.gdal_dataset()
+            if (values := val_at_coord(ds, point)) is None:
+                continue
+
+            color_interpretation = [
+                COLOR_INTERPRETATION[ds.GetRasterBand(bidx).GetRasterColorInterpretation()]
+                for bidx in range(1, res.band_count + 1)
+            ]
+
+            items.append(
+                RasterLayerIdentifyItem(
+                    resource=ResourceRef(id=res.id),
+                    color_interpretation=color_interpretation,
+                    values=values.flatten().tolist(),
+                )
+            )
+
+    return RasterLayerIdentifyResponse(items=items)
+
+
+def val_at_coord(ds: gdal.Dataset, point: Point):
+    """Simplified version of gdallocationinfo with less options. Borrowed from
+    https://github.com/OSGeo/gdal/blob/master/swig/python/gdal-utils/osgeo_utils/samples/gdallocationinfo.py
+    """
+
+    # Read geotransform matrix and calculate corresponding pixel coordinates
+    geotransform = ds.GetGeoTransform()
+    inv_geotransform = gdal.InvGeoTransform(geotransform)
+    i = int(inv_geotransform[0] + inv_geotransform[1] * point.x + inv_geotransform[2] * point.y)
+    j = int(inv_geotransform[3] + inv_geotransform[4] * point.x + inv_geotransform[5] * point.y)
+
+    if i < 0 or i >= ds.RasterXSize or j < 0 or j >= ds.RasterYSize:
+        return None
+
+    result = ds.ReadAsArray(i, j, 1, 1)
+    return result
+
+
+def setup_pyramid(comp, config):
+    config.add_route(
+        "raster_layer.identify",
+        "/api/component/raster_layer/identify",
+        get=identify,
+    )
