9ac94c8d5f1184df65d2573fc8db0f315f7a2b5c
Aleksandr Dezhin <me@dezhin.net>
2025-05-22 13:46:46 +0000

    FIXUP Note on timezone plugin

--- nextgisweb/gui/nodepkg/dayjs.ts
+++ nextgisweb/gui/nodepkg/dayjs.ts
@@ -4,6 +4,10 @@ import { default as _localizedFormat } from "dayjs/plugin/localizedFormat";
 import { default as _relativeTime } from "dayjs/plugin/relativeTime";
 import { default as _utc } from "dayjs/plugin/utc";
 
+// WARNING: Avoid using the dayjs/plugin/timezone plugin as it creates problems
+// with the DatePicker widget when handling dates before the year 99, including
+// BC dates.
+
 for (const plugin of [_utc, _localizedFormat, _relativeTime, _localeData]) {
     dayjs.extend(plugin);
 }
