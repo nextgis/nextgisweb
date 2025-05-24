1a8189300efc401f055ed326e26de0a78f3dd327
Aleksandr Dezhin <me@dezhin.net>
2025-05-24 21:46:42 +0000

    Fix cleanup tests
    NGW-1597

--- nextgisweb/file_storage/test/test_cleanup.py
+++ nextgisweb/file_storage/test/test_cleanup.py
@@ -15,13 +15,37 @@ def off_keep_interfal(ngw_env):
     ngw_env.file_storage.options["cleanup_keep_interval"] = value
 
 
-def test_keep_delete(ngw_env, ngw_txn):
-    fobj_keep = FileObj(component="test").from_content(b"").persist()
-    fobj_delete = FileObj(component="test").from_content(b"")
+@pytest.mark.parametrize(
+    "wet",
+    [
+        pytest.param(True, id="wet"),
+        pytest.param(False, id="dry"),
+    ],
+)
+@pytest.mark.parametrize(
+    "kwargs, unref, orphan",
+    [
+        pytest.param(dict(), False, False, id="default"),
+        pytest.param(dict(unreferenced=True, orphaned=True), False, False, id="force"),
+        pytest.param(dict(unreferenced=False, orphaned=False), True, True, id="disable"),
+    ],
+)
+def test_cleanup(wet, kwargs, unref, orphan, ngw_env, ngw_txn):
+    fobj_unref = FileObj(component="test").from_content(b"").persist()
+    fobj_orphan = FileObj(component="test").from_content(b"")
+
+    assert fobj_unref.filename().is_file()
+    assert fobj_orphan.filename().is_file()
 
     DBSession.flush()
 
-    ngw_env.file_storage.cleanup(dry_run=False)
+    ngw_env.file_storage.cleanup(**kwargs, dry_run=not wet)
 
-    assert fobj_keep.filename().is_file()
-    assert not fobj_delete.filename().is_file()
+    fe_unref = fobj_unref.filename().is_file()
+    fe_orphan = fobj_orphan.filename().is_file()
+
+    if wet:
+        assert fe_unref == unref
+        assert fe_orphan == orphan
+    else:
+        assert fe_unref and fe_orphan
