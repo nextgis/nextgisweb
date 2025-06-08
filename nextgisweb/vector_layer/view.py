3efb4002c5a45dd7002d9b592f31a2b3b6d3fb63
Aleksandr Dezhin <me@dezhin.net>
2025-06-08 18:33:12 +0000

    Hide unavailable operations for versioned layers
    NGW-1595

--- nextgisweb/vector_layer/view.py
+++ nextgisweb/vector_layer/view.py
@@ -8,3 +8,6 @@ class VectorLayerWidget(Widget):
     resource = VectorLayer
     operation = ("create", "update")
     amdmod = jsentry("@nextgisweb/vector-layer/resource-widget")
+
+    def is_applicable(self):
+        return isinstance(self.obj, VectorLayer) and self.obj.fversioning is None
