2e2fe409a8d29b4351bb81d26a4c7297dc49e699
Artemiy <rendrom@gmail.com>
2025-07-03 08:58:21 +0000

    Enhance useKeydownListener to show isPressed state

--- nextgisweb/gui/nodepkg/hook/useKeydownListener.ts
+++ nextgisweb/gui/nodepkg/hook/useKeydownListener.ts
@@ -1,26 +1,48 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 
 import { useEventListener } from "./useEventListener";
 
 export function useKeydownListener<T extends HTMLElement = HTMLDivElement>(
+    /** See all available keys here https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_key_values */
     key: string,
-    handlerProp: (e: KeyboardEvent) => void,
+    handlerProp?: (e: KeyboardEvent) => void,
     element?: React.RefObject<T>,
     options?: boolean | AddEventListenerOptions
 ) {
-    const handler_ = useCallback(
+    const [isPressed, setIsPressed] = useState(false);
+
+    const downHandler = useCallback(
         (e: KeyboardEvent) => {
             if (e.key && e.key.toLowerCase() === key.toLowerCase()) {
-                handlerProp(e);
+                setIsPressed(true);
+                handlerProp?.(e);
             }
         },
         [handlerProp, key]
     );
 
+    const upHandler = useCallback(
+        (e: KeyboardEvent) => {
+            if (e.key && e.key.toLowerCase() === key.toLowerCase()) {
+                setIsPressed(false);
+            }
+        },
+        [key]
+    );
+
     useEventListener(
         "keydown",
-        handler_,
+        downHandler,
         element as React.RefObject<T>,
         options
     );
+
+    useEventListener(
+        "keyup",
+        upHandler,
+        element as React.RefObject<T>,
+        options
+    );
+
+    return isPressed;
 }
