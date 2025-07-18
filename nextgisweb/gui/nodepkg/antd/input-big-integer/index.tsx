cea84e355adeeb28b77aafcd8aa9aeac3813c062
Artemiy <rendrom@gmail.com>
2025-07-18 08:13:38 +0000

    Add min and max for integer and biginteger input
    NGW-1610

--- nextgisweb/gui/nodepkg/antd/input-big-integer/index.tsx
+++ nextgisweb/gui/nodepkg/antd/input-big-integer/index.tsx
@@ -1,19 +1,44 @@
 import { InputNumber } from "@nextgisweb/gui/antd";
 import type { InputNumberProps } from "@nextgisweb/gui/antd";
 
-export type InputBigIntegerProps = InputNumberProps;
+export type InputBigIntegerProps = Omit<InputNumberProps<string>, "stringMode">;
 
-export default function InputBigIntegerProps(props: InputBigIntegerProps) {
+const MAX_INT64 = (BigInt(1) << BigInt(63)) - BigInt(1);
+const MIN_INT64 = -(BigInt(1) << BigInt(63));
+
+function clampBigInt(value: string | number | bigint): bigint {
+    const v = typeof value === "bigint" ? value : BigInt(value);
+    return v < MIN_INT64 ? MIN_INT64 : v > MAX_INT64 ? MAX_INT64 : v;
+}
+
+export default function InputBigInteger({
+    min: minProp,
+    max: maxProp,
+    ...props
+}: InputBigIntegerProps) {
+    const min = minProp !== undefined ? clampBigInt(minProp) : MIN_INT64;
+    const max = maxProp !== undefined ? clampBigInt(maxProp) : MAX_INT64;
+
+    // Formatter ensures the entered value is parsed as a bigint
+    const formatter = (v: number | string | undefined) => {
+        if (typeof v === "string" && v) {
+            try {
+                const bigIntPart = v.split(".")[0];
+                const clamped = clampBigInt(bigIntPart);
+                return String(clamped);
+            } catch {
+                return "";
+            }
+        }
+        return "";
+    };
+
     return (
         <InputNumber
             stringMode
-            formatter={(val) => {
-                if (typeof val === "string") {
-                    const v = val.match(/\d+/);
-                    return v ? v[0] : "";
-                }
-                return "";
-            }}
+            max={max.toString()}
+            min={min.toString()}
+            formatter={formatter}
             {...props}
         />
     );
--- nextgisweb/gui/nodepkg/antd/input-integer/index.tsx
+++ nextgisweb/gui/nodepkg/antd/input-integer/index.tsx
@@ -1,20 +1,37 @@
+import { clamp } from "lodash-es";
+
 import { InputNumber } from "@nextgisweb/gui/antd";
 import type { InputNumberProps } from "@nextgisweb/gui/antd";
 
-export type InputIntegerProps = InputNumberProps;
+export type InputIntegerProps = Omit<InputNumberProps<number>, "formatter">;
 
-export default function InputInteger(props: InputNumberProps) {
-    const maxint = 2 ** 63 - 1;
-    const max: number = (props.max as number) ?? maxint;
+const MAX_INT32 = 2 ** 31 - 1;
+const MIN_INT32 = -(2 ** 31);
 
+function clampInt(val: number) {
+    return clamp(val, MIN_INT32, MAX_INT32);
+}
+
+export default function InputInteger({
+    min: minProp,
+    max: maxProp,
+    ...props
+}: InputIntegerProps) {
+    const min = clampInt(minProp ?? MIN_INT32);
+    const max = clampInt(maxProp || MAX_INT32);
+
+    // Formatter is used to ensure the entered value is parsed as an integer
     const formatter = (v: number | string | undefined) => {
-        if (typeof v === "string") {
-            const int = parseInt(v, 10);
-            const maxInt = int > max ? props.max : int;
-            return v ? String(maxInt) : "";
+        if (typeof v === "string" && v) {
+            try {
+                const int = parseInt(v, 10);
+                return String(clampInt(int));
+            } catch {
+                return "";
+            }
         }
         return "";
     };
 
-    return <InputNumber {...props} formatter={formatter} />;
+    return <InputNumber min={min} max={max} formatter={formatter} {...props} />;
 }
