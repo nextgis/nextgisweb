1578098ac5f74fd8455bb5c4f610addebe7463f2
Artemiy <rendrom@gmail.com>
2025-09-03 22:44:01 +0000

    Remove mocha from external build
    NGW-1705

--- nextgisweb/jsrealm/nodepkg/jsrealm/testentry/driver/mocha.ts
+++ nextgisweb/jsrealm/nodepkg/jsrealm/testentry/driver/mocha.ts
@@ -1,13 +1,15 @@
 import "./mocha.less";
 
-export default (value: (...args: []) => void, el: HTMLElement) => {
+import type * as Mocha from "mocha";
+
+export default async (value: (...args: []) => void, el: HTMLElement) => {
     const root = document.createElement("div");
     root.id = "mocha";
     el.append(root);
-
-    ngwExternal("mocha/mocha").then(() => {
-        mocha.setup("bdd");
-        value();
-        mocha.run();
-    });
+    // @ts-expect-error There is no type declarations for Mocha UMD
+    await import("mocha/mocha");
+    const mocha = (window as any).mocha as Mocha.MochaGlobals;
+    mocha.setup("bdd");
+    value();
+    mocha.run();
 };
