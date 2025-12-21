1038b2d9260219324b341ca0a7798316a41b7b6f
Aleksandr Dezhin <me@dezhin.net>
2025-12-21 05:00:57 +0000

    Remove deprecated Enum re-export

--- nextgisweb/lib/db.py
+++ nextgisweb/lib/db.py
@@ -1,14 +1,3 @@
 from sqlalchemy import *  # noqa: F403
 from sqlalchemy import event, func, sql, types  # noqa: F401
 from sqlalchemy.orm import *  # noqa: F403
-from typing_extensions import deprecated
-
-import nextgisweb.lib.saext as saext
-
-
-@deprecated(
-    "Use 'from nextgisweb.lib.saext import Enum' instead, which available "
-    "since nextgisweb >= 4.9.0.dev14."
-)
-class Enum(saext.Enum):
-    pass
