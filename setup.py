4c5bfaa149ab92f8672c851c0b37f044681fed11
Aleksandr Dezhin <me@dezhin.net>
2026-01-11 18:25:21 +0000

    Add pytest-cov development dependency

--- setup.py
+++ setup.py
@@ -77,6 +77,7 @@ extras_require = dict(
         "mapbox-vector-tile",
         "pre-commit",
         "pyramid-debugtoolbar==4.10",
+        "pytest-cov",
         "pytest==9.0.*",
         "ruff>=0.12.12",
         "sqlglot==26.30.0",
