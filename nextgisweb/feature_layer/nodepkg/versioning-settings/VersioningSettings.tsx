9c432b6c71f8736844ca04e5af01b536f1f857c1
Aleksandr Dezhin <me@dezhin.net>
2026-02-01 02:36:23 +0000

    Use three states for default feature versioning setting
    NGW-1786

--- nextgisweb/feature_layer/nodepkg/versioning-settings/VersioningSettings.tsx
+++ nextgisweb/feature_layer/nodepkg/versioning-settings/VersioningSettings.tsx
@@ -1,73 +1,77 @@
 import { useCallback, useEffect, useState } from "react";
 
-import type {
-    FeatureLayerCSettingsRead,
-    FeatureLayerCSettingsUpdate,
-} from "@nextgisweb/feature-layer/type/api";
-import { message } from "@nextgisweb/gui/antd";
-import { LoadingWrapper } from "@nextgisweb/gui/component";
+import { Radio, Space, message } from "@nextgisweb/gui/antd";
+import { LoadingWrapper, SaveButton } from "@nextgisweb/gui/component";
 import { errorModal } from "@nextgisweb/gui/error";
+import { assert } from "@nextgisweb/jsrealm/error";
 import { route } from "@nextgisweb/pyramid/api";
 import { useAbortController } from "@nextgisweb/pyramid/hook";
+import { useRouteGet } from "@nextgisweb/pyramid/hook/useRouteGet";
 import { gettext } from "@nextgisweb/pyramid/i18n";
 
-import { VersioningForm } from "./VersioningForm";
+// prettier-ignore
+const msg = {
+    info: gettext("Select the default feature versioning setting for new layers. This affects layers created via UI and HTTP API. You can always change the versioning setting for individual layers later."),
+    auto: gettext("Auto: follow system-wide settings"),
+    true: gettext("Enable feature versioning by default"),
+    false: gettext("Disable feature versioning by default"),
+};
 
 export function VersioningSettings() {
-    const [status, setStatus] = useState<string>("loading");
-    const [settings, setSettings] = useState<FeatureLayerCSettingsRead>();
+    const [value, setValue] = useState<boolean | null>();
     const { makeSignal } = useAbortController();
-    const [messageApi, contextHolder] = message.useMessage();
+    const [saving, setSaving] = useState(false);
+
+    const { data, isLoading } = useRouteGet({
+        name: "pyramid.csettings",
+        options: { query: { feature_layer: ["versioning_default"] } },
+    });
 
     useEffect(() => {
-        async function load() {
-            try {
-                const signal = makeSignal();
-                const csettings = await route("pyramid.csettings").get({
-                    query: { feature_layer: ["all"] },
-                    signal,
-                });
-                setSettings(csettings.feature_layer);
-            } catch (err) {
-                errorModal(err);
-            } finally {
-                setStatus("loaded");
-            }
+        if (!data) return;
+        const newValue = data.feature_layer?.versioning_default;
+        assert(newValue !== undefined);
+        setValue(newValue);
+    }, [data]);
+
+    const [messageApi, contextHolder] = message.useMessage();
+
+    const save = useCallback(async () => {
+        setSaving(true);
+        try {
+            await route("pyramid.csettings").put({
+                json: { feature_layer: { versioning_default: value } },
+                signal: makeSignal(),
+            });
+            messageApi.success(gettext("The setting is saved."));
+        } catch (err) {
+            errorModal(err);
+        } finally {
+            setSaving(false);
         }
-        load();
-    }, [makeSignal]);
+    }, [value, makeSignal, messageApi]);
 
-    const onFinish = useCallback(
-        async (values: FeatureLayerCSettingsUpdate) => {
-            setStatus("saving");
-            try {
-                await route("pyramid.csettings").put({
-                    json: { feature_layer: values },
-                    signal: makeSignal(),
-                });
-
-                messageApi.success(gettext("Settings saved"));
-            } catch (err) {
-                errorModal(err);
-            } finally {
-                setStatus("loaded");
-            }
-        },
-        [makeSignal, messageApi]
-    );
-
-    if (status === "loading") {
-        return <LoadingWrapper loading={true} />;
+    if (isLoading) {
+        return <LoadingWrapper />;
     }
 
     return (
-        <>
+        <Space direction="vertical" size="middle">
             {contextHolder}
-            <VersioningForm
-                initialValues={settings}
-                onFinish={onFinish}
-                status={status}
-            />
-        </>
+            <div style={{ maxWidth: "50em" }}>{msg.info}</div>
+            <Radio.Group
+                value={String(value)}
+                onChange={(e) => setValue(eval(e.target.value))}
+            >
+                <Space direction="vertical">
+                    <Radio value="null">{msg.auto}</Radio>
+                    <Radio value="true">{msg.true}</Radio>
+                    <Radio value="false">{msg.false}</Radio>
+                </Space>
+            </Radio.Group>
+            <SaveButton onClick={save} loading={saving}>
+                {gettext("Save")}
+            </SaveButton>
+        </Space>
     );
 }
