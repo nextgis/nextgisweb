5d5cc434eaff36bf521f72b9fdb4e5fb3d7f8e3a
Artemiy <rendrom@gmail.com>
2026-02-19 11:56:25 +0000

    Hide identify toggle for non-identifiable layers
    NGW-1971

--- nextgisweb/webmap/nodepkg/plugin/layer-identifiable/LayerIdentifiablePlugin.tsx
+++ nextgisweb/webmap/nodepkg/plugin/layer-identifiable/LayerIdentifiablePlugin.tsx
@@ -1,10 +1,17 @@
 import { Checkbox } from "@nextgisweb/gui/antd";
 import { gettext } from "@nextgisweb/pyramid/i18n";
+import type { TreeLayerStore } from "@nextgisweb/webmap/store/tree-store/TreeItemStore";
 import type { PluginState } from "@nextgisweb/webmap/type";
 
 import { PluginBase } from "../PluginBase";
 
 export class LayerIdentifiablePlugin extends PluginBase {
+    getPluginState(nodeData: TreeLayerStore): PluginState {
+        const stat = super.getPluginState(nodeData);
+        stat.enabled = stat.enabled && !!nodeData.identification;
+        return stat;
+    }
+
     render({ nodeData }: PluginState) {
         const { identifiable } = nodeData;
 
