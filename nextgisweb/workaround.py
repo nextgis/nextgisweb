2a03875caf5f751851c3ac6b194eb3655107a048
Eduard Popov <popovfest@gmail.com>
2026-04-22 03:41:11 +0000

    Remove python < 3.11 backport

--- nextgisweb/workaround.py
+++ nextgisweb/workaround.py
@@ -1,10 +1,4 @@
 # ruff: noqa: F401
-import sys
 
 # https://bugs.python.org/issue47082
 import numpy
-
-if sys.version_info < (3, 11):
-    from backports.datetime_fromisoformat import MonkeyPatch
-
-    MonkeyPatch.patch_fromisoformat()
