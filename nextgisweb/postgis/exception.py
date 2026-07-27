a4e6f1886a2db2863eb3f7709f72a33c3680d951
Aleksandr Dezhin <me@dezhin.net>
2026-07-27 19:53:16 +0000

    Handle ExternalDatabaseError psycopg 3 comatible way
    NGW-1875

--- nextgisweb/postgis/exception.py
+++ nextgisweb/postgis/exception.py
@@ -1,23 +1,30 @@
 from sqlalchemy.exc import StatementError
 
-from nextgisweb.env import gettext
+from nextgisweb.env import gettext, gettextf
 
 from nextgisweb.core.exception import ExternalServiceError
 
 
 class ExternalDatabaseError(ExternalServiceError):
     title = gettext("External database error")
+    message = gettext("An error occurred while accessing the external database.")
+
+    _detail_sqlstate = gettextf(
+        "PostgreSQL error code: {}. An explanation of the error code can be found here: "
+        "https://www.postgresql.org/docs/current/errcodes-appendix.html. Additional details may "
+        "be available in the external database logs."
+    )
 
     def __init__(self, *args, sa_error=None, **kwargs):
         super().__init__(*args, **kwargs)
 
-        if not hasattr(self, "detail") and sa_error is not None:
+        if sa_error is not None:
             if (
                 isinstance(sa_error, StatementError)
-                and sa_error.orig is not None
-                and sa_error.orig.pgcode is not None
+                and (dbapi_error := sa_error.orig) is not None
+                and (dbapi_diag := dbapi_error.diag) is not None
             ):
-                detail = "PostgreSQL error code: %s." % sa_error.orig.pgcode
+                # TODO: Add human-readable error name after psycopg upgrade
+                self.detail = self._detail_sqlstate(dbapi_diag.sqlstate)
             else:
-                detail = "SQLAlchemy error code: %s." % sa_error.code
-            setattr(self, "detail", detail)
+                self.detail = "SQLAlchemy error code: %s." % sa_error.code
