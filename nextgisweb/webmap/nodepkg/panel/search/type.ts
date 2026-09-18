d3fae513d621e533726288ab66bb9ca3afa3b97a
Zakhar Putivtsev <send.zpko@gmail.com>
2026-09-18 18:41:09 +0000

    WIP Use new API for text search

--- nextgisweb/webmap/nodepkg/panel/search/type.ts
+++ nextgisweb/webmap/nodepkg/panel/search/type.ts
@@ -17,6 +17,7 @@ export interface SearchResult {
   geometry: Geometry;
   key: number;
   featureId?: number;
+  searchContext?: number[];
 }
 
 type SearchStepResult = [number, SearchResultGroup[], boolean];
